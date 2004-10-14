11c1c2411d1988d02827644a12d72b09b90f2e39
Gordon Maclean <maclean@ucar.edu>
2004-10-14 20:19:00 +0000

    added support for callback arg in interrupt register

--- dsm/modules/rtl_isa_irq.c
+++ dsm/modules/rtl_isa_irq.c
@@ -29,7 +29,7 @@
 
 /* RTLinux includes...  */
 #include <rtl_posixio.h>
-#include <rtl_pthread.h>
+// #include <rtl_pthread.h>
 #include <rtl_spinlock.h>
 
 /* Linux module includes... */
@@ -45,24 +45,37 @@ RTLINUX_MODULE(rtl_isa_irq);
 MODULE_AUTHOR("John Wasinger <wasinger@ucar.edu>");
 MODULE_DESCRIPTION("RTLinux ISA IRQ de-multiplexing Module");
 
+#define NUM_ISA_IRQS VIPER_NUM_ISA_IRQS
+
 static unsigned int rtl_isa_irq_enabled_mask = 0;
 
+struct isrData {
+    isa_irq_hander_t handler;
+    void* callbackPtr;
+};
+
 /* 
- * isa_isrs[] is an array of function pointers to interrupt service routines
- * for devices that are connected to the ISA bus.  This implementation
+ * isa_isrs[] is an array of isrData structures, containing function
+ * pointers to interrupt service routines for devices that are
+ * connected to the ISA bus.  This implementation
  * doesn't support sharing ISA interrupts, only one ISR is called
  * for each interrupt.
+ * This current version must be called from your modules init_module
+ * function since it does a kmalloc() here. To remove that restriction,
+ * one could kmalloc all the structures during this init_module,
+ * and initialize the function pointers to 0.
+ *
  * It could be enhanced to support multiple ISRs.
- * If one requires that rtl_request_isa_irq be called at module init
+ * If one requires that rtl_request_isa_irq be called at init_module
  * time, then you could kmalloc entries in a list.
  * Otherwise, one could declare fixed arrays of ISRs (say 5) for
  * each IRQ.
  */
 
-static isa_irq_hander_t isa_isrs[NUM_ISA_IRQS];
+static struct isrData* isa_isrs[NUM_ISA_IRQS];
 static int isa_irqs[NUM_ISA_IRQS] = { 3,4,5,6,7,10,11,12 };
 
-static rtl_pthread_spinlock_t irq_controller_lock;
+static rtl_spinlock_t irq_controller_lock;
 
 /* -- Utility --------------------------------------------------------- */
 static void rtl_mask_and_ack_isa_irq(unsigned int isa_irq_index)
@@ -85,99 +98,134 @@ static unsigned int rtl_isa_irq_demux_isr (unsigned int irq, struct rtl_frame *r
 {
   unsigned int irq_status;
   int i, res = 0;
-
-  rtl_global_pend_irq(irq);
-  // rtl_printf("isa demux, irq=%d\n",irq);
+  int imask = 1;
 
   irq_status = VIPER_ISA_IRQ_STATUS & 0xFF;
+  // rtl_printf("isa irq=%d, status=%x, mask=%x\n",irq,irq_status,
+  // 	rtl_isa_irq_enabled_mask);
+
+  if (!irq_status) {
+      // Don't do the rtl_global_pend_irq(irq), just return
+      rtl_hard_enable_irq(irq);
+      return 0;
+  }
 
   /* Continue processing multiplexed ISA ISRs until all are processed... */
-  while ( irq_status & rtl_isa_irq_enabled_mask )
-  {
-    rtl_pthread_spin_lock(&irq_controller_lock);
-    for (i=0; i < VIPER_NUM_ISA_IRQS; i++)
-    {
-      if (irq_status & (1<<i))
-      {
+
+  while (irq_status & rtl_isa_irq_enabled_mask) {
+    imask = 1;
+    rtl_spin_lock(&irq_controller_lock);
+    for (i=0; i < NUM_ISA_IRQS; i++) {
+      if (irq_status & imask) {
         /* Ack the indicated ISR's register bit. */
         rtl_mask_and_ack_isa_irq(i);
 
-	// rtl_printf("isa irq=%d\n",i);
         /* Call the indicated ISR. */
-	if (isa_isrs[i]) res = (*isa_isrs[i])(isa_irqs[i], regs);
+	if (isa_isrs[i]) res =
+	    (*isa_isrs[i]->handler)(isa_irqs[i], isa_isrs[i]->callbackPtr,regs);
 
 	/* Enable (unmask) the indicated ISR again. */
         rtl_unmask_isa_irq(i);
+
+	irq_status &= ~imask;
+	if (!irq_status) break;		// done
       }
+      imask <<= 1;
     }
+    rtl_spin_unlock(&irq_controller_lock);
     irq_status = VIPER_ISA_IRQ_STATUS & 0xFF;
-    rtl_pthread_spin_unlock(&irq_controller_lock);
+    // rtl_printf("isa irq=%d, status=%x, mask=%x\n",irq,irq_status,
+  	// rtl_isa_irq_enabled_mask);
   }
+
+  rtl_global_pend_irq(irq);
   rtl_hard_enable_irq(irq);
   return res;
 }
 
-int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler)
+int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler, void* callbackPtr)
 {
     int i;
-    rtl_pthread_spin_lock(&irq_controller_lock);
+    int ret = -EINVAL;		/* no such isa irq */
+    unsigned long flags;
+
+    rtl_spin_lock_irqsave(&irq_controller_lock,flags);
     for (i = 0; i < NUM_ISA_IRQS; i++) {
 	if (isa_irqs[i] == irq) {
+
 	    rtl_printf("requesting isa irq=%d\n",irq);
-	    isa_isrs[i] = handler;
+
+	    ret = -EBUSY;
+	    if (isa_isrs[i]) break;
+
+	    ret = -ENOMEM;
+	    isa_isrs[i] = kmalloc(sizeof(struct isrData),GFP_KERNEL);
+	    if (!isa_isrs[i]) break;
+
+	    isa_isrs[i]->handler = handler;
+	    isa_isrs[i]->callbackPtr = callbackPtr;
 	    rtl_unmask_isa_irq(i);
-	    rtl_pthread_spin_unlock(&irq_controller_lock);
-	    return 0;
+	    ret = 0;
+	    break;
 	}
     }
-    rtl_pthread_spin_unlock(&irq_controller_lock);
-    return -RTL_EINVAL;		/* no such isa irq */
+    rtl_spin_unlock_irqrestore(&irq_controller_lock,flags);
+    return ret;
 }
 
 int rtl_free_isa_irq(unsigned int irq)
 {
     int i;
-    rtl_pthread_spin_lock(&irq_controller_lock);
+    int ret = -EINVAL;		/* no such isa irq */
+    unsigned long flags;
+    rtl_spin_lock_irqsave(&irq_controller_lock,flags);
+
     for (i = 0; i < NUM_ISA_IRQS; i++) {
-	if (isa_irqs[i] == irq) {
+	if (isa_irqs[i] == irq && isa_isrs[i]) {
 	    rtl_printf("freeing isa irq=%d\n",irq);
-	    isa_isrs[i] = NULL;
 	    rtl_mask_isa_irq(i);
-	    rtl_pthread_spin_unlock(&irq_controller_lock);
-	    return 0;
+	    if (isa_isrs[i]) kfree(isa_isrs[i]);
+	    isa_isrs[i] = NULL;
+	    ret = 0;
+	    break;
 	}
     }
-    rtl_pthread_spin_unlock(&irq_controller_lock);
-    rtl_printf("can't free isa irq=%d\n",irq);
-    return -RTL_EINVAL;		/* no such isa irq */
+
+    rtl_spin_unlock_irqrestore(&irq_controller_lock,flags);
+
+    if (ret == -EINVAL) rtl_printf("can't free isa irq=%d\n",irq);
+    return ret;
 }
 
 /* -- MODULE ---------------------------------------------------------- */
 void cleanup_module (void)
 {
-  int i;
+    int i;
 
-  rtl_pthread_spin_lock(&irq_controller_lock);
-  /* disable ISRs */
-  for (i=0; i<NUM_ISA_IRQS; i++) {
-    if (isa_isrs[i]) {
-	rtl_printf("(%s) %s:\t IRQ %2d disabled\n",
-                 __FILE__, __FUNCTION__, isa_irqs[i]);
-	rtl_mask_isa_irq(i);
-	isa_isrs[i] = NULL;
+    unsigned long flags;
+    rtl_spin_lock_irqsave(&irq_controller_lock,flags);
+
+    /* disable ISRs */
+    for (i=0; i<NUM_ISA_IRQS; i++) {
+	if (isa_isrs[i]) {
+	    rtl_printf("(%s) %s:\t IRQ %2d disabled\n",
+		     __FILE__, __FUNCTION__, isa_irqs[i]);
+	    rtl_mask_isa_irq(i);
+	    kfree(isa_isrs[i]);
+	    isa_isrs[i] = NULL;
+	}
     }
-  }
-  rtl_pthread_spin_unlock(&irq_controller_lock);
+    rtl_spin_unlock_irqrestore(&irq_controller_lock,flags);
 
-  /* release interrupt handler */
-  rtl_free_irq(VIPER_CPLD_IRQ);
+    /* release interrupt handler */
+    rtl_free_irq(VIPER_CPLD_IRQ);
 }
 /* -- MODULE ---------------------------------------------------------- */
 int init_module (void)
 {
   int i;
 
-  rtl_pthread_spin_init(&irq_controller_lock,0);
+  rtl_spin_lock_init(&irq_controller_lock);
 
   rtl_printf("(%s) %s:\t compiled on %s at %s\n\n",
              __FILE__, __FUNCTION__, __DATE__, __TIME__);
--- dsm/modules/rtl_isa_irq.h
+++ dsm/modules/rtl_isa_irq.h
@@ -7,10 +7,19 @@
 
 #include <rtl.h>
 
-#define NUM_ISA_IRQS 8
+// #define NUM_ISA_IRQS 8
 
-typedef unsigned int (*isa_irq_hander_t) (unsigned int irq, struct rtl_frame *regs);
+typedef unsigned int (*isa_irq_hander_t) (unsigned int irq, void* callbackPtr,
+	struct rtl_frame *regs);
 
-int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler);
+/* register an ISA interrupt handler to be called on receipt of ISA
+ * interrupt.
+ * Params:
+ *    irq: an ISA interrupt: 3,4,5,6,7,10,11 or 12
+ *    handler: a pointer to a function of type isa_irq_handler_t
+ *    callbackPtr: the pointer that you want passed to your handler,
+ *	which may be (void*)0 (NULL) if you don't need it.
+ */
+int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler,void* callbackPtr);
 
 int rtl_free_isa_irq(unsigned int irq);
