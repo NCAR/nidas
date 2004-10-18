bc6512ea6aaa00d7788d2736c0f7e0b6fb9ceb98
Gordon Maclean <maclean@ucar.edu>
2004-10-18 14:27:10 +0000

    header files for viper and win_com8 serial card

---
+++ dsm/modules/dsm_viper.h
@@ -0,0 +1,24 @@
+/*
+  *******************************************************************
+  Copyright by the National Center for Atmospheric Research
+									      
+  $LastChangedDate$
+									      
+  $LastChangedRevision$
+									      
+  $LastChangedBy$
+									      
+  $HeadURL$
+
+  *******************************************************************
+
+  Constants needed for driver code on an Arcom Viper card.
+
+*/
+
+#ifndef DSM_VIPER_H
+#define DSM_VIPER_H
+
+#define SYSTEM_ISA_IOPORT_BASE 0xf7000000
+
+#endif
