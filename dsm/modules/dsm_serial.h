df6779eca3ca3b5f6ffdd5fbbfd4b4923a2c4b34
Gordon Maclean <maclean@ucar.edu>
2004-10-18 14:58:32 +0000

    new serial port driver and fifos

---
+++ dsm/modules/dsm_serial.h
@@ -0,0 +1,213 @@
+/*
+  *******************************************************************
+  Copyright by the National Center for Atmospheric Research
+									      
+  $LastChangedDate$
+									      
+  $LastChangedRevision$
+									      
+  $LastChangedBy$
+									      
+  $HeadURL$
+
+  *******************************************************************
+
+   RTLinux serial driver for NCAR ADS3 DSM system. Serial port
+   input is assembled into time-tagged samples.
+
+*/
+
+#ifndef DSM_SERIAL_H
+#define DSM_SERIAL_H
+
+#ifndef __KERNEL__
+#include <sys/ioctl.h>
+#endif
+
+#include <irigclock.h>		/* for irigClockRates */
+
+#define DSM_SERIAL_MAGIC 'S'
+
+#define MAX_DSM_SERIAL_MESSAGE_SIZE 1024
+
+struct dsm_serial_prompt {
+  char str[128];
+  int len;
+  enum irigClockRates rate;
+};
+
+struct dsm_serial_record_info {
+  char sep[16];
+  int sepLen;
+  unsigned char atBOM;
+  int recordLen;
+};
+
+typedef unsigned long dsm_sample_timetag_t;
+typedef unsigned short dsm_sample_length_t;
+
+struct dsm_serial_sample {
+  dsm_sample_timetag_t timetag;	/* timetag of sample */
+  dsm_sample_length_t length;	/* number of bytes in data */
+  char data[];			/* the data */
+};
+
+
+/*
+ * The enumeration of IOCTLs that this driver supports.
+ * See pages 130-132 of Linux Device Driver's Manual 
+ */
+
+/*
+ * There is a problem with struct termios in RT-linux.  It is defined
+ * differently in various header files.  The essential fields
+ * at the beginning of the structure are the same, but the value of
+ * NCCS varies, and some definitions include other trailing fields.
+ * is different.
+ *
+ * Here is the basic definition, from
+ *   /opt/rtldk-2.0/rtlinuxpro/linux/include/asm/termbits.h
+ *************************************************************
+ *  typedef unsigned char   cc_t;
+ *  typedef unsigned int    speed_t;
+ *  typedef unsigned int    tcflag_t;
+
+ *  #define NCCS 19
+ *  struct termios {
+ *    tcflag_t c_iflag;               * input mode flags *
+ *    tcflag_t c_oflag;               * output mode flags *
+ *    tcflag_t c_cflag;               * control mode flags *
+ *    tcflag_t c_lflag;               * local mode flags *
+ *    cc_t c_line;                    * line discipline *
+ *    cc_t c_cc[NCCS];                * control characters *
+ *   };
+ *************************************************************
+
+ * FSM labs wrote  /opt/rtldk-2.0/rtlinuxpro/include/posix/termios.h
+ * for their serial driver, which added a bunch of stuff after c_cc.
+
+ * glibc comes with /opt/arm_tools/arm-linux/include/bits/termios.h
+ * which declares NCCS as 32, and adds some speed fields after c_cc.
+ * That is the termios.h that is likely to be included in 
+ * user code.
+
+ * As long as we just use the 4 flag fields and the essential
+ * values in c_cc we're OK.  However user and driver code may
+ * disagree on sizeof(struct termios).
+
+ * The _IO macros use sizeof on their third arg to create the
+ * ioctl command value.  To avoid problems we'll just use "int"
+ * as the third value, so that user and driver code create the
+ * same ioctl values.  Then when doing the DSMSER_TCGETS ioctls,
+ * use SIZEOF_TERMIOS for the length, from either user or
+ * kernel code.
+ */
+
+#define SIZEOF_TERMIOS 36	/* lowest common denominator for termios len */
+
+#define DSMSER_TCSETS _IOW(DSM_SERIAL_MAGIC,0,int)
+#define DSMSER_TCGETS _IOR(DSM_SERIAL_MAGIC,1,int)
+#define DSMSER_WEEPROM _IO(DSM_SERIAL_MAGIC,2)
+#define DSMSER_SET_PROMPT _IOW(DSM_SERIAL_MAGIC,3,\
+	struct dsm_serial_prompt)
+#define DSMSER_GET_PROMPT _IOR(DSM_SERIAL_MAGIC,4,\
+	struct dsm_serial_prompt)
+
+#define DSMSER_START_PROMPTER _IO(DSM_SERIAL_MAGIC,5)
+#define DSMSER_STOP_PROMPTER _IO(DSM_SERIAL_MAGIC,6)
+
+#define DSMSER_SET_RECORD_SEP _IOW(DSM_SERIAL_MAGIC,7,\
+	struct dsm_serial_record_info)
+#define DSMSER_GET_RECORD_SEP _IOR(DSM_SERIAL_MAGIC,8,\
+	struct dsm_serial_record_info)
+
+#ifdef __KERNEL__
+
+#include <linux/circ_buf.h>
+#include <rtl_pthread.h>
+#include <rtl_semaphore.h>
+
+/* exposed functions that we provide */
+
+extern int dsm_serial_get_numboards();
+
+extern const char* dsm_serial_get_devprefix();
+
+extern int dsm_serial_get_numports(int board);
+
+extern const char* dsm_serial_get_devname(int port);
+
+struct dsm_sample_circ_buf {
+    struct dsm_serial_sample **buf;
+    int head;
+    int tail;
+};
+
+#define SAMPLE_POOL_SIZE 4
+
+#define UNKNOWN_TIMETAG_VALUE 0xffffffff
+
+enum boardTypes { BOARD_UNKNOWN, BOARD_WIN_COM8 = 1 };
+
+struct serialPort {
+    struct serialBoard* board;	/* pointer to my board structure */
+    char *devname;	/* device name */
+    int portNum;	/* port number: 0-(N-1), N=total number of ports
+    			 * maintained by this driver, counting all boards */
+    int ioport;		/* ISA IOport address, e.g. 0x300 */
+    unsigned long addr;	/* full address, including BASE address of ISA */
+    int irq;		/* port's interrupt */
+    int type;		/* type of uart, one of the values from serial.h */
+    rtl_spinlock_t lock;	/* mutex lock for the port */
+    int flags;		/* used for ASYNC_* flags in serial.h */
+    int revision;
+    unsigned char MCR;
+    unsigned char IER;
+    unsigned char LCR;
+    unsigned char ACR;
+    int baud_base;
+    int quot;
+    int custom_divisor;
+    int timeout;
+    int xmit_fifo_size;
+    struct termios termios;
+
+    struct dsm_serial_prompt prompt;	/* prompt sent to sensor */
+    unsigned char promptOn;		/* are we sending prompts? */
+
+			/* information about how records are separated
+			 * in the stream of data from the sensor */
+    struct dsm_serial_record_info recinfo;
+    int sepcnt;
+
+    struct dsm_sample_circ_buf sample_pool;
+    struct dsm_serial_sample* sample;	/* current sample being read */
+
+    rtl_sem_t sample_sem;
+
+    int incount;
+    dsm_sample_timetag_t next_timetag;
+    unsigned long nsamples;		/* counter of samples through the sys */
+
+    struct circ_buf xmit;
+
+    int pe_cnt;
+    int oe_cnt;
+    int fe_cnt;
+    int char_overflows;
+    int sample_overflows;
+
+};
+
+struct serialBoard {
+    int type;
+    unsigned long addr;
+    int irq;
+    struct serialPort* ports;
+    int numports;
+    rtl_spinlock_t lock;
+};
+
+#endif	/* __KERNEL__ */
+
+#endif
---
+++ dsm/modules/dsm_serial_fifo.h
@@ -0,0 +1,78 @@
+/*
+  *******************************************************************
+  Copyright by the National Center for Atmospheric Research
+									      
+  $LastChangedDate$
+									      
+  $LastChangedRevision$
+									      
+  $LastChangedBy$
+									      
+  $HeadURL$
+
+  *******************************************************************
+									      
+  FIFO interface between dsm_serial serial port driver and the
+  user side.
+									      
+  Since user space code cannot read directly from RT-Linux devices
+  one has to provide a read/write pair of FIFOs to the user side.
+									      
+  And, since an RT-Linux interrupt function cannot write directly
+  to a FIFO, we implement an RT-Linux device in dsm_serial, and
+  this module provides the pair of threads to broker the data
+  between the device and the FIFOs.
+									      
+  This module also supports an IOCTL FIFO that the user can
+  send commands over.  Upon receipt of an ioctl command
+  this module does things like opening/closing fifos and
+  the serial port device, and also can send ioctl commands
+  on to the RT-Linux device.
+									      
+*/
+
+#ifndef DSM_SERIAL_FIFO_H
+#define DSM_SERIAL_FIFO_H
+
+#ifdef BOZO
+
+#ifndef __KERNEL__
+#include <sys/ioctl.h>
+#endif
+
+#endif
+
+#define DSM_SERIAL_FIFO_MAGIC 'F'
+
+#define DSMSER_OPEN _IOW(DSM_SERIAL_FIFO_MAGIC,0,int)
+#define DSMSER_CLOSE _IO(DSM_SERIAL_FIFO_MAGIC,1)
+
+#ifdef __KERNEL__
+
+#include <ioctl_fifo.h>
+
+struct dsm_serial_fifo_port {
+  char* inFifoName;
+  char* outFifoName;
+  char* devname;
+  int devfd;
+  int inFifoFd;
+  int outFifoFd;
+  void* in_thread_stack;
+  void* out_thread_stack;
+  pthread_t in_thread;
+  pthread_t out_thread;
+
+};
+
+struct dsm_serial_fifo_board {
+    struct dsm_serial_fifo_port* ports;
+    int numports;
+    struct ioctlHandle* ioctlhandle;
+};
+
+  
+
+#endif	/* __KERNEL__ */
+
+#endif
