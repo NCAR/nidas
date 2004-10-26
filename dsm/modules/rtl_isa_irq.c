73e823a35d75840de5f76d5754462c29dfd23335
Gordon Maclean <maclean@ucar.edu>
2004-10-26 16:27:15 +0000

    use IRQ_WIN_MODE to do interrupts in what Arcom calls Windows or Linux mode

--- dsm/modules/rtl_isa_irq.c
+++ dsm/modules/rtl_isa_irq.c
@@ -35,11 +35,14 @@
 /* Linux module includes... */
 #include <linux/autoconf.h>
 #include <asm/arch/irqs.h>
-#include <asm/arch/pxa-regs.h>
-#include <asm/arch/viper.h>
+// #include <asm/arch/pxa-regs.h>
+// #include <asm/arch/viper.h>
 #include <linux/module.h>
 
 #include <rtl_isa_irq.h>
+#include <dsm_viper.h>
+
+#define IRQ_WIN_MODE
 
 RTLINUX_MODULE(rtl_isa_irq);
 MODULE_AUTHOR("John Wasinger <wasinger@ucar.edu>");
@@ -47,7 +50,7 @@ MODULE_DESCRIPTION("RTLinux ISA IRQ de-multiplexing Module");
 
 #define NUM_ISA_IRQS VIPER_NUM_ISA_IRQS
 
-static unsigned int rtl_isa_irq_enabled_mask = 0;
+static unsigned char rtl_isa_irq_enabled_mask = 0;
 
 struct isrData {
     isa_irq_hander_t handler;
@@ -78,69 +81,116 @@ static int isa_irqs[NUM_ISA_IRQS] = { 3,4,5,6,7,10,11,12 };
 static rtl_spinlock_t irq_controller_lock;
 
 /* -- Utility --------------------------------------------------------- */
-static void rtl_mask_and_ack_isa_irq(unsigned int isa_irq_index)
+static inline void rtl_mask_and_ack_isa_irq(unsigned int isa_irq_index)
 {
-  rtl_isa_irq_enabled_mask &= ~(1 << isa_irq_index);
-  VIPER_ISA_IRQ_STATUS = 1 << isa_irq_index;
+    rtl_isa_irq_enabled_mask &= ~(1 << isa_irq_index);
+    VIPER_ISA_IRQ_STATUS = 1 << isa_irq_index;
+}
+static inline void rtl_ack_isa_irq(unsigned int isa_irq_index)
+{
+    VIPER_ISA_IRQ_STATUS = 1 << isa_irq_index;
 }
 /* -- Utility --------------------------------------------------------- */
-static void rtl_mask_isa_irq(unsigned int isa_irq_index)
+static inline void rtl_mask_isa_irq(unsigned int isa_irq_index)
 {
-  rtl_isa_irq_enabled_mask &= ~(1 << isa_irq_index);
+    rtl_isa_irq_enabled_mask &= ~(1 << isa_irq_index);
 }
 /* -- Utility --------------------------------------------------------- */
-static void rtl_unmask_isa_irq(unsigned int isa_irq_index)
+static inline void rtl_unmask_isa_irq(unsigned int isa_irq_index)
 {
-  rtl_isa_irq_enabled_mask |= (1 << isa_irq_index);
+    rtl_isa_irq_enabled_mask |= (1 << isa_irq_index);
 }
 /* -- RTLinux --------------------------------------------------------- */
 static unsigned int rtl_isa_irq_demux_isr (unsigned int irq, struct rtl_frame *regs)
 {
-  unsigned int irq_status;
-  int i, res = 0;
-  int imask = 1;
+    unsigned short irq_status;
+    int i, res = 0;
+    unsigned short imask = 1;
 
-  irq_status = VIPER_ISA_IRQ_STATUS & 0xFF;
-  // rtl_printf("isa irq=%d, status=%x, mask=%x\n",irq,irq_status,
-  // 	rtl_isa_irq_enabled_mask);
+#define CHECK_REENTRANT
+#define COUNT_SPURIOUS
+/*
+*/
 
-  if (!irq_status) {
-      // Don't do the rtl_global_pend_irq(irq), just return
-      rtl_hard_enable_irq(irq);
-      return 0;
-  }
+#ifdef CHECK_REENTRANT
+    static int running = 0;
+    static int n_reentrant = 0;
+#endif
 
-  /* Continue processing multiplexed ISA ISRs until all are processed... */
+#ifdef COUNT_SPURIOUS
+    static int spurious = 0;
+#endif
 
-  while (irq_status & rtl_isa_irq_enabled_mask) {
-    imask = 1;
-    rtl_spin_lock(&irq_controller_lock);
-    for (i=0; i < NUM_ISA_IRQS; i++) {
-      if (irq_status & imask) {
-        /* Ack the indicated ISR's register bit. */
-        rtl_mask_and_ack_isa_irq(i);
-
-        /* Call the indicated ISR. */
-	if (isa_isrs[i]) res =
-	    (*isa_isrs[i]->handler)(isa_irqs[i], isa_isrs[i]->callbackPtr,regs);
-
-	/* Enable (unmask) the indicated ISR again. */
-        rtl_unmask_isa_irq(i);
-
-	irq_status &= ~imask;
-	if (!irq_status) break;		// done
-      }
-      imask <<= 1;
+#ifdef CHECK_REENTRANT
+    if (running) {
+	if (!(n_reentrant++ % 1000))
+		rtl_printf("demux running, n_reentrant=%d\n",
+			n_reentrant);
     }
-    rtl_spin_unlock(&irq_controller_lock);
+    running = 1;
+#endif
+
     irq_status = VIPER_ISA_IRQ_STATUS & 0xFF;
     // rtl_printf("isa irq=%d, status=%x, mask=%x\n",irq,irq_status,
-  	// rtl_isa_irq_enabled_mask);
-  }
+    // 	rtl_isa_irq_enabled_mask);
 
-  rtl_global_pend_irq(irq);
-  rtl_hard_enable_irq(irq);
-  return res;
+    if (!irq_status) {
+#ifdef COUNT_SPURIOUS
+	if (!(spurious++ % 1000)) rtl_printf("spurious ISA interrupt #%d\n",
+		spurious);
+#endif
+#ifdef CHECK_REENTRANT
+	running = 0;
+#endif
+	rtl_hard_enable_irq(irq);
+	return 0;
+    }
+
+    /* Continue processing multiplexed ISA ISRs until all are processed... */
+
+    while (irq_status & rtl_isa_irq_enabled_mask) {
+	imask = 1;
+	rtl_spin_lock(&irq_controller_lock);
+	for (i=0; i < NUM_ISA_IRQS; i++) {
+	    if (irq_status & imask) {
+	
+		/* Ack-ing the indicated ISR's register bit
+		 * by doing
+		 * is not necessary (and seems to confuse the hardware)
+		 * if the VIPER_ISA_IRQ_ICR_AUTOCLR bit is set in the ICR.
+		 * It also does not seem to be necessary to maintain a
+		 * mask the current ISA interrupt here.
+		 * Since we don't enable the CPLD interrupt
+		 * until just before we return, only one execution of
+		 * this ISR will be happening at any one time.
+		 */
+
+		/* Call the indicated ISR. */
+		if (isa_isrs[i])
+		    res = (*isa_isrs[i]->handler)(isa_irqs[i],
+		    	isa_isrs[i]->callbackPtr,regs);
+
+#ifndef IRQ_WIN_MODE
+		VIPER_ISA_IRQ_STATUS = imask;
+#endif
+		irq_status &= ~imask;
+	    }
+	    if (!irq_status) break;		// done
+	    imask <<= 1;
+	}
+	rtl_spin_unlock(&irq_controller_lock);
+
+	irq_status = VIPER_ISA_IRQ_STATUS & 0xFF;
+    }
+
+#ifdef CHECK_REENTRANT
+    running = 0;
+#endif
+
+    // rtl_global_pend_irq(irq);
+    rtl_hard_enable_irq(irq);
+
+    return 0;
 }
 
 int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler, void* callbackPtr)
@@ -153,7 +203,7 @@ int rtl_request_isa_irq(unsigned int irq, isa_irq_hander_t handler, void* callba
     for (i = 0; i < NUM_ISA_IRQS; i++) {
 	if (isa_irqs[i] == irq) {
 
-	    rtl_printf("requesting isa irq=%d\n",irq);
+	    rtl_printf("requesting isa irq=%d, index=%d\n",irq,i);
 
 	    ret = -EBUSY;
 	    if (isa_isrs[i]) break;
@@ -220,10 +270,12 @@ void cleanup_module (void)
     /* release interrupt handler */
     rtl_free_irq(VIPER_CPLD_IRQ);
 }
+
 /* -- MODULE ---------------------------------------------------------- */
 int init_module (void)
 {
   int i;
+  int irq;
 
   rtl_spin_lock_init(&irq_controller_lock);
 
@@ -235,7 +287,37 @@ int init_module (void)
   }
 
   /* install a handler for the multiplexed interrupt */
-  if ( rtl_request_irq( VIPER_CPLD_IRQ, rtl_isa_irq_demux_isr ) < 0 )
+
+#ifdef IRQ_WIN_MODE
+  VIPER_ISA_IRQ_ICR = VIPER_ISA_IRQ_ICR_RETRIG | VIPER_ISA_IRQ_ICR_AUTOCLR;
+  set_GPIO_IRQ_edge(IRQ_TO_GPIO(VIPER_CPLD_IRQ), GPIO_RISING_EDGE);
+#else
+  /* LINUX_MODE (no auto-clear, no retrig) doesn't work -
+   *    simultaneous interrupts can be missed.
+   * RETRIG only also can miss simultaneous interrupts.
+   *    but seems to be better than no retrig.
+   * Also, according to the manual we should use BOTH_EDGE
+   * when not using auto-clear, but I then see 50% spurious
+   * interrupts.
+   * However, just using RISING_EDGE or FALLING_EDGE doesn't
+   * work at all.  It appears that the CPLD line toggles
+   * on either an interrupt or an interrupt acknowledge.
+   *
+   */
+  // VIPER_ISA_IRQ_ICR = VIPER_ISA_IRQ_ICR_LINUX_MODE;
+  // VIPER_ISA_IRQ_ICR = VIPER_ISA_IRQ_ICR_RETRIG;
+  VIPER_ISA_IRQ_ICR = 0;
+
+  // set_GPIO_IRQ_edge(IRQ_TO_GPIO(VIPER_CPLD_IRQ), GPIO_RISING_EDGE);
+  // set_GPIO_IRQ_edge(IRQ_TO_GPIO(VIPER_CPLD_IRQ), GPIO_FALLING_EDGE);
+  set_GPIO_IRQ_edge(IRQ_TO_GPIO(VIPER_CPLD_IRQ), GPIO_BOTH_EDGES);
+#endif
+
+  // rtl_printf("VIPER_ISA_IRQ_ICR = %x\n", VIPER_ISA_IRQ_ICR & 0xF);
+
+  irq = rtl_map_gpos_irq(VIPER_CPLD_IRQ);	/* does nothing on viper */
+
+  if ( rtl_request_irq(irq, rtl_isa_irq_demux_isr ) < 0 )
   {
     /* failed... */
     cleanup_module();
@@ -244,7 +326,7 @@ int init_module (void)
     return -EIO;
   }
   rtl_printf("(%s) %s:\t allocated IRQ at #%d\n",
-	     __FILE__, __FUNCTION__, VIPER_CPLD_IRQ);
+	     __FILE__, __FUNCTION__, irq);
 
   return 0;
 }
