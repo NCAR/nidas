a329f352e8b4c08561f498b6d738dd4d76a67324
Gordon Maclean <maclean@ucar.edu>
2004-11-09 17:14:48 +0000

    serial port checker

---
+++ dsm/src/ck_serial.cc
@@ -0,0 +1,127 @@
+/*
+ ********************************************************************
+    Copyright by the National Center for Atmospheric Research
+
+    $LastChangedDate: 2004-10-15 17:53:32 -0600 (Fri, 15 Oct 2004) $
+
+    $LastChangedRevision$
+
+    $LastChangedBy$
+
+    $HeadURL: http://orion/svn/hiaper/ads3/dsm/class/RTL_DSMSensor.h $
+ ********************************************************************
+
+    Test the serial ports.
+
+*/
+
+
+#include <PortSelectorTest.h>
+
+/* include file(s) for the sensor to be tested */
+#include <DSMSerialSensor.h>
+#include <SampleScanf.h>
+#include <dsm_serial.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace dsm;
+
+/**
+ * A little SampleClient for testing purposes.  Currently
+ * just prints out some fields from the Samples it receives.
+ */
+class TestSampleClient : public SampleClient {
+public:
+
+  bool receive(const Sample *s)
+  	throw(SampleParseException,atdUtil::IOException)
+{
+    cerr << dec << "timetag= " << s->getTimeTag() << " id= " << s->getId() <<
+    	" len=" << s->getDataLength();
+    float* data = (float*) s->getConstVoidDataPtr();
+
+    for (unsigned int i = 0; i < s->getDataLength(); i++)
+        std::cerr << ' ' << data[i];
+    std::cerr << std::endl;
+    return true;
+}
+
+};
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << "/dev/dsmserX ..." << endl;
+	return 1;
+    }
+
+    PortSelectorTest* handler = PortSelectorTest::createInstance();
+    handler->start();
+
+    /* Create the test SampleClient */
+
+    TestSampleClient test;
+    SampleScanf scanner;
+    scanner.setFormat("%*s%f%x");
+
+    scanner.addSampleClient(&test);
+
+    try {
+	for (int iarg = 1; iarg < argc; iarg++) {
+	    const char* dev = argv[iarg];
+
+
+/* Substitute your sensor instantiation here ********************** */
+	    DSMSerialSensor* sens = new DSMSerialSensor(dev);
+
+	    sens->setId(iarg);
+
+	    sens->setBaudRate(115200);
+	    std::cerr << "doing sens->open" << std::endl;
+	    sens->open(O_RDWR);
+	    struct dsm_serial_record_info recinfo;
+	    recinfo.sep[0] = '\n';
+	    recinfo.sepLen = 1;
+	    recinfo.atBOM = 0;
+	    recinfo.recordLen = 0;
+	    sens->ioctl(DSMSER_SET_RECORD_SEP,&recinfo,sizeof(recinfo));
+
+	    struct dsm_serial_prompt prompt;
+	    strcpy(prompt.str,"hitme\n");
+	    prompt.len = 6;
+	    prompt.rate = IRIG_1_HZ;
+	    sens->ioctl(DSMSER_SET_PROMPT,&prompt,sizeof(prompt));
+
+	    sens->ioctl(DSMSER_START_PROMPTER,(const void*)0,0);
+
+/* Add the SampleClient to the sensor. It will receive all the samples */
+
+
+	    sens->addSampleClient(&scanner);
+
+/* Now your sensor to the PortSelectorTestor, and you should start
+   to see samples being received by the SampleClient.
+   When the PortSelectorTestor is destroyed, it will call the
+   sensor destructors.
+*/
+
+	    handler->addSensorPort(sens);
+	}
+    }
+    catch (atdUtil::IOException& ioe) {
+      std::cerr << ioe.what() << std::endl;
+      throw atdUtil::Exception(ioe.what());
+    }
+
+    try {
+	handler->join();
+    }
+    catch(atdUtil::Exception& e) {
+        cerr << "join exception: " << e.what() << endl;
+	return 1;
+    }
+    delete handler;
+    return 0;
+}
