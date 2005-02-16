129c48e8e6c9abf34b16a313764b92a2896dfce1
Gordon Maclean <maclean@ucar.edu>
2005-02-16 05:47:35 +0000

    multiset that sorts samples by time

---
+++ dsm/class/SortedSampleSet.h
@@ -0,0 +1,34 @@
+//
+//              Copyright 2004 (C) by UCAR
+//
+
+#ifndef DSM_SORTEDSAMPLESET_H
+#define DSM_SORTEDSAMPLESET_H
+
+#include <Sample.h>
+
+#include <set>
+
+namespace dsm {
+
+class SampleTimetagComparator {
+public:
+  bool operator() (const Sample* x, const Sample *y) const {
+    // return true if x is less than y
+    int d = x->getTimeTag() - y->getTimeTag();
+    if (d < 0) {
+      if (d < -86100000) return false;	// x has rolled over back to 0
+      return true;
+    }
+    else {
+      if (d > 86100000) return true;	// y has rolled over back to 0
+      return false;
+    }
+  }
+};
+
+class SortedSampleSet: public std::multiset<const Sample*,SampleTimetagComparator> {};
+
+}
+
+#endif
