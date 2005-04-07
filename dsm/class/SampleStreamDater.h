55e529bd917ba7f660e69fe35fc051ba4492b90e
Gordon Maclean <maclean@ucar.edu>
2005-04-07 19:55:04 +0000

    provides full date tags of samples coming on a sample stream

---
+++ dsm/class/SampleStreamDater.h
@@ -0,0 +1,58 @@
+
+/*
+ ********************************************************************
+    Copyright by the National Center for Atmospheric Research
+
+    $LastChangedDate: 2004-10-15 17:53:32 -0600 (Fri, 15 Oct 2004) $
+
+    $LastChangedRevision$
+
+    $LastChangedBy$
+
+    $HeadURL: http://orion/svn/hiaper/ads3/dsm/class/RTL_DSMSensor.h $
+ ********************************************************************
+
+*/
+
+#ifndef DSM_SAMPLESTREAMDATER_H
+#define DSM_SAMPLESTREAMDATER_H
+
+#include <Sample.h>
+#include <irigclock.h>
+
+namespace dsm {
+
+class SampleStreamDater {
+public:
+    SampleStreamDater(int maxClockDiff = 180000):
+    	maxClockDiffMsec(maxClockDiff),clockTime(0) {}
+
+    const Sample* operator()(const Sample*);
+    Sample* operator()(Sample*);
+
+    dsm_sys_time_t getTime() const { return sampleTime; }
+
+    dsm_sys_time_t getClockTime() const { return clockTime; }
+
+    typedef enum { NO_CLOCK, OUT_OF_SPEC, OK } status_t;
+
+    status_t getStatus() const { return status; }
+private:
+
+    status_t computeTime(const Sample*);
+
+    int maxClockDiffMsec;
+
+    dsm_sys_time_t t0day;
+
+    dsm_sys_time_t sampleTime;
+
+    dsm_sys_time_t clockTime;
+
+    status_t status;
+
+};
+
+}
+
+#endif
