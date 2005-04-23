284864920000e03db23b5f59e0ef77dac06812fd
Grant Gray <gray@ucar.edu>
2005-04-23 17:43:10 +0000

    Good version.  Now has ability to block average after data acquisition.

--- dsm/modules/testa2dint.c
+++ dsm/modules/testa2dint.c
@@ -68,9 +68,7 @@ volatile long boot = 3;
 volatile long ncycles = 2;
 volatile long filter = 2;
 volatile long gain = 1;
-
-US   fix_a2d(US a);
-UC   bit_rvs(UC a);
+volatile long nsamp = 1;
 
 
 RTLINUX_MODULE(trya2d);
@@ -86,6 +84,8 @@ MODULE_PARM(filter, "1l");
 MODULE_PARM_DESC(filter, "Sets filter to be loaded: 2-10Kz, 1-1KHz, 0-500Hz");
 MODULE_PARM(gain, "1l");
 MODULE_PARM_DESC(gain, "Set gain codes to 10*gain");
+MODULE_PARM(nsamp, "1l");
+MODULE_PARM_DESC(nsamp, "Number of output samples to average");
 
 int init_module(void)
 {
@@ -131,26 +131,26 @@ void cleanup_module(void)
 
 	//Take CPLD out of AUTO mode 
 	FIFOCtl = FIFOCtlLoad;
-	outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-	outb(bit_rvs(FIFOCtlLoad), (UC *)isa_address);
+	outb((A2DIOFIFO), (UC *)chan_addr);
+	outb((FIFOCtlLoad), (UC *)isa_address);
 	rtl_printf("%s: Reset FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
 
 	// Read all the status lines
-	outb(bit_rvs(A2DSTATRD), (UC *)chan_addr);
+	outb((A2DSTATRD), (UC *)chan_addr);
 	for(i = 0; i < MAXA2DS; i++)
 	{
-		stat = fix_a2d(inw((US *)isa_address + i));
+		stat = (inw((US *)isa_address + i));
 		if(i == a2dmaster)rtl_printf("master stat = 0x%04X\n", stat);
 	}
 
 	// Read all the int lines
-	outb(bit_rvs(A2DIOSYSCTL), (UC *)chan_addr);
-	ints = bit_rvs(inb((UC *)isa_address));
+	outb((A2DIOSYSCTL), (UC *)chan_addr);
+	ints = (inb((UC *)isa_address));
 	rtl_printf("Int lines = 0x%02X\n", ints);
 	
 	// Shut down A/D's
-	outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
-	outw(fix_a2d(A2DABORT), (US *)isa_address);
+	outb((A2DCMNDWR), (UC *)chan_addr);
+	outw((A2DABORT), (US *)isa_address);
 
 	rtl_pthread_cancel(aAthread);
 	rtl_pthread_join(aAthread, NULL);
@@ -161,11 +161,15 @@ void cleanup_module(void)
 
 static void *TryA2D_thread(void *t)
 {
-	int j, k = 0, ctr = 0, ndata = 800, datavol[] = {40, 80, 800};
-	US stat, data[800], ints;
+	int i, j, k = 0, l, ctr = 0, ndata = 800, datavol[] = {40, 80, 800};
+	US stat, ints;
 	unsigned char intbit = 1;
+	US data[1024];
+	long avgs[MAXA2DS];
 
 	ndata = datavol[filter];
+
+	if(nsamp > ndata/MAXA2DS)nsamp=ndata/MAXA2DS;
 
 	rtl_printf("%s: In p-thread, FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
 	// Reset A/D's don't change configuration
@@ -174,10 +178,10 @@ static void *TryA2D_thread(void *t)
 
 // Start the conversions
 	rtl_printf("%s: Starting A/D's ", __FILE__);
-   	outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+   	outb((A2DCMNDWR), (UC *)chan_addr);
 	for(j = 0; j < MAXA2DS; j++)
 	{
-   		outw(fix_a2d(A2DREADDATA), (US *)isa_address + j);
+   		outw((A2DREADDATA), (US *)isa_address + j);
 		rtl_printf("%1d ", j);
 	}
 	rtl_usleep(10000); // Let them run a few milliseconds (10)
@@ -185,31 +189,31 @@ static void *TryA2D_thread(void *t)
 
 // Then do a soft reboot
 	rtl_printf("%s: Soft resetting A/D's ", __FILE__);
-	outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+	outb((A2DCMNDWR), (UC *)chan_addr);
 	for(j = 0; j < MAXA2DS; j++)
 	{
-		outw(fix_a2d(A2DABORT), (US *)isa_address + j);
+		outw((A2DABORT), (US *)isa_address + j);
 		rtl_printf("%1d ", j);
 	}
 	rtl_printf("\n");
 
 /* These didn't have the channel pointer set to A2DIOFIFO
 //Clear SYNC 
-	outb(bit_rvs(FIFOCtl & ~A2DSYNC), (UC *)isa_address);
-	outb(bit_rvs((FIFOCtl & ~A2DSYNC) | A2DSYNCCK), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl & ~A2DSYNC), (UC *)isa_address);
+	outb((FIFOCtl & ~A2DSYNC), (UC *)isa_address);
+	outb(((FIFOCtl & ~A2DSYNC) | A2DSYNCCK), (UC *)isa_address);
+	outb((FIFOCtl & ~A2DSYNC), (UC *)isa_address);
 
 //Set SYNC and stop the A2D's
-	outb(bit_rvs(FIFOCtl | A2DSYNC), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl | A2DSYNC | A2DSYNCCK), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl | A2DSYNC), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC | A2DSYNCCK), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC), (UC *)isa_address);
 */
-// Load FIFOCtl
+// Set FIFOCtl for load mode
 	// Point at the FIFO control word
-	//  and write FIFOCtl into register on CPLD
+	//  and write FIFOCtlLoad into register on CPLD
 	FIFOCtl = FIFOCtlLoad;
-	outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-	outb(bit_rvs(FIFOCtl), (UC *)isa_address);
+	outb((A2DIOFIFO), (UC *)chan_addr);
+	outb((FIFOCtl), (UC *)isa_address);
 
 // Set the gain codes to 10*gain
 	outb(A2DIOGAIN03, (UC *)chan_addr);
@@ -224,17 +228,17 @@ static void *TryA2D_thread(void *t)
 	if(master != 0)a2dmaster = (UC)master;
 
 	// Point at FIFOSTAT and set master A/D
-	outb(bit_rvs(A2DIOFIFOSTAT), (UC *)chan_addr);
-	outb(bit_rvs(a2dmaster), (UC *)isa_address);
+	outb((A2DIOFIFOSTAT), (UC *)chan_addr);
+	outb((a2dmaster), (UC *)isa_address);
 	rtl_printf("%s: Setting a2dmaster = 0x%02X\n", __FILE__, a2dmaster);
 
 	if(boot == 1)
 	{
 	// Configure A/D's from internal ROM
-		outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+		outb((A2DCMNDWR), (UC *)chan_addr);
 		for(j = 0; j < MAXA2DS; j++)
 		{
-			outw(fix_a2d(A2DBFIR), (US *)isa_address + j);
+			outw((A2DBFIR), (US *)isa_address + j);
 		}
 	}
 
@@ -242,14 +246,14 @@ static void *TryA2D_thread(void *t)
 	{
 
 		// Set A/D's to accept configuration data with errors masked
-		outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+		outb((A2DCMNDWR), (UC *)chan_addr);
 
 		for(k = 0; k < MAXA2DS; k++)
 		{
-			outw(fix_a2d(A2DWRCONFEM), (US *)isa_address + k);
+			outw((A2DWRCONFEM), (US *)isa_address + k);
 		}
 
-		outb(bit_rvs(A2DCONFWR), (UC *)chan_addr);
+		outb((A2DCONFWR), (UC *)chan_addr);
 
 		for(k = 0; k < MAXA2DS; k++)
 		{
@@ -262,7 +266,7 @@ static void *TryA2D_thread(void *t)
 					rtl_printf(" %04X", filterptr[j]);	
 				}
 #endif
-				outw(fix_a2d(filterptr[j]), (US *)isa_address + k);
+				outw((filterptr[j]), (US *)isa_address + k);
 				rtl_usleep(20);
 			}
 		}
@@ -271,11 +275,11 @@ static void *TryA2D_thread(void *t)
 	else if(boot == 3)
 	{
 		// Set A/D's to accept configuration data
-		outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+		outb((A2DCMNDWR), (UC *)chan_addr);
 
 		for(k = 0; k < MAXA2DS; k++)
 		{
-			outw(fix_a2d(A2DWRCONFIG), (US *)isa_address + k);
+			outw((A2DWRCONFIG), (US *)isa_address + k);
 		}
 
 
@@ -294,15 +298,15 @@ static void *TryA2D_thread(void *t)
 #endif
 				// Set channel pointer to Config write and
 				//   write out configuration word
-				outb(bit_rvs(A2DCONFWR), (UC *)chan_addr);
-				outw(fix_a2d(filterptr[j]), (US *)isa_address + k);
+				outb((A2DCONFWR), (UC *)chan_addr);
+				outw((filterptr[j]), (US *)isa_address + k);
 				rtl_usleep(30);
 	
 				// Set channel pointer to sysctl to read int lines
 				// Wait for interrupt bit to set
 			
-				outb(bit_rvs(A2DIOSYSCTL), (UC *)chan_addr);
-				while((bit_rvs(inb((UC *)isa_address)) & intbit) == 0)
+				outb((A2DIOSYSCTL), (UC *)chan_addr);
+				while(((inb((UC *)isa_address)) & intbit) == 0)
 				{
 					rtl_usleep(30);	
 					if(ctr++ > GAZILLION)
@@ -312,8 +316,8 @@ static void *TryA2D_thread(void *t)
 					}
 				}
 				// Read status word from target a/d to clear interrupt
-				outb(bit_rvs(A2DSTATRD), (UC *)chan_addr);
-				stat = fix_a2d(inw((US *)isa_address + k));
+				outb((A2DSTATRD), (UC *)chan_addr);
+				stat = (inw((US *)isa_address + k));
 
 				// Check status bits for errors
 				if(stat & A2DCRCERR)
@@ -329,7 +333,7 @@ byebye:
 			intbit <<= 1;
 		}
 		// Set channel pointer back to FIFO
-		outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
+		outb((A2DIOFIFO), (UC *)chan_addr);
 	}
 
 	else if(boot == 0)
@@ -338,10 +342,10 @@ byebye:
 	}
 
 	// Reset A/D's don't change configuration
-	outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+	outb((A2DCMNDWR), (UC *)chan_addr);
 	for(j = 0; j < MAXA2DS; j++)
 	{
-		outw(fix_a2d(A2DABORT), (US *)isa_address + j);
+		outw((A2DABORT), (US *)isa_address + j);
 	}
 
 	rtl_usleep(1000);
@@ -350,11 +354,11 @@ byebye:
 	rtl_printf("\n\n");
 
 	// Read status bits all a/d's
-	outb(bit_rvs(A2DSTATRD), (UC *)chan_addr); // Status read subchannel
+	outb((A2DSTATRD), (UC *)chan_addr); // Status read subchannel
 
 	for(j = 0; j < MAXA2DS; j++)
 	{
-		stat = fix_a2d(inw((US *)isa_address + j));
+		stat = (inw((US *)isa_address + j));
 		rtl_printf("Status: a/d%1d stat = 0x%04X\n", j, stat);
 	}
 //#endif
@@ -362,52 +366,52 @@ byebye:
 	rtl_usleep(DELAYNUM1);	// Give A/D's a chance to load
 
 // Start the conversions
-   	outb(bit_rvs(A2DCMNDWR), (UC *)chan_addr);
+   	outb((A2DCMNDWR), (UC *)chan_addr);
 	for(j = 0; j < MAXA2DS; j++)
 	{
-   		outw(fix_a2d(A2DREADDATA), (US *)isa_address + j);
+   		outw((A2DREADDATA), (US *)isa_address + j);
 	}
 
 // Do a read on status and data to clear interrupt on Master
 	// Point channel pointer at A/D Stat 
 	// and read the master a/d status
-	outb(bit_rvs(A2DSTATRD), (UC *)chan_addr);
+	outb((A2DSTATRD), (UC *)chan_addr);
 	rtl_printf("%s: Pre-start stat: \n", __FILE__); 
 	for(j = 0; j < MAXA2DS; j++)
 	{
-//		stat = fix_a2d(inw((US *)isa_address + a2dmaster));
-		stat = fix_a2d(inw((US *)isa_address + j));
+//		stat = (inw((US *)isa_address + a2dmaster));
+		stat = (inw((US *)isa_address + j));
 		rtl_printf("%01d:	0x%04x\n", j, stat);
 	}
 
 // Read the interrupt lines to see what's what
-	outb(bit_rvs(A2DIOSYSCTL), (UC *)chan_addr);
-	ints = bit_rvs(inb((UC *)isa_address));
+	outb((A2DIOSYSCTL), (UC *)chan_addr);
+	ints = (inb((UC *)isa_address));
 	rtl_printf("%s: Pre-start ints = 0x%02X\n", __FILE__, ints);
 
 //Set SYNC and stop the A2D's
 	rtl_printf("%s: Setting SYNC.  FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
-	outb(bit_rvs(FIFOCtl | A2DSYNC), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl | A2DSYNC | A2DSYNCCK), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl | A2DSYNC), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC | A2DSYNCCK), (UC *)isa_address);
+	outb((FIFOCtl | A2DSYNC), (UC *)isa_address);
 
 //Clear FIFO
 	FIFOCtl &= ~FIFOCLR;	// Make certain LSB of FIFOCtl is clear
 	rtl_printf("%s: Clearing FIFO .  FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
-	outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-	outb(bit_rvs(FIFOCtl), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl | FIFOCLR), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl), (UC *)isa_address);
+	outb((A2DIOFIFO), (UC *)chan_addr);
+	outb((FIFOCtl), (UC *)isa_address);
+	outb((FIFOCtl | FIFOCLR), (UC *)isa_address);
+	outb((FIFOCtl), (UC *)isa_address);
 
 // Read the FIFO status bits
 	rtl_printf("%s: Reading FIFO status. FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
-	outb(bit_rvs(A2DIOFIFOSTAT), (UC *)chan_addr);
-	stat = bit_rvs(inb((UC *)isa_address));
+	outb((A2DIOFIFOSTAT), (UC *)chan_addr);
+	stat = (inb((UC *)isa_address));
 
 // Read the interrupt lines to see what's what
 	rtl_printf("%s: Checking int lines. FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
-	outb(bit_rvs(A2DIOSYSCTL), (UC *)chan_addr);
-	ints = bit_rvs(inb((UC *)isa_address));
+	outb((A2DIOSYSCTL), (UC *)chan_addr);
+	ints = (inb((UC *)isa_address));
 	rtl_printf("%s: Pre-sync: ints = 0x%02X, fstat = 0x%02X\n", 
 				__FILE__, ints, stat);
 
@@ -415,52 +419,62 @@ byebye:
 	FIFOCtl = FIFOCtlRun;
 	rtl_printf("%s: Switching to RUN mode.  FIFOCtl = 0x%02X\n", 
 					__FILE__, FIFOCtl);
-	outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-	outb(bit_rvs(FIFOCtl), (UC *)isa_address);
+	outb((A2DIOFIFO), (UC *)chan_addr);
+	outb((FIFOCtl), (UC *)isa_address);
 
 // Clear SYNC and let A/D's run
 	rtl_printf("%s: Clearing SYNC. FIFOCtl = 0x%02X\n", __FILE__, FIFOCtl);
-	outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-	outb(bit_rvs(FIFOCtl & ~A2DSYNC), (UC *)isa_address);
-	outb(bit_rvs((FIFOCtl & ~A2DSYNC) | A2DSYNCCK), (UC *)isa_address);
-	outb(bit_rvs(FIFOCtl & ~A2DSYNC), (UC *)isa_address);
+	outb((A2DIOFIFO), (UC *)chan_addr);
+	outb((FIFOCtl & ~A2DSYNC), (UC *)isa_address);
+	outb(((FIFOCtl & ~A2DSYNC) | A2DSYNCCK), (UC *)isa_address);
+	outb((FIFOCtl & ~A2DSYNC), (UC *)isa_address);
 	rtl_printf("%s: Clearing the A/D SYNC line\n", __FILE__);
 
 // Take data!
  	
- 	for(j = 0; j < ncycles; j++)	
+ 	for(i = 0; i < ncycles; i++)	
 	{	
 		// Wait .01 seconds
 		rtl_usleep(7500);
 //		rtl_usleep(1000000);
 	
 		// Read the FIFO status bits
-		outb(bit_rvs(A2DIOFIFOSTAT), (UC *)chan_addr);
-		stat = bit_rvs(inb((UC *)isa_address));
+		outb((A2DIOFIFOSTAT), (UC *)chan_addr);
+		stat = (inb((UC *)isa_address));
 
 		// Read the interrupt lines to see what's what
-		outb(bit_rvs(A2DIOSYSCTL), (UC *)chan_addr);
-		ints = bit_rvs(inb((UC *)isa_address));
+		outb((A2DIOSYSCTL), (UC *)chan_addr);
+		ints = (inb((UC *)isa_address));
 
-		for(j = 0; j < 800; j++)
+		for(j = 0; j < ndata; j++)
 		{
 			// Read data values from FIFO
-			outb(bit_rvs(A2DIOFIFO), (UC *)chan_addr);
-			data[j] = fix_a2d(inw((US *)isa_address));
+			outb((A2DIOFIFO), (UC *)chan_addr);
+			data[j] = (inw((US *)isa_address));
 		}
+		for(j = 0; j < MAXA2DS; j++)avgs[j] = 0;	// Clear avg registers
+		for(j = 0; j < MAXA2DS; j++)
+		{
+			for(l = 0; l < nsamp; l++)
+			{
+				avgs[j] += (short)data[l*MAXA2DS +j];
+			}
+		}
+		for(j = 0; j < MAXA2DS; j++)avgs[j] /= nsamp;	//normalize
 
 		// Read the FIFO status bits
-		outb(bit_rvs(A2DIOFIFOSTAT), (UC *)chan_addr);
-		stat = bit_rvs(inb((UC *)isa_address));
+		outb((A2DIOFIFOSTAT), (UC *)chan_addr);
+		stat = (inb((UC *)isa_address));
 
 		if(k % 100 == 0)
 		{
 			rtl_printf("%5d:i-0x%02X,fs-0x%02X:", 
-				k, ints, stat);
+				k/100, ints, stat);
 
 			for(j = 0; j < MAXA2DS; j++)
 			{
-				rtl_printf("0x%04X ", data[j]);
+//				rtl_printf("0x%04X ", data[j]);
+				rtl_printf("0x%04X ", (US)(avgs[j] & 0xFFFF));
 			}
 			rtl_printf("\n");
 		}
@@ -470,36 +484,3 @@ byebye:
 	return 0;
 }
 
-US fix_a2d(US viper)
-{
-#ifndef FIX_A2D
-	return(viper);
-#else
-	return((viper &0xFF00) | (US)bit_rvs(viper & 0x00FF));
-#endif
-}
-
-
-/* 
-bit_rvs reverses the bits in a UC
-*/
-
-UC bit_rvs(UC incoming)
-{
-	UC masknum, locnum = incoming, revnum = 0;
-	int i;
-
-#ifndef FIX_A2D
-	return(incoming);
-#endif
-
-	masknum = 1;
-
-	for(i = 0; i < 8; i++)
-		{
-		revnum <<= 1;
-		if(locnum & masknum)revnum += 1;
-		masknum <<=1;
-		}
-	return(revnum);	
-}
