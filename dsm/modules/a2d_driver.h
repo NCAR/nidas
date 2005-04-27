f6bc5caadbbcd59d32d18bada51ec39b168c0f46
Gordon Maclean <maclean@ucar.edu>
2005-04-27 19:01:10 +0000

    moved driver-only defs to bottom

--- dsm/modules/a2d_driver.h
+++ dsm/modules/a2d_driver.h
@@ -25,6 +25,11 @@
 #include <sys/types.h>
 #endif
 
+/* This header is also included from user-side code that
+ * wants to get the values of the ioctl commands, and
+ * the definition of the structures.
+ */
+
 //Conveniences
 #ifndef		US
 #define		US	unsigned short
@@ -45,10 +50,74 @@
 #define		SC	char
 #endif
 
+#define	MAXA2DS			8	// Max A/D's per card
 #define	A2D_MAX_RATE	5000
 #define INTRP_RATE		100
-#define	HWFIFODEPTH		1024
 #define	RATERATIO		(A2D_MAX_RATE/INTRP_RATE)	
+
+
+// A/D Filter configuration file parameters
+#define CONFBLOCKS  12  // 12 blocks as described below
+#define CONFBLLEN	43	// 42 data words plus 1 CRCC (Confirmed by ADI)
+
+/* Structures that are passed via ioctls to/from this driver */
+typedef struct
+{
+	US a2dstat[MAXA2DS];
+	US fifostat;
+}A2D_GET;
+
+typedef struct 
+{
+	int	vcalx8;		// Calibration voltage: 
+				// 128=0, 48=-10, 208 = +10, .125 V/bit
+	US	status[MAXA2DS];	// A/D status flag
+//
+	int	gain[MAXA2DS];	// Gain settings 
+	int	Hz[MAXA2DS];		// Sample rate in Hz. 0 is off.
+	int	calset[MAXA2DS];	// Calibration flags
+	int	offset[MAXA2DS];	// Offset flags
+	float	norm[MAXA2DS];	// Normalization factors
+	US	master;		// Designates master A/D
+	US	ctr[MAXA2DS];		// Current value of ctr;
+	UL	ptr[MAXA2DS];		// Pointer offset from beginning of 
+	US	filter[CONFBLOCKS*CONFBLLEN+1];	// Filter data
+				// data summing buffer
+} A2D_SET;
+
+typedef struct 
+{
+	dsm_sample_time_t timestamp;	// timetag of sample 
+	dsm_sample_length_t size;		// number of bytes in data 
+  	SS data[RATERATIO*MAXA2DS]; 
+}A2DSAMPLE;
+
+
+/* Pick a character as the magic number of your driver.
+ * It isn't strictly necessary that it be distinct between
+ * all modules on the system, but is a good idea. With
+ * distinct magic numbers one can catch a user sending
+ * an ioctl to the wrong device.
+ */
+#define A2D_MAGIC 'A'
+
+/*
+ * The enumeration of IOCTLs that this driver supports.
+ * See pages 130-132 of Linux Device Driver's Manual 
+ */
+#define A2D_GET_IOCTL _IOR(A2D_MAGIC,0,A2D_GET)
+#define A2D_SET_IOCTL _IOW(A2D_MAGIC,1,A2D_SET)
+#define A2D_CAL_IOCTL _IOW(A2D_MAGIC,2,A2D_SET)
+#define A2D_RUN_IOCTL _IO(A2D_MAGIC,3)
+#define A2D_STOP_IOCTL _IO(A2D_MAGIC,4)
+#define A2D_RESTART_IOCTL _IO(A2D_MAGIC,5)
+
+
+
+#ifdef __RTCORE_KERNEL__
+/********  Start of definitions used by the driver module only **********/
+
+#define	HWFIFODEPTH		1024
 
 //Status/error
 #define A2DLOADOK	 	 0
@@ -70,7 +139,6 @@
 #define	A2DIOSEP		0x00000010	// Card addr separation
 #define	ARMISABASE		0xf7000000
 #define	A2DBASE			(A2DIOBASE+ARMISABASE)
-#define	MAXA2DS			8	// Max A/D's per card
 #define	A2DIOWIDTH		0x10	// Width of I/O space
 
 // I/O channels for the A/D card
@@ -140,76 +208,11 @@
 #define	FIFONOTFULL		0x0008  // FIFO not full
 #define INV1PPS			0x0010	// Inverted 1 PPS pulse
 
-// A/D Filter configuration file parameters
-#define CONFBLOCKS  12  // 12 blocks as described below
-#define CONFBLLEN	43	// 42 data words plus 1 CRCC (Confirmed by ADI)
-
-/* Structures that are passed via ioctls to/from this driver */
-typedef struct 
-{
-	dsm_sample_time_t timestamp;	// timetag of sample 
-	dsm_sample_length_t size;		// number of bytes in data 
-  	short data[RATERATIO*MAXA2DS]; 
-}A2DSAMPLE;
-
-typedef struct
-{
-	US a2dstat[8];
-	US fifostat;
-	// char c[RATERATIO][MAXA2DS];
-}A2D_GET;
-
-typedef struct 
-{
-	int	vcalx8;		// Calibration voltage: 
-				// 128=0, 48=-10, 208 = +10, .125 V/bit
-	US	status[8];	// A/D status flag
-//
-	int	gain[8];	// Gain settings 
-	int	Hz[8];		// Sample rate in Hz. 0 is off.
-	int	calset[8];	// Calibration flags
-	int	offset[8];	// Offset flags
-	float	norm[8];	// Normalization factors
-	US	master;		// Designates master A/D
-	US	ctr[8];		// Current value of ctr;
-	UL	ptr[8];		// Pointer offset from beginning of 
-	US	filter[CONFBLOCKS*CONFBLLEN+1];	// Filter data
-				// data summing buffer
-}A2D_SET;
-
 
 /*
 static int __init a2d_init();		//For Linux kernel
 static void __exit a2d_cleanup(void);	//For Linux kernel
 */
-
-/* This header is also included from user-side code that
- * wants to get the values of the ioctl commands, and
- * the definition of the structures.
- */
-
-/* Pick a character as the magic number of your driver.
- * It isn't strictly necessary that it be distinct between
- * all modules on the system, but is a good idea. With
- * distinct magic numbers one can catch a user sending
- * an ioctl to the wrong device.
- */
-#define A2D_MAGIC 'A'
-
-/*
- * The enumeration of IOCTLs that this driver supports.
- * See pages 130-132 of Linux Device Driver's Manual 
- */
-#define A2D_GET_IOCTL _IOR(A2D_MAGIC,0,A2D_GET)
-#define A2D_SET_IOCTL _IOW(A2D_MAGIC,1,A2D_SET)
-#define A2D_CAL_IOCTL _IOW(A2D_MAGIC,2,A2D_SET)
-#define A2D_RUN_IOCTL _IO(A2D_MAGIC,3)
-#define A2D_STOP_IOCTL _IO(A2D_MAGIC,4)
-#define A2D_RESTART_IOCTL _IO(A2D_MAGIC,5)
-
-// Now in source code #include <ioctl_fifo.h>
-
-#ifdef __RTCORE_KERNEL__
 
 #define	RTL_DEBUGIT(a)	rtl_printf("DEBUGIT %d\n", a)
 #define	DEBUGIT(a)	printf("DEBUGIT %d\n", a)
