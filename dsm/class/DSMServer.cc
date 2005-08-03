0b85132abaf6b5c9a5a4b395572e9bd34564320e
Gordon Maclean <maclean@ucar.edu>
2005-08-03 23:56:15 +0000

    changed stdin/out/err in daemon

--- dsm/class/DSMServer.cc
+++ dsm/class/DSMServer.cc
@@ -55,8 +55,8 @@ int DSMServer::main(int argc, char** argv) throw()
     else {
         logger = atdUtil::Logger::createInstance(
                 "dsm_server",LOG_CONS,LOG_LOCAL5);
-	// fork to background
-	if (daemon(1,1) < 0) {
+	// fork to background, send stdout/stderr to /dev/null
+	if (daemon(1,0) < 0) {
 	    logger->log(LOG_ERR, "Cannot run as daemon: %m");
 	    cerr << "daemon(1,1) failed: " << strerror(errno) << endl;
 	}
