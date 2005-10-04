f42457b001f258713f8c9af1680530357d4e8778
Gordon Maclean <maclean@ucar.edu>
2005-10-04 17:46:25 +0000

    reworked main and run methods

--- dsm/src/sync_server.cc
+++ dsm/src/sync_server.cc
@@ -30,23 +30,128 @@
 using namespace dsm;
 using namespace std;
 
-class Runstring {
-public:
-    Runstring(int argc, char** argv);
 
-    static void usage(const char* argv0);
+class SyncServer
+{
+public:
+
+    SyncServer();
+
+    int parseRunstring(int argc, char** argv) throw();
+
+    int run() throw();
+
+    void simLoop(SampleInputStream& input,SampleOutputStream* output,
+	SyncRecordGenerator& syncGen) throw(atdUtil::IOException);
+
+    void normLoop(SampleInputStream& input,SampleOutputStream* output,
+	SyncRecordGenerator& syncGen) throw(atdUtil::IOException);
+
+
+// static functions
+    static void sigAction(int sig, siginfo_t* siginfo, void* vptr);
+
+    static void setupSignals();
+
+    static int main(int argc, char** argv) throw();
+
+    static int usage(const char* argv0);
 
     static const int DEFAULT_PORT;
+
+private:
+
+    static bool interrupted;
+
     string xmlFileName;
+
     string dataFileName;
+
     int port;
+
     bool simulationMode;
 };
 
-const int Runstring::DEFAULT_PORT = 30001;
+int main(int argc, char** argv)
+{
+    return SyncServer::main(argc,argv);
+}
 
-Runstring::Runstring(int argc, char** argv):
+
+/* static */
+bool SyncServer::interrupted = false;
+
+const int SyncServer::DEFAULT_PORT = 30001;
+
+/* static */
+void SyncServer::sigAction(int sig, siginfo_t* siginfo, void* vptr) {
+    cerr <<
+    	"received signal " << strsignal(sig) << '(' << sig << ')' <<
+	", si_signo=" << (siginfo ? siginfo->si_signo : -1) <<
+	", si_errno=" << (siginfo ? siginfo->si_errno : -1) <<
+	", si_code=" << (siginfo ? siginfo->si_code : -1) << endl;
+                                                                                
+    switch(sig) {
+    case SIGHUP:
+    case SIGTERM:
+    case SIGINT:
+            SyncServer::interrupted = true;
+    break;
+    }
+}
+
+/* static */
+void SyncServer::setupSignals()
+{
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset,SIGHUP);
+    sigaddset(&sigset,SIGTERM);
+    sigaddset(&sigset,SIGINT);
+    sigprocmask(SIG_UNBLOCK,&sigset,(sigset_t*)0);
+                                                                                
+    struct sigaction act;
+    sigemptyset(&sigset);
+    act.sa_mask = sigset;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = SyncServer::sigAction;
+    sigaction(SIGHUP,&act,(struct sigaction *)0);
+    sigaction(SIGINT,&act,(struct sigaction *)0);
+    sigaction(SIGTERM,&act,(struct sigaction *)0);
+}
+
+/* static */
+int SyncServer::usage(const char* argv0)
+{
+    cerr << "\
+Usage: " << argv0 << " [-x xml_file] [-p port] raw_data_file\n\
+  -p port: sync record output socket port number: default=" << DEFAULT_PORT << "\n\
+  -s: simulation mode (pause a second before sending each sync record)\n\
+  -x xml_file (optional), default: $ADS3_CONFIG/projects/$ADS3_PROJECT/$ADS3_AIRCRAFT/flights/$ADS3_FLIGHT\n\
+" << endl;
+    return 1;
+}
+
+/* static */
+int SyncServer::main(int argc, char** argv) throw()
+{
+    setupSignals();
+
+    SyncServer sync;
+
+    int res;
+    
+    if ((res = sync.parseRunstring(argc,argv)) != 0) return res;
+    return sync.run();
+}
+
+
+SyncServer::SyncServer():
 	port(DEFAULT_PORT),simulationMode(false)
+{
+}
+
+int SyncServer::parseRunstring(int argc, char** argv) throw()
 {
     extern char *optarg;       /* set by getopt() */
     extern int optind;       /* "  "     "     */
@@ -78,76 +183,12 @@ Runstring::Runstring(int argc, char** argv):
     }
     if (optind == argc - 1) dataFileName = argv[optind++];
     if (dataFileName.length() == 0) usage(argv[0]);
-    if (optind != argc) usage(argv[0]);
+    if (optind != argc) return usage(argv[0]);
+    return 0;
 }
 
-void Runstring::usage(const char* argv0)
+int SyncServer::run() throw()
 {
-    cerr << "\
-Usage: " << argv0 << " [-x xml_file] [-p port] raw_data_file\n\
-  -p port: sync record output socket port number: default=" << DEFAULT_PORT << "\n\
-  -s: simulation mode (pause a second before sending each sync record)\n\
-  -x xml_file (optional), default: $ADS3_CONFIG/projects/$ADS3_PROJECT/$ADS3_AIRCRAFT/flights/$ADS3_FLIGHT\n\
-" << endl;
-    exit(1);
-}
-
-class SyncServer
-{
-public:
-
-    static void sigAction(int sig, siginfo_t* siginfo, void* vptr);
-
-    static void setupSignals();
-
-    static int main(int argc, char** argv);
-
-    static bool interrupted;
-};
-
-bool SyncServer::interrupted = false;
-
-void SyncServer::sigAction(int sig, siginfo_t* siginfo, void* vptr) {
-    cerr <<
-    	"received signal " << strsignal(sig) << '(' << sig << ')' <<
-	", si_signo=" << (siginfo ? siginfo->si_signo : -1) <<
-	", si_errno=" << (siginfo ? siginfo->si_errno : -1) <<
-	", si_code=" << (siginfo ? siginfo->si_code : -1) << endl;
-                                                                                
-    switch(sig) {
-    case SIGHUP:
-    case SIGTERM:
-    case SIGINT:
-            SyncServer::interrupted = true;
-    break;
-    }
-}
-
-void SyncServer::setupSignals()
-{
-    sigset_t sigset;
-    sigemptyset(&sigset);
-    sigaddset(&sigset,SIGHUP);
-    sigaddset(&sigset,SIGTERM);
-    sigaddset(&sigset,SIGINT);
-    sigprocmask(SIG_UNBLOCK,&sigset,(sigset_t*)0);
-                                                                                
-    struct sigaction act;
-    sigemptyset(&sigset);
-    act.sa_mask = sigset;
-    act.sa_flags = SA_SIGINFO;
-    act.sa_sigaction = SyncServer::sigAction;
-    sigaction(SIGHUP,&act,(struct sigaction *)0);
-    sigaction(SIGINT,&act,(struct sigaction *)0);
-    sigaction(SIGTERM,&act,(struct sigaction *)0);
-}
-
-int SyncServer::main(int argc, char** argv)
-{
-
-    Runstring rstr(argc,argv);
-
-    setupSignals();
 
     IOChannel* iochan = 0;
 
@@ -168,16 +209,19 @@ int SyncServer::main(int argc, char** argv)
 	fset->setStartTime(start);
 	fset->setEndTime(end);
 #else
-	fset->setFileName(rstr.dataFileName);
+	fset->setFileName(dataFileName);
 #endif
 
 	SortedSampleInputStream input(iochan,2000);	// SortedSampleStream owns the iochan ptr.
+	// Block while waiting for heapSize to become less than heapMax.
+	input.setHeapBlock(true);
+	input.setHeapMax(10000000);
 	input.init();
 
 	auto_ptr<Project> project;
 
 	auto_ptr<xercesc::DOMDocument> doc(
-		DSMEngine::parseXMLConfigFile(rstr.xmlFileName));
+		DSMEngine::parseXMLConfigFile(xmlFileName));
 
 	project = auto_ptr<Project>(Project::getInstance());
 	project->fromDOMElement(doc->getDocumentElement());
@@ -213,73 +257,14 @@ int SyncServer::main(int argc, char** argv)
 	SyncRecordGenerator syncGen;
 	syncGen.connect(&input);
 
-	dsm::ServerSocket* servSock = new dsm::ServerSocket(rstr.port);
+	dsm::ServerSocket* servSock = new dsm::ServerSocket(port);
 	SampleOutputStream* output = new SampleOutputStream(servSock);
 
 	output->connect();
 	syncGen.connected(output);
 
-	bool simulationMode = rstr.simulationMode;
-
-	struct timespec nsleep;
-	dsm_time_t nextDataSec = 0;
-	dsm_time_t timeOffset = 0;
-	int granularity = USECS_PER_SEC / 10;	// how often to wake up
-	bool first = true;
-	try {
-	    for (;;) {
-	        if (!output->getIOStream()) break;	 // check for disconn
-		if (interrupted) break;
-
-		Sample* samp = input.readSample();
-
-		dsm_time_t tt = samp->getTimeTag();
-		// dsm_sample_id_t sampid = samp->getId();
-		if (simulationMode) {
-#ifdef DEBUG
-		    cerr << "tt=" << tt/USECS_PER_SEC << '.' <<
-			setfill('0') << setw(3) << (tt % USECS_PER_SEC) / 1000 <<
-			" nextDataSec=" <<  nextDataSec/USECS_PER_SEC << '.' <<
-			setfill('0') << setw(3) << ( nextDataSec % USECS_PER_SEC) / 1000 << endl;
-#endif
-		    while (tt > nextDataSec) {
-			dsm_time_t tnow = getSystemTime();
-			long tsleep = granularity - (tnow % granularity);
-			// cerr << "tsleep=" << tsleep << endl;
-			nsleep.tv_sec = tsleep / USECS_PER_SEC;
-			nsleep.tv_nsec = (tsleep % USECS_PER_SEC) * 1000;
-			if (nanosleep(&nsleep,0) < 0 && errno == EINTR) break;
-
-			tnow += tsleep;
-
-			// time diff between now and data timetags
-			if (timeOffset == 0) {	
-			    timeOffset = tnow - tt;
-			    timeOffset -= (timeOffset % granularity);
-			}
-			nextDataSec = tnow - timeOffset;
-		    }
-		}
-		if (first) {
-		    syncGen.sendHeader(tt,output->getIOStream());
-		    first = false;
-		}
-		input.distribute(samp);
-		samp->freeReference();
-	    }
-	}
-	catch (atdUtil::EOFException& eof) {
-	    cerr << eof.what() << endl;
-	    syncGen.disconnect(&input);
-	    syncGen.disconnected(output);
-	    throw eof;
-	}
-	catch (atdUtil::IOException& ioe) {
-	    cerr << ioe.what() << endl;
-	    syncGen.disconnect(&input);
-	    syncGen.disconnected(output);
-	    throw ioe;
-	}
+	if (true || simulationMode) simLoop(input,output,syncGen);
+	else normLoop(input,output,syncGen);
     }
     catch (atdUtil::EOFException& eof) {
         cerr << eof.what() << endl;
@@ -304,7 +289,91 @@ int SyncServer::main(int argc, char** argv)
     return 0;
 }
 
-int main(int argc, char** argv)
+void SyncServer::simLoop(SampleInputStream& input,SampleOutputStream* output,
+	SyncRecordGenerator& syncGen) throw(atdUtil::IOException)
 {
-    return SyncServer::main(argc,argv);
+
+    struct timespec nsleep;
+    dsm_time_t nextDataSec = 0;
+    dsm_time_t timeOffset = 0;
+    int granularity = USECS_PER_SEC / 10;	// how often to wake up
+    bool first = true;
+    try {
+	for (;;) {
+	    if (!output->getIOStream()) break;	 // check for disconn
+	    if (interrupted) break;
+
+	    Sample* samp = input.readSample();
+
+	    dsm_time_t tt = samp->getTimeTag();
+	    // dsm_sample_id_t sampid = samp->getId();
+	    if (simulationMode) {
+#ifdef DEBUG
+		cerr << "tt=" << tt/USECS_PER_SEC << '.' <<
+		    setfill('0') << setw(3) << (tt % USECS_PER_SEC) / 1000 <<
+		    " nextDataSec=" <<  nextDataSec/USECS_PER_SEC << '.' <<
+		    setfill('0') << setw(3) << ( nextDataSec % USECS_PER_SEC) / 1000 << endl;
+#endif
+		while (tt > nextDataSec) {
+		    dsm_time_t tnow = getSystemTime();
+		    long tsleep = granularity - (tnow % granularity);
+		    // cerr << "tsleep=" << tsleep << endl;
+		    nsleep.tv_sec = tsleep / USECS_PER_SEC;
+		    nsleep.tv_nsec = (tsleep % USECS_PER_SEC) * 1000;
+		    if (nanosleep(&nsleep,0) < 0 && errno == EINTR) break;
+
+		    tnow += tsleep;
+
+		    // time diff between now and data timetags
+		    if (timeOffset == 0) {	
+			timeOffset = tnow - tt;
+			timeOffset -= (timeOffset % granularity);
+		    }
+		    nextDataSec = tnow - timeOffset;
+		}
+	    }
+	    if (first) {
+		syncGen.sendHeader(tt,output->getIOStream());
+		first = false;
+	    }
+	    input.distribute(samp);
+	    samp->freeReference();
+	}
+    }
+    catch (atdUtil::EOFException& eof) {
+	cerr << eof.what() << endl;
+	syncGen.disconnect(&input);
+	syncGen.disconnected(output);
+	throw eof;
+    }
+    catch (atdUtil::IOException& ioe) {
+	cerr << ioe.what() << endl;
+	syncGen.disconnect(&input);
+	syncGen.disconnected(output);
+	throw ioe;
+    }
+}
+
+void SyncServer::normLoop(SampleInputStream& input,SampleOutputStream* output,
+	SyncRecordGenerator& syncGen) throw(atdUtil::IOException)
+{
+
+    try {
+	for (;;) {
+	    if (interrupted) break;
+	    input.readSamples();
+	}
+    }
+    catch (atdUtil::EOFException& eof) {
+	cerr << eof.what() << endl;
+	syncGen.disconnect(&input);
+	syncGen.disconnected(output);
+	throw eof;
+    }
+    catch (atdUtil::IOException& ioe) {
+	cerr << ioe.what() << endl;
+	syncGen.disconnect(&input);
+	syncGen.disconnected(output);
+	throw ioe;
+    }
 }
