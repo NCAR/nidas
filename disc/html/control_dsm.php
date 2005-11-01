2bf9149b2ed8943b3eb654c4244f56e3a97f5a90
John Wasinger <wasinger@ucar.edu>
2005-11-01 18:45:20 +0000

    Resized interface page.  Now dynamically updates the time and status.

--- disc/html/control_dsm.php
+++ disc/html/control_dsm.php
@@ -1,30 +1,30 @@
-<link href='index.css' rel='stylesheet' type='text/css'>
-
 <?php
 include_once('utils/utils.php');
 
-if (empty($_POST['dsm']))
-  exit('<h5>You need to select some DSM(s)</h5>');
+echo "<script>window.parent.selectedDsm=''; window.parent.recvResp('";
+
+If (empty($_POST['dsm']))
+  exit("<h5>You need to select some DSM(s)</h5>')</script>");
 
 $action = $_POST['act'];
 if (empty($action))
-  exit('<h5>You need to select an action</h5>');
-
-if ($action == 'calibrate analog')
-  exit('<h5>not yet implemented...</h5>');
+  exit("<h5>You need to select an action</h5>')</script>");
 
 if (sizeof($_POST['dsm']) > 1)
-  echo '<h4>Results from the XMLRPC calls</h4>';
+  echo "<h4>Results from the XMLRPC calls</h4>";
 else
-  echo '<h4>Results from the XMLRPC call</h4>';
+  echo "<h4>Results from the XMLRPC call</h4>";
 
 foreach ($_POST['dsm'] as $dsm) {
   echo "$action $dsm... ";
 
   $result = xu_rpc_http_concise( array( 'method' => $action,
-                                        'args' => '',
-                                        'host' => $dsm,
-       'uri' => '/RPC2', 'port' => '30003', 'debug' => '0', 'output' => 'xmlrpc'));
+                                        'args'   => '',
+                                        'host'   => $dsm,
+                                        'uri'    => '/RPC2',
+                                        'port'   => '30003',
+                                        'debug'  => '0',
+                                        'output' => 'xmlrpc' ));
 
   if (empty($result))
     echo "(no response)";
@@ -40,4 +40,5 @@ foreach ($_POST['dsm'] as $dsm) {
 
   echo "<br>";
 }
+echo "');</script>";
 ?>
---
+++ disc/html/index.php
@@ -0,0 +1,128 @@
+<html>
+  <head>
+  <title>Aircraft Web Interface</title>
+<!-- ----------------------------------------------------------------------- -->
+<!-- This script updates the DSM time displays by fetching the time from     -->
+<!-- the XML based printStatus messages being multicast to the server.       -->
+<!-- ----------------------------------------------------------------------- -->
+    <script type='text/javascript' src='jsolait/init.js'></script>
+    <script type='text/javascript' src='periodic.js'></script>
+    <link href='index.css' rel='stylesheet' type='text/css'>
+  </head>
+
+<?php
+include_once('utils/utils.php');
+
+//-- ----------------------------------------------------------------------- -->
+//-- Query the dsm_server for a list of DSM names and locations.             -->
+//-- ----------------------------------------------------------------------- -->
+$dsmList = xu_rpc_http_concise( array( 'method' => 'GetDsmList',
+                                       'args'   => '',
+                                       'host'   => 'localhost',
+                                       'uri'    => '/RPC2',
+                                       'port'   => '30002',
+                                       'debug'  =>  '0',
+                                       'output' => 'xmlrpc' ));
+
+// TODO - see if xu_rpc_http_concise provides any status variables to test here instead.
+if ($dsmList == "")
+  exit("<h5>DSM server not responding!</h5>");
+?>
+
+
+<script>
+<!-- ----------------------------------------------------------------------- -->
+<!-- The Hidden iframe below will contain javascript that will call recvResp.-->
+<!-- ----------------------------------------------------------------------- -->
+var xmlrpc = importModule("xmlrpc");
+GetClocks = new xmlrpc.XMLRPCMethod('xmlrpc.php?port=30005&method=GetClocks', '');
+
+GetStatus = new Array();
+<?php foreach ($dsmList as $key => $val) { ?>
+GetStatus['<?=$key?>'] =
+  new xmlrpc.XMLRPCMethod('xmlrpc.php?port=30005&method=GetStatus&args=<?=$key?>', '');
+<?php } ?>
+
+var selectedDsm = '';
+function recvResp(content) {
+  document.getElementById('stat').innerHTML = content;
+}
+function selectDsm(content) {
+  selectedDsm = content;
+  if (selectedDsm)
+    recvResp( GetStatus[selectedDsm]() );
+  else
+    recvResp( "" );
+}
+function clicker(that) {
+  if (that.selectedIndex+1 > 0)
+    return that.options[that.selectedIndex].id;
+  else
+    return '';
+}
+</script>
+
+
+<!-- ----------------------------------------------------------------------- -->
+<!-- Table layout:  +--------------------------+--------------------------+  -->
+<!--                |                          |                          |  -->
+<!--                | control                  | image                    |  -->
+<!--                |                          |                          |  -->
+<!--                +--------------------------+--------------------------+  -->
+<!--                |                                                     |  -->
+<!--                | status / response                                   |  -->
+<!--                |                                                     |  -->
+<!--                +-----------------------------------------------------+  -->
+<!-- ----------------------------------------------------------------------- -->
+<table border><tbody align='center' valign='top'>
+<tr>
+<td>
+<!-- ----------------------------------------------------------------------- -->
+<!-- This form provides a selection of DSMs to control.  There are two       -->
+<!-- steps in this form: the selection of the dsm and the choice of action.  -->
+<!--                                                                         -->
+<!-- TODO - higlight the option red when in warning...                       -->
+<!--  <option value='YYY'                        >YYY (----/--/-- --:--:--)  -->
+<!--  <option value='XXX' style='color: #ff0000;'>YYY (----/--/-- --:--:--)  -->
+<!-- ----------------------------------------------------------------------- -->
+
+<form action='control_dsm.php' method='POST' target='scriptframe'>
+
+  <select name='dsm[]' size="4" multiple="multiple"
+         onclick='selectDsm(clicker(this))'>
+    <?php foreach ($dsmList as $key => $val) { ?>
+    <option value='<?=$key?>' id='<?=$key?>' label='<?=str_pad($val,11, '_')?>'>
+       <?=str_pad($val,11, '_')?> (---- -- -- --:--:--)</option>
+    <?php } ?>
+  </select><p>
+
+  action:&nbsp
+  <select name='act'>
+    <option value='' selected>
+    <option>Start</option>
+    <option>Stop</option>
+    <option>Restart</option>
+    <option>Quit</option>
+<!--     <option>calibrate analog</option> -->
+  </select>
+
+  &nbsp
+  <input type=submit value='submit &raquo;' class='button'
+         onclick="selectedDsm=''; recvResp('working...')">
+
+</form>
+</td>
+<td>
+<img src='GV-top-456x402.jpg'>
+</td>
+</tr>
+<tr>
+<!-- ----------------------------------------------------------------------- -->
+<!-- The status table is displayed in this row.  The Hidden iframe is used   -->
+<!-- to receive javascript commands that refreash this row as well.          -->
+<!-- ----------------------------------------------------------------------- -->
+<iframe name='scriptframe' width=0 height=0 frameborder=0 src=''</iframe>
+<td id='stat' colspan=2></td>
+</tr>
+</tbody></table>
+</html>
