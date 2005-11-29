18af78d168af6d00613e8925b46b5fd12a9f15a0
Gordon Maclean <maclean@ucar.edu>
2005-11-29 18:48:17 +0000

    doxy comments

--- dsm/class/SyncRecordVariable.h
+++ dsm/class/SyncRecordVariable.h
@@ -25,10 +25,26 @@ namespace dsm {
 class SyncRecordVariable : public Variable
 {
 public:
+    /**
+     * Get the index into the sync record of the 
+     * first value for this variable.
+     */
     int getSyncRecOffset() const { return offset; }
+
     void setSyncRecOffset(int val) { offset = val; }
 
+    /**
+     * Get the index into the sync record of the lag
+     * value for this variable.
+     * The lag value is the fractional number of micro-seconds
+     * to add to the sync record time to get the time tag
+     * of the first value of this variable in the sync record.
+     * One would use it like so:
+     * float lag = sync_rec[var->getLagOffset()];
+     * if (!isnan(lag)) tt += lag;
+     */
     int getLagOffset() const { return lagOffset; }
+
     void setLagOffset(int val) { lagOffset = val; }
 
 protected:
