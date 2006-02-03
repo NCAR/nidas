4475c799a531628eff4aa9db3a50bec917c99c23
Gordon Maclean <maclean@ucar.edu>
2006-02-03 21:23:30 +0000

    port changes

--- dsm/class/Datagrams.h
+++ dsm/class/Datagrams.h
@@ -18,10 +18,11 @@
 
 #define DSM_MULTICAST_PORT        30000
 #define DSM_MULTICAST_STATUS_PORT 30001
-#define ADS_XMLRPC_PORT           30002 // DSMServerIntf.cc
-#define DSM_XMLRPC_PORT           30003 // DSMEngineIntf.cc
-#define DIR_XMLRPC_PORT           30004 // ADSDirectorIntf.cc
-#define ADS_XMLRPC_STATUS_PORT    30005 // for status_listener.cc
+#define RSERIAL_PORT    	  30002 // DSMServerIntf.cc
+#define ADS_XMLRPC_PORT           30003 // DSMServerIntf.cc
+#define DSM_XMLRPC_PORT           30004 // DSMEngineIntf.cc
+#define DIR_XMLRPC_PORT           30005 // ADSDirectorIntf.cc
+#define ADS_XMLRPC_STATUS_PORT    30006 // for status_listener.cc
 
 #define DSM_MULTICAST_ADDR "239.0.0.10"
 
