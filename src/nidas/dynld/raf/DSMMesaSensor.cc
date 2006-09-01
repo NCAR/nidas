69a24fa1e901bbcb91414100114c96d8ab67f529
Chris Webster <cjw@ucar.edu>
2006-09-01 22:14:52 +0000

    Anything IO user space code.  Covering APN-232, Pulse Counting and 260X.

--- src/nidas/dynld/raf/DSMMesaSensor.cc
+++ src/nidas/dynld/raf/DSMMesaSensor.cc
@@ -18,6 +18,8 @@
 #include <nidas/dynld/raf/DSMMesaSensor.h>
 #include <nidas/core/RTL_IODevice.h>
 
+#include <nidas/util/Logger.h>
+
 #include <asm/ioctls.h>
 #include <iostream>
 #include <sstream>
@@ -32,104 +34,266 @@ NIDAS_CREATOR_FUNCTION_NS(raf,DSMMesaSensor)
 DSMMesaSensor::DSMMesaSensor()
 {
   cerr << __PRETTY_FUNCTION__ << endl;
+
+  memset(&radar_info, 0, sizeof(radar_info));
+  memset(&counter_info, 0, sizeof(counter_info));
+  memset(&p260x_info, 0, sizeof(p260x_info));
 }
 
-DSMMesaSensor::~DSMMesaSensor() {
+DSMMesaSensor::~DSMMesaSensor()
+{
 }
 
 IODevice* DSMMesaSensor::buildIODevice() throw(n_u::IOException)
 {
-    return new RTL_IODevice();
+  return new RTL_IODevice();
 }
 
 SampleScanner* DSMMesaSensor::buildSampleScanner()
 {
-    return new SampleScanner();
+  return new SampleScanner();
+}
+
+
+/*---------------------------------------------------------------------------*/
+void DSMMesaSensor::open(int flags) throw(n_u::IOException)
+{
+  DSMSensor::open(flags);
+
+  cerr << __PRETTY_FUNCTION__ << "open-begin" << endl;
+
+  if (sendFPGACodeToDriver() == false)
+  {
+    close();
+    return;
+  }
+
+  // Send down rates.  They are initialized in init()
+  if (counter_info.rate > 0)
+    ioctl(COUNTERS_SET, &counter_info, sizeof(counter_info));
+
+  if (p260x_info.rate > 0)
+    ioctl(PMS260X_SET, &p260x_info, sizeof(p260x_info));
+
+  if (radar_info.rate > 0)
+    ioctl(RADAR_SET, &radar_info, sizeof(radar_info));
+
+  cerr << __PRETTY_FUNCTION__ << "open-end" << endl;
 }
 
 /*---------------------------------------------------------------------------*/
-/* Function: readbytesfromfile
-   Purpose: Read the specified number of bytes from the specified offset in
-         the specified open file.
-   Used by: Local functions.
-   Returns: The number of bytes read.
-   Notes:
-     -The file position is not restored to its original value.
-     -Don't try to read 0 bytes.
-*/
-
-static size_t readbytesfromfile(FILE *f, long fromoffset, size_t numbytes,
-                                unsigned char *bufptr)
+bool DSMMesaSensor::process(const Sample * samp, list<const Sample *>& results)
+        throw()
 {
-  if(fseek(f, fromoffset, SEEK_SET) != 0)
+  // pointer to 16 bit raw data
+  const unsigned short * sp =
+		(const unsigned short *)samp->getConstVoidDataPtr();
+
+  // We send up the SampleID as part of the data packet.
+  short sampleID = *sp++;
+
+  unsigned int nvalues = samp->getDataByteLength() / sizeof(short) - 1;
+
+  SampleT<float>* osamp = getSample<float>(nvalues);
+  osamp->setTimeTag(samp->getTimeTag());
+  osamp->setId(sampleID + getId());
+
+  float * dptr = osamp->getDataPtr();
+
+  if (sampleID == ID_260X)
+  {
+    // these values must correspond to the sequence of
+    // <variable> tags in the <sample> for this sensor.
+    *dptr++ = sp[0];	// Strobes.
+
+    for (size_t iout = 0; iout < 8; ++iout)
+      *dptr++ = sp[iout+1];
+
+    for (size_t iout = 0; iout < TWO_SIXTY_BINS; ++iout)
+      *dptr++ = sp[iout+8+1];
+  }
+  else
+  {
+    unsigned int nvariables = 0;
+
+    switch (sampleID)
+    {
+      case ID_COUNTERS:
+        nvariables = counter_info.nChannels;
+        break;
+      case ID_RADAR:
+        nvariables = radar_info.nChannels;
+        break;
+      case ID_DIG_IN:
+        break;
+      case ID_DIG_OUT:
+        break;
+    }
+
+    if (nvalues != nvariables)
+    {
+      n_u::Logger::getInstance()->log(LOG_ERR,
+	"Mesa sample id %d (dsm=%d,sensor=%d): Expected %d raw values, got %d",
+	samp->getId(),GET_DSM_ID(samp->getId()),GET_SHORT_ID(samp->getId()),
+	nvariables, nvalues);
+      return false;
+    }
+
+    for (size_t i = 0; i < nvalues; ++i)
+      *dptr++ = *sp++;
+  }
+  results.push_back(osamp);
+  return true;
+}
+
+/*---------------------------------------------------------------------------*/
+void DSMMesaSensor::fromDOMElement(const xercesc::DOMElement * node)
+	throw(n_u::InvalidParameterException)
+{
+  DSMSensor::fromDOMElement(node);
+
+  int rate, i;
+  dsm_sample_id_t sampleId;
+
+  for (SampleTagIterator ti = getSampleTagIterator(); ti.hasNext(); )
+  {
+    const SampleTag* tag = ti.next();
+    rate = irigClockRateToEnum((int)tag->getRate());
+    sampleId = tag->getId();
+
+// cerr << "init, dsm id=" << tag->getDSMId() << ", sensor id=" << tag->getSensorId() << ", sample id=" << tag->getSampleId() << ", rate=" << tag->getRate() << endl;
+
+    switch (tag->getSampleId())
+    {
+      case ID_COUNTERS:
+        i = 0;
+        for (	VariableIterator vi = tag->getVariableIterator();
+		i < N_COUNTERS && vi.hasNext();
+		++i, vi.next())
+        {
+          ++counter_info.nChannels;
+          counter_info.rate = (int)tag->getRate();
+        }
+        break;
+      case ID_DIG_IN:
+        cerr << "DSMMesaSensor::init() DIG_IN not implemented yet." << endl;
+        break;
+      case ID_DIG_OUT:
+        cerr << "DSMMesaSensor::init() DIG_OUT not implemented yet." << endl;
+        break;
+      case ID_260X:
+        p260x_info.nChannels = 1;
+        p260x_info.rate = (int)tag->getRate();
+        break;
+      case ID_RADAR:
+        radar_info.nChannels = 1;
+        radar_info.rate = (int)tag->getRate();
+        break;
+      default:
+        cerr << "DSMMesaSensor::init() Unknown sampleID " << tag->getSampleId() << endl;
+    }
+
+/*
+    for (VariableIterator vi = tag->getVariableIterator(); vi.hasNext(); )
+    {
+      const Variable* var = vi.next();
+ cerr << "  var=" << var->getName() << endl;
+    }
+*/
+  }
+
+  cerr << __PRETTY_FUNCTION__ << "fromDOMElement-end" << endl;
+}
+
+/*---------------------------------------------------------------------------*/
+bool DSMMesaSensor::sendFPGACodeToDriver() throw(n_u::IOException)
+{
+  char devstr[64];
+  FILE * fdMesaFPGAfile;
+
+  // Open up the FPGA program from disk...
+  strcpy(devstr, "/opt/mesa_fpga_file.bit");
+  cerr << "opening " << devstr << endl;
+  if ((fdMesaFPGAfile = fopen(devstr, "rb")) == NULL)
+  {
+    cerr << "Failed to open FPGA program file " << devstr << endl;
+    return false;
+  }
+
+  size_t filesize = filelengthq(fdMesaFPGAfile);
+  cerr << "FPGA file size: " << filesize << endl;
+
+  // Send the Load FPGA Program ioctl
+  cerr << "go select file type" << endl;
+  selectfiletype(fdMesaFPGAfile);
+  cerr << "file type selected" << endl;
+
+  struct _prog prog;
+  do
+  {
+    prog.len = fread(prog.buffer, 1, MAX_BUFFER, fdMesaFPGAfile);
+    ioctl(MESA_LOAD, &prog, sizeof(struct _prog));
+  }
+  while( !feof(fdMesaFPGAfile) );
+
+  ioctl(MESA_DONE, 0, 0);
+
+  cerr << "Done sending bit file down." << endl;
+  fclose(fdMesaFPGAfile);
+  return true;
+}
+
+/*---------------------------------------------------------------------------*/
+long DSMMesaSensor::filelengthq(FILE *f)
+{
+  long curpos = ftell(f);
+
+  if (curpos < 0)
+    return -1;
+
+  if (fseek(f, 0, SEEK_END) != 0)
+    return -1;
+
+  long len = ftell(f);
+
+  if (len < 0)
+    return -1;
+
+  if (fseek(f, curpos, SEEK_SET) != 0)
+    return -1;
+
+  return len;
+}
+
+/*---------------------------------------------------------------------------*/
+size_t DSMMesaSensor::readbytesfromfile(FILE *f, long fromoffset,
+		size_t numbytes, unsigned char *bufptr)
+{
+  if (fseek(f, fromoffset, SEEK_SET) != 0)
   {
     return 0 ;
   }
   return fread(bufptr, 1, numbytes, f) ;
 }
+
 /*---------------------------------------------------------------------------*/
-
-/* Function: outportbwswap
-   Purpose: Send a byte to the 4I34 data port with bits swapped.
-   Used by: Local functions.
-   Returns: Nothing.
-   Notes:
-*/
-
-void DSMMesaSensor::outportbwswap(unsigned thebyte)
+void DSMMesaSensor::selectfiletype(FILE * fp)
 {
-  unsigned char* ptr;
-  static unsigned char swaptab[256] =
-  {
-    0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0,
-    0x30, 0xB0, 0x70, 0xF0, 0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8,
-    0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8, 0x04, 0x84, 0x44, 0xC4,
-    0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
-    0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC,
-    0x3C, 0xBC, 0x7C, 0xFC, 0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2,
-    0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2, 0x0A, 0x8A, 0x4A, 0xCA,
-    0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
-    0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6,
-    0x36, 0xB6, 0x76, 0xF6, 0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE,
-    0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE, 0x01, 0x81, 0x41, 0xC1,
-    0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
-    0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9,
-    0x39, 0xB9, 0x79, 0xF9, 0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5,
-    0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5, 0x0D, 0x8D, 0x4D, 0xCD,
-    0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
-    0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3,
-    0x33, 0xB3, 0x73, 0xF3, 0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB,
-    0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB, 0x07, 0x87, 0x47, 0xC7,
-    0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
-    0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF,
-    0x3F, 0xBF, 0x7F, 0xFF
-  };
-
-  ptr = &swaptab[thebyte];
-  write(fdMesaFPGAfifo, ptr, 1);
-}
-/*---------------------------------------------------------------------------*/
-/* Function: selectfiletype
-   Purpose: Set up for processing the input file.
-   Used by: Local functions.
-   Returns: Nothing.
-*/
-void DSMMesaSensor::selectfiletype(void)
-{
-  unsigned char b[14] ;
+  unsigned char b[14];
+  long ImageLen;	// Number of bytes in device image portion of file.
 
   /* Read in the interesting parts of the file header. */
-  if(readbytesfromfile(fdMesaFPGAfile, 0, sizeof(b), b) != sizeof(b))
+  if (readbytesfromfile(fp, 0, sizeof(b), b) != sizeof(b))
   {
-    printf( "Unexpected end of file.\n") ;
+    cerr << "Unexpected end of file." << endl;
   }
 
   /* Figure out what kind of file we have. */
-  if((b[0] == 0x00) && (b[1] == 0x09) && (b[11] == 0x00) && (b[12] == 0x01) &&
+  if ((b[0] == 0x00) && (b[1] == 0x09) && (b[11] == 0x00) && (b[12] == 0x01) &&
      (b[13] == 'a'))
   {       /* Looks like a .BIT file. */
     signed long base ;
-    printf("\nLooks like a .BIT file:") ;
+    cerr << "Looks like a .BIT file:" << endl ;
     base = 14 ; /* Offset of design name length field. */
 
     /* Display file particulars. */
@@ -139,196 +303,26 @@ void DSMMesaSensor::selectfiletype(void)
     printf("\nDesign date:          ") ; sayfileinfo(&base) ;
     printf("\nDesign time:          ") ; sayfileinfo(&base) ;
 */
-    if(readbytesfromfile(fdMesaFPGAfile, base, 4, b) != 4)
+    if (readbytesfromfile(fp, base, 4, b) != 4)
     {
-      printf("Base address error\n");
+      cerr << "Base address error" << endl;
     }
     ImageLen = (((unsigned long)b[0] << 24) |
                 ((unsigned long)b[1] << 16) |
                 ((unsigned long)b[2] << 8) |
                 (unsigned long)b[3]) ;
-    printf("\nConfiguration length: %lu bytes", ImageLen) ;
+    cerr << "Configuration length: " << ImageLen << " bytes" << endl;
 
     /* We leave the file position set to the next byte in the file,
        which should be the first byte of the body of the data image. */
   }
-  else if((b[0] == 0xFF) && (b[4] == 0x55) && (b[5] == 0x99) &&
+  else if ((b[0] == 0xFF) && (b[4] == 0x55) && (b[5] == 0x99) &&
           (b[6] == 0xAA) && (b[7] == 0x66))
   {       /* Looks like a PROM file. */
-    printf("\nLooks like a PROM file:") ;
+    cerr << "Looks like a PROM file." << endl;
   }
   else
   {       /* It isn't something we know about. */
-    printf( "Unknown file type.\n") ;
-  }
-}
-/*---------------------------------------------------------------------------*/
-static long DSMMesaSensor::filelengthq(FILE *f)
-{
-  long curpos, len ;
-
-  curpos = ftell(f) ;
-  if(curpos < 0)
-  {
-    return -1 ;
-  }
-  if(fseek(f, 0, SEEK_END) != 0)
-  {
-    return -1 ;
-  }
-  len = ftell(f) ;
-  if(len < 0)
-  {
-    return -1 ;
-  }
-  if(fseek(f, curpos, SEEK_SET) != 0)
-  {
-    return -1 ;
-  }
-  return len ;
-}
-/*---------------------------------------------------------------------------*/
-
-void DSMMesaSensor::open(int flags) throw(n_u::IOException)
-{
-
-  DSMSensor::open(flags);
-
-  char devstr[30];
-  int fd_mesa_counter[3];  // file pointers 
-  int fd_mesa_radar[1];  // file pointers 
-  unsigned long len,total, i;
-  unsigned long filesize;
-
-  cerr << __PRETTY_FUNCTION__ << "mps-begin" << endl;
-
-
-  // Open up the FPGA program FIFO to the driver...
-  sprintf(devstr, "/dev/mesa_program_board");
-  err("opening '%s'", devstr);
-  fdMesaFPGAfifo = open(devstr, O_NONBLOCK | O_WRONLY);
-  err("fdMesaFPGAfifo = 0x%x", fdMesaFPGAfifo);
-
-  // Open up the FPGA program drom disk...
-  sprintf(devstr, "/opt/mesa_fpga_file.bit");
-  err("opening '%s'", devstr);
-  fdMesaFPGAfile = fopen(devstr, "rb");
-
-  err("bit file opened");
-  filesize = filelengthq(fdMesaFPGAfile);
-  err("file size: %lu", filesize);
-
-  //Send the Load FPGA Program ioctl
-  ioctl(MESA_LOAD, &filesize, sizeof(unsigned long));
-  total = 0;
-  err("go select file type");
-  selectfiletype();
-  err("file type selected");
-  do {
-    total += len = fread(&buffer, 1, MAX_BUFFER, fdMesaFPGAfile);
-    for(i = 0; i < len; i++){
-      outportbwswap(buffer[i]);
-    }
-//  }while ( total < filesize );
-  }while(!feof(fdMesaFPGAfile));
-  err("Done sending bit file down");
-  fclose(fdMesaFPGAfile);
-  close(fdMesaFPGAfifo);
-  err(" opening sensors...");
-
-  // open all of the counter FIFOs
-  for (int ii=0; ii < counter_channels; ii++)
-  {
-    sprintf(devstr, "/dev/mesa_in_%d", ii);
-    fd_mesa_counter[ii] = open(devstr, O_RDONLY);
-    if (fd_mesa_counter[ii] < 0)
-    {
-      err("failed to open '%s'", devstr);
-      return 0;
-    }
-    err("opened '%s' @ 0x%x", devstr, fd_mesa_counter[ii]);
-  }
-  // open the radar FIFO
-  for (int ii=0; ii < radar_channels; ii++)
-  {
-    sprintf(devstr, "/dev/mesa_in_%d", ii + counter_channels);
-    fd_mesa_radar[ii] = open(devstr, O_RDONLY);
-    if (fd_mesa_radar[ii] < 0)
-    {
-      err("failed to open '%s'", devstr);
-      return 0;
-    }
-    err("opened '%s' @ 0x%x", devstr, fd_mesa_radar[ii]);
-  }
-
-  // Note: fd_set is a 1024 bit mask.
-  fd_set readfds;
-
-  // Set the counters.   
-  ioctl(COUNTERS_SET, &set_counter, sizeof(set_counter));
-
-  // Set the counters.   
-  ioctl(COUNTERS_SET, &set_counter, sizeof(set_counter));
-
-  // Set the radar.   
-
-  cerr << __PRETTY_FUNCTION__ << "mps-end" << endl;
-}
-
-void DSMMesaSensor::close() throw(n_u::IOException)
-{
-  cerr << __PRETTY_FUNCTION__ << endl;
-
-  RTL_DSMSensor::close();
-}
-
-void DSMMesaSensor::fromDOMElement(const DOMElement* node)
-  throw(n_u::InvalidParameterException)
-{
-  RTL_DSMSensor::fromDOMElement(node);
-  XDOMElement xnode(node);
-
-  cerr << __PRETTY_FUNCTION__ << ": xnode element name: " <<
-    xnode.getNodeName() << endl;
-	
-  DOMNode* child;
-  for (child = node->getFirstChild(); child != 0;
-       child = child->getNextSibling())
-  {
-    if (child->getNodeType() != DOMNode::ELEMENT_NODE) continue;
-    XDOMElement xchild((DOMElement*) child);
-    const string& elname = xchild.getNodeName();
-
-    if (!elname.compare("mesacfg")) {
-
-      if ( xchild.getAttributeValue("desc").c_str()[0] == 'C' ) {
-        set_counter.rate = strtoul(xchild.getAttributeValue("rate").c_str(),
-                              NULL,0);
-        set_counter.channel = strtoul(xchild.getAttributeValue("chan").c_str(),
-                              NULL,0);
-        counter_channels = set_counter.channel;
-      }
-      else if ( xchild.getAttributeValue("desc").c_str()[0] == 'R' ) {
-        set_radar.rate = strtoul(xchild.getAttributeValue("rate").c_str(),
-                              NULL,0);
-        set_radar.channel = strtoul(xchild.getAttributeValue("chan").c_str(),
-                              NULL,0);
-        radar_channels = set_radar.channel;
-      }
-      else if ( xchild.getAttributeValue("desc").c_str()[0] == 'P' ) {
-        set_PMS260X.rate = strtoul(xchild.getAttributeValue("rate").c_str(),
-                              NULL,0);
-        set_PMS260X.channel = strtoul(xchild.getAttributeValue("chan").c_str(),
-                              NULL,0);
-      }
-      else {
-        throw n_u::InvalidParameterException (__PRETTY_FUNCTION__,
-          "not a valid desc:", xchild.getAttributeValue("desc").c_str());
-
-      cerr << " desc: " << xchild.getAttributeValue("desc")
-           << " rate: " << xchild.getAttributeValue("rate")
-           << " chan: " << xchild.getAttributeValue("chan")  << endl;
-      }
-    }
+    cerr <<  "Unknown file type." << endl ;
   }
 }
--- src/nidas/dynld/raf/DSMMesaSensor.h
+++ src/nidas/dynld/raf/DSMMesaSensor.h
@@ -12,6 +12,7 @@
 
  ******************************************************************
 */
+
 #ifndef NIDAS_DYNLD_RAF_DSMMESASENSOR_H
 #define NIDAS_DYNLD_RAF_DSMMESASENSOR_H
 
@@ -23,41 +24,74 @@ namespace nidas { namespace dynld { namespace raf {
 using namespace nidas::core;
 
 /**
- * A sensor connected to a MESA port.
+ * Sensors connected to the Mesa AnythingIO card.  Current programming is
+ * for a PMS1D-260X, Pulse Counting, and the APN-232 Radar Altimeter.
+ * Digital in/out coming soon.
  */
 class DSMMesaSensor : public DSMSensor {
 
 public:
-
   DSMMesaSensor();
-
   ~DSMMesaSensor();
 
-  IODevice* buildIODevice() throw(nidas::util::IOException);
+  IODevice *
+  buildIODevice() throw(nidas::util::IOException);
 
-  SampleScanner* buildSampleScanner();
+  SampleScanner *
+  buildSampleScanner();
 
-  /* This opens the associated RT-Linux FIFOs.  */
-  void open(int flags) throw(nidas::util::IOException);
+  /**
+   * open the sensor and perform any intialization to the driver.
+   */
+  void
+  open(int flags) throw(nidas::util::IOException);
 
-  /* This closes the associated RT-Linux FIFOs. */
-  void close() throw(nidas::util::IOException);
+  void
+  fromDOMElement(const xercesc::DOMElement *)
+    throw(nidas::util::InvalidParameterException);
 
-  /* Extract the MESA configuration elements from the XML header. */
-  void fromDOMElement(const xercesc::DOMElement*)
-       throw(nidas::util::InvalidParameterException);
+  bool
+  process(const Sample * samp, std::list<const Sample *>& results)
+	throw();
 
-  private:
+private:
+  /**
+   * Download FPGA code from flash/disk to driver.
+   *
+   * @returns whether file was succesfully transmitted.
+   */
+  bool
+  sendFPGACodeToDriver() throw(nidas::util::IOException);
 
-  struct radar_set set_radar;
-  struct counters_set set_counter;
-  int counter_channels;
-  int radar_channels;
+  /**
+   * Read the specified number of bytes from the specified offset in
+   * the specified open file.
+   *
+   * Note: The file position is not restored to its original value.
+   * Don't try to read 0 bytes.
+   *
+   * @returns The number of bytes read.
+   *
+   * @see sendFPGACodeToDriver()
+   */
+  size_t
+  readbytesfromfile(FILE * f, long fromoffset, size_t numbytes,
+		unsigned char * bufptr);
 
-  FILE* fdMesaFPGAfile;
-  int fdMesaFPGAfifo;
-  signed long ImageLen ; /* Number of bytes in device image portion of 
-                                 file. */
+  /**
+   * Set up for processing the input file.
+   *
+   * @see sendFPGACodeToDriver()
+   */
+  void
+  DSMMesaSensor::selectfiletype(FILE * fp);
+
+  long
+  DSMMesaSensor::filelengthq(FILE * f);
+
+  struct radar_set radar_info;
+  struct pms260x_set p260x_info;
+  struct counters_set counter_info;
 };
 
 }}}	// namespace nidas namespace dynld namespace raf
