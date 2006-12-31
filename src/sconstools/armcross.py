04a7457786a39495a4fcb18d5a93eba3510e3ea8
Gary Granger <granger@ucar.edu>
2006-12-31 06:04:25 +0000

    append /opt/arm_tools/bin to the PATH, to act as a default location to find the ARM cross-compile tools

--- src/sconstools/armcross.py
+++ src/sconstools/armcross.py
@@ -3,10 +3,21 @@
 Customize an environment to use the GCC ARM cross-compiler tools.
 """
 
+import os
+
 def generate(env):
     """
     Add Builders and construction variables for C compilers to an Environment.
     """
+
+    # Just put the common locations of arm tools on the path too, in case
+    # they are not already there.
+    env.AppendENVPath('PATH', '/opt/arm_tools/bin')
+    
+#     # Look for the compiler to get the path.
+#     gcc = env.WhereIs('arm-linux-gcc')
+#     if (gcc):
+#         env['ARM_CROSS_BINDIR'] = os.path.dirname(gcc)
     env.Replace(AR	= 'arm-linux-ar')
     env.Replace(AS	= 'arm-linux-as')
     env.Replace(CC	= 'arm-linux-gcc')
