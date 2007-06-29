9b894553cb54ffede4c612671cabca4e5eee0141
John Wasinger <wasinger@ucar.edu>
2007-06-29 23:03:50 +0000

    Implemented process method for the C-130's Inertial Reference System.

--- src/nidas/dynld/raf/IRS_HW_YG1854.cc
+++ src/nidas/dynld/raf/IRS_HW_YG1854.cc
@@ -20,33 +20,58 @@ NIDAS_CREATOR_FUNCTION_NS(raf,IRS_HW_YG1854);
 
 float IRS_HW_YG1854::processLabel(const long data)
 {
+  /* All clients of arinc20bit are in pirad's, so multiply by 180 right
+   * now to get to degree's.
+   */
+  static const float arinc20bits = (1.0 / (1 << 20));
+  static const float arinc15bits = (1.0 / (1 << 15));
+  static const float arinc13bits = (1.0 / (1 << 13));
+  static const float arinc8bits  = (1.0 / (1 << 8));
+
 //err("%4o 0x%08lx", (int)(data & 0xff), (data & (unsigned long)0xffffff00) );
 
   switch (data & 0xff) {
-  case 0310:  // present_lat
-  case 0311:  // present_lon
-  case 0312:  // ground_speed
-  case 0313:  // track_angle_true
-  case 0314:  // true_heading
-  case 0315:  // wind_speed
-  case 0316:  // wind_dir_true
-  case 0321:  // drift_angle
-  case 0324:  // pitch_angle
-  case 0325:  // roll_angle
-  case 0326:  // pitch_rate
-  case 0327:  // roll_rate
-  case 0330:  // yaw_rate
-  case 0331:  // long_accel
-  case 0332:  // lat_accel
-  case 0333:  // normal_accel
-  case 0335:  // track_ang_rate
-  case 0361:  // inertial_alt
-  case 0364:  // vertical_accel
-  case 0365:  // inrt_vert_speed
-  case 0366:  // velocity_ns
-  case 0367:  // velocity_ew
+  case 0310:  // BNR - pos_latitude         (rad)
+  case 0311:  // BNR - pos_longitude        (rad)
+  case 0313:  // BNR - track_angle_true     (rad)
+  case 0314:  // BNR - true_heading         (rad)
+  case 0316:  // BNR - wind_dir_true        (rad)
+  case 0321:  // BNR - drift_angle          (rad)
+  case 0324:  // BNR - pitch_angle          (rad)
+  case 0325:  // BNR - roll_angle           (rad)
+     return (float)(data>>11) * arinc20bits * 180.0;
+
+  case 0312:  // BNR - ground_speed         (knot)
+  case 0366:  // BNR - velocity_ns          (knot)
+  case 0367:  // BNR - velocity_ew          (knot)
+     return (float)(data>>11) * arinc8bits * KTS_MS;
+
+  case 0315:  // BNR - wind_speed           (knot)
+     return (float)(data>>10) * arinc13bits * KTS_MS;
+
+  case 0326:  // BNR - pitch_rate           (deg/s)
+  case 0327:  // BNR - roll_rate            (deg/s)
+  case 0330:  // BNR - yaw_rate             (deg/s)
+     return (float)(data>>16) * arinc8bits;
+
+  case 0331:  // BNR - long_accel           (G)
+  case 0332:  // BNR - lat_accel            (G)
+  case 0333:  // BNR - normal_accel         (G)
+     return (float)(data>>16) * arinc13bits * G_MPS2;
+
+  case 0335:  // BNR - track_ang_rate       (deg/s)
+     return (float)(data>>11) * arinc15bits;
+
+  case 0361:  // BNR - inertial_alt         (f)
+     return (float)(data>>11) * 0.125 * FT_MTR;
+
+  case 0364:  // BNR - vertical_accel       (G)
+     return (float)(data>>16) * arinc13bits * G_MPS2;
+
+  case 0365:  // BNR - inrt_vert_speed      (ft/min)
+     return (float)(data>>11) * 0.03125 * FPM_MPS;
+
   default:
     break;
   }
-  return 42.0;
 }
