65f9ecd17ecffac6591b68680261f8eeeefbd42c
Mike Spowart <spowart@ucar.edu>
2007-07-16 21:51:38 +0000

    Working Anything IO.  Check in before PASE test flights.

--- src/firmware/anythingIO/anythingIO_260x.vhd
+++ src/firmware/anythingIO/anythingIO_260x.vhd
@@ -13,7 +13,7 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 USE ieee.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
---USE WORK.data_memory.ALL;
+--USE data__memory.ALL;
 entity PC104 is port 
 (
      -- bus interface signals --
@@ -41,7 +41,7 @@ entity PC104 is port
   IORD: in STD_LOGIC;   
   IOWR: in STD_LOGIC;
   SA: in STD_LOGIC_VECTOR (9 downto 0); 		-- address bus
-  SD: inout STD_LOGIC_VECTOR (15 downto 0); 		-- data bus
+  SD: out STD_LOGIC_VECTOR (15 downto 0); 		-- data bus
 -- led bits
   LEDS: out STD_LOGIC_VECTOR(5 downto 0);
   strobe: in std_logic;  
@@ -61,29 +61,41 @@ entity PC104 is port
   cdebug: out std_logic
 );
 end PC104;
-
 Architecture behavior of PC104 is
-  type memory is array(63 downto 0) of std_logic_vector(15 downto 0);
 -- 0x220 hex  
   constant Decode : STD_LOGIC_VECTOR (5 downto 0) := "100010"; 
+  
 -- misc global signals --
-  signal Histogram: memory;
+--  type memory is array(63 downto 0) of std_logic_vector(15 downto 0);
+  type ram_type is array(63 downto 0) of std_logic_vector(15 downto 0);
+--  signal Histogram: memory; --:=
+  signal Histogram: ram_type:=
+    (X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",
+     X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000",X"0000");
+  signal index: integer:=0;
   signal CardSelect: STD_LOGIC:='0';
+  signal HistAdd: std_logic_vector (15 downto 0):=X"0000";	
   signal strobes: std_logic_vector (15 downto 0):=X"0000";	
-  signal index: std_logic_vector(5 downto 0):="000000";
-  signal hist_index: integer:=0;
-  signal hist_data: std_logic_vector(15 downto 0):=X"0000";
   signal count0: std_logic_vector(15 downto 0):=X"0000";
   signal count1: std_logic_vector(15 downto 0):=X"0000";
   signal count0_latch: std_logic_vector(15 downto 0):=X"0000";
   signal count1_latch: std_logic_vector(15 downto 0):=X"0000";
   signal count_clear: std_logic:='0';
-  signal altitude: std_logic_vector(0 to 23):=X"000000";
+  signal altitude: std_logic_vector(23 downto 0):=X"000000";
   signal clear_hist: std_logic:='0';
   signal house_data: std_logic_vector(15 downto 0):=X"0000";
   signal house_adv: std_logic:='0';
   signal alt_clock_old1: std_logic:='0';
   signal alt_clock_old2: std_logic:='0';  
+  signal read_data: std_logic:='0';
+  signal read_data_old1: std_logic:='0';
+  signal read_data_old2: std_logic:='0';  
   signal pulse0_old1: std_logic:='0';
   signal pulse1_old1: std_logic:='0';
   signal pulse0_old2: std_logic:='0';
@@ -114,13 +126,17 @@ begin
 
 --    DRQ <= "ZZ";
 --    IRQ <= "ZZZZZZZZZZZZZ";
---    DIS <= '0';		-- uncomment to leave configuration decode on
-    DIS <= '1';		-- uncomment to disable configuration decode
+    DIS <= '0';		-- uncomment to leave configuration decode on
+--    DIS <= '1';		-- uncomment to disable configuration decode
 
 --    MEMCS16 <= 'Z';
 --    IOCHRDY <= 'Z';
 --  ckout <= SynClk;
-  leds(0) <= not ledcount(23);
+  HistAdd(15 downto 6) <= "0000000000";
+  HistAdd(5 downto 0) <= HistBits(5 downto 0);
+--  leds(0) <= not ledcount(23);
+  leds(0) <= not(HistBits(0) and HistBits(1) and HistBits(2) and HistBits(3)
+              and HistBits(4) and HistBits(5));
   leds(1) <= not ledcount(24);
   leds(2) <= not ledcount(25);
   leds(3) <= not ledcount(26);
@@ -129,7 +145,7 @@ begin
 
   ledblinker: process (SynClk) 
   begin
-    if SynClk'event and SynClk = '1' then
+    if rising_edge(SynClk) then
       ledcount <= ledcount + 1;			
     end if;
   end process ledblinker;
@@ -139,9 +155,13 @@ begin
     if rising_edge(SynClk) then
       alt_clock_old2 <= alt_clock_old1;      
       alt_clock_old1 <= alt_clock;
-      if alt_clock_old2 = '1' and alt_clock_old1 = '0' then
-        altitude <= altitude(1 to 23) & radar_alt; --bit into lsb
---      altitude <= radar_alt & altitude(0 to 22); --bit into msb
+      if alt_clock_old2 = '0' and alt_clock_old1 = '1' then
+--        altitude <= altitude(1 to 23) & radar_alt; --bit into msb
+--        altitude <= radar_alt & altitude(0 to 22); --bit into lsb
+--          altitude <= altitude(22 downto 0) & radar_alt; --bit into lsb
+          altitude <= radar_alt & altitude(23 downto 1) ; --bit into msb
+          ndebug <= radar_alt;
+          pdebug <= altitude(23);
       end if;
     end if;
   end process RadarAltimeter;
@@ -156,15 +176,9 @@ begin
       if count_clear = '1' then
         count0 <= X"0000";
         count1 <= X"0000";
-        ndebug <= '1';
-      else
-        ndebug <= '0';
       end if;
       if pulse0_old2 = '0' and pulse0_old1 = '1' then
         count0 <= count0 + 1;
-        pdebug <= '1';
-      else
-        pdebug <= '0';
       end if;
       if pulse1_old2 = '0' and pulse1_old1 = '1' then
         count1 <= count1 + 1;
@@ -178,15 +192,11 @@ begin
       latch_count_old1 <= latch_count;	 
       latch_count_old2 <= latch_count_old1;
       if latch_count_old2 = '0' and latch_count_old1 = '1' then
+        count_clear <= '1';
         count0_latch <= count0;	   
         count1_latch <= count1;
---	   count0 <= X"0000";
---	   count1 <= X"0000";
-        count_clear <= '1';
-        ldebug <= '1';	   
       else 
         count_clear <= '0';
-        ldebug <= '0';
       end if;
     end if;
   end process LatchCounts;
@@ -225,51 +235,57 @@ begin
   end process Localbuffer;	
 
   AddDecode: process (SA, AEN, IORD)
+--  variable i: integer;
   begin
-    if SA(3 downto 0) = "0000" and SA(9 downto 4) = Decode and AEN = '0' then
+    clear_hist <= '0';
+	 read_data <= '0';
+    if SA(3 downto 0) = "0010" and SA(9 downto 4) = Decode and AEN = '0' then
       IOCS16 <= '0';
       if IORD = '0' then 
         SD(15 downto 0) <= strobes(15 downto 0);
       end if;		
-    elsif SA(3 downto 0) = "0001" and SA(9 downto 4) = Decode and AEN = '0' then 
-      clear_hist <= '1';
-      index <= "000000";
+    elsif SA(3 downto 0) = "0011" and SA(9 downto 4) = Decode and AEN = '0' then 
       IOCS16 <= 'Z';	       
-    elsif SA(3 downto 0) = "0010" and SA(9 downto 4) = Decode and AEN = '0' then       
+      if IORD = '0' then 
+        ldebug <= '1';
+        clear_hist <= '1';
+      end if;     
+    elsif SA(3 downto 0) = "0100" and SA(9 downto 4) = Decode and AEN = '0' then       
       IOCS16 <= '0';
       if IORD = '0' then 
-        SD <= Histogram(conv_integer(index));
-        index <= index + 1;
+        ldebug <= '0';
+        SD(15 downto 0) <= Histogram(index);
+        read_data <= '1';
       end if;     
-    elsif SA(3 downto 0) = "0011" and SA(9 downto 4) = Decode and AEN = '0' then
+    elsif SA(3 downto 0) = "0101" and SA(9 downto 4) = Decode and AEN = '0' then
       IOCS16 <= 'Z';
       Hadvance <= '1';
       house_adv <= '1';
-    elsif SA(3 downto 0) = "0100" and SA(9 downto 4) = Decode and AEN = '0' then
+    elsif SA(3 downto 0) = "0110" and SA(9 downto 4) = Decode and AEN = '0' then
       IOCS16 <= '0';
       if IORD = '0' then       
         SD <= house_data;
       end if;     
-    elsif SA(3 downto 0) = "0101" and SA(9 downto 4) = Decode and AEN = '0' then
+    elsif SA(3 downto 0) = "0111" and SA(9 downto 4) = Decode and AEN = '0' then
       IOCS16 <= 'Z';
       Hreset <= '1';
-    elsif SA(3 downto 0) = "0110" and SA(9 downto 4) = Decode and AEN = '0' then 
+    elsif SA(3 downto 0) = "1000" and SA(9 downto 4) = Decode and AEN = '0' then 
       IOCS16 <= '0';
       if IORD = '0' then 
         SD(15 downto 0) <= count0_latch(15 downto 0);
-        cdebug <='0';    
+--        cdebug <='0';    
       end if;
-    elsif SA(3 downto 0) = "1000" and SA(9 downto 4) = Decode and AEN = '0' then 
+    elsif SA(3 downto 0) = "1010" and SA(9 downto 4) = Decode and AEN = '0' then 
       IOCS16 <= '0';
       if IORD = '0' then 
         SD(15 downto 0) <= count1_latch(15 downto 0); 
       end if;	       
-    elsif SA(3 downto 0) = "1010" and SA(9 downto 4) = Decode and AEN = '0' then 
+    elsif SA(3 downto 0) = "1100" and SA(9 downto 4) = Decode and AEN = '0' then 
       IOCS16 <= '0';
       if IORD = '0' then 
-        SD(15 downto 8) <= altitude(2 to 9);      
-        SD(7 downto 1) <= altitude(10 to 16);      
-        SD(0) <= altitude(20) or altitude(21) or altitude (22);
+        SD(15 downto 8) <= altitude(21 downto 14);      
+        SD(7 downto 1) <= altitude(13 downto 7);      
+        SD(0) <= altitude(1) or altitude(2) or altitude (4);
       end if;
     else
       Hadvance <= '0';
@@ -277,32 +293,47 @@ begin
       Hreset <= '0';
       SD(15 downto 0) <= "ZZZZZZZZZZZZZZZZ";
       IOCS16 <= 'Z';
-      cdebug <= '1';
+--      cdebug <= '1';
     end if;
   end process AddDecode;
 
   histo: process(SynClk,clear_hist)
   variable i: integer;
+  variable j: integer;
+  variable k: integer;
+  variable n: integer;
   begin
-    if (clear_hist = '1') then
-      i := 63;
-      while i>=32 loop
-        Histogram(i) <= X"0000";
-        i := i - 1;
-      end loop;
-      i := 31;
-      while i>=0 loop
-        Histogram(i) <= X"0000";
-        i := i - 1;
-      end loop;   
+    if clear_hist = '1' then
+      i := 0;
+      cdebug <= '1';
+      strobes <= X"0000";
+		for k in 0 to 63 loop
+        Histogram(k) <= X"0000";
+		end loop;
+--      i := 63;
+--      while i>=0 loop
+--        Histogram(i) <= X"0000";
+--        i := i - 1;
+--      end loop;
+--      i := 31;
+--      while i>=0 loop
+--        Histogram(i) <= X"0000";
+--        i := i - 1;
+--      end loop;   
     elsif rising_edge(SynClk) then
+	   j := conv_integer(HistAdd);
+	   n := j;
+      cdebug <= '0';
       strobe_old2 <= strobe_old1;
       strobe_old1 <= strobe;
-      if strobe_old2 = '1' and strobe_old1 = '0' then
+		read_data_old2 <= read_data_old1;
+		read_data_old1 <= read_data;
+		if read_data_old2 = '0' and read_data_old1 = '1' then
+		  index <= i;
+		  i := i + 1;
+      elsif strobe_old2 = '0' and strobe_old1 = '1' then
         strobes <= strobes + 1;
-        hist_index <= conv_integer(Histbits);
-        hist_data <= Histogram(hist_index) + 1;
-        Histogram(hist_index) <= hist_data;
+        Histogram(n) <= Histogram(n) + 1;
       end if;
     end if;
   end process histo;
