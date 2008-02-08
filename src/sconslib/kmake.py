a7a51b9c78728db20d5790fe75d190c1e6baf252
Gordon Maclean <maclean@ucar.edu>
2008-02-08 21:01:56 +0000

    catch make error

--- src/sconslib/kmake.py
+++ src/sconslib/kmake.py
@@ -1,24 +1,5 @@
-
 import os
 import SCons.Errors
-# import re
-
-def run(cmd):
-    import sys
-    import os
-    import SCons.Script
-    """Run a command and return the return code."""
-    res = os.system(cmd)
-    code = 0
-    # Assumes that if a process doesn't call exit, it was successful
-    if (os.WIFEXITED(res)):
-        code = os.WEXITSTATUS(res)
-        if code != 0:
-            print "Error: return code: " + str(code)
-            raise Exception(cmd + ": error: return code: " + str(code))
-            # if SCons.Script.keep_going_on_error == 0:
-             #    sys.exit(code)
-    return code
 
 def Kmake(env,target,source):
 
@@ -26,16 +7,11 @@ def Kmake(env,target,source):
 	    print "KERNELDIR not specified, " + target[0].abspath + " will not be built"
             return None
 
-    print 'KMAKE=' + env['KMAKE']
-    print "KMAKE PATH=" + env['ENV']['PATH']
     # Have the shell subprocess do a cd to the source directory.
     # If scons/python does it, then the -j multithreaded option doesn't work.
     srcdir = os.path.dirname(source[0].abspath)
-    try:
-      env.Execute('cd ' + srcdir + ';' + env['KMAKE'])
-    except:
-      raise SCons.Errors.UserError, 'error in ' + env['KMAKE']
-      
+    if env.Execute('cd ' + srcdir + '; ' + env['KMAKE']) != 0:
+          raise SCons.Errors.UserError, 'error in ' + env['KMAKE']
     return None
 
 def get_svnversion(env):
