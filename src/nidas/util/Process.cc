9b9d5f6d139b411c54c506e2105a5eb5619e8b4d
Gordon Maclean <maclean@ucar.edu>
2008-09-10 17:37:07 +0000

    Added spawn methods

--- src/nidas/util/Process.cc
+++ src/nidas/util/Process.cc
@@ -16,9 +16,13 @@
 #include <nidas/util/Process.h>
 
 #include <sys/stat.h>
+#include <sys/resource.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <fcntl.h>
 #include <cassert>
 #include <cstring>
+#include <sstream>
 
 #include <iostream>
 #include <cstdlib>  // atexit()
@@ -38,6 +42,284 @@ public:
 private:
     int fd;
 };
+}
+
+Process::Process(pid_t pid): _pid(pid),
+    _infd(-1),_instream_ap(new ostream(0)),
+    _outfd(-1),_errfd(-1)
+{
+    if (::kill(_pid,0) < 0) _pid = -1;
+}
+
+Process::Process(): _pid(-1),_infd(-1),
+    _outfd(-1),_errfd(-1)
+{
+}
+
+Process::Process(const Process& x):
+    _pid(x._pid),
+    _infd(x._infd),_inbuf_ap(x._inbuf_ap),_instream_ap(x._instream_ap),
+    _outfd(x._outfd),_outbuf_ap(x._outbuf_ap),_outstream_ap(x._outstream_ap),
+    _errfd(x._errfd),_errbuf_ap(x._errbuf_ap),_errstream_ap(x._errstream_ap)
+{
+    x._infd = -1;
+    x._outfd = -1;
+    x._errfd = -1;
+}
+
+Process& Process::operator = (const Process& x)
+{
+    _pid = x._pid;
+    _infd = x._infd;
+    _inbuf_ap = x._inbuf_ap;
+    _instream_ap = x._instream_ap;
+
+    _outfd = x._outfd;
+    _outbuf_ap = x._outbuf_ap;
+    _outstream_ap = x._outstream_ap;
+
+    _errfd = x._errfd;
+    _errbuf_ap = x._errbuf_ap;
+    _errstream_ap = x._errstream_ap;
+    return *this;
+}
+
+Process::~Process()
+{
+}
+
+void Process::setInFd(int val)
+{
+    _infd = val;
+    _inbuf_ap.reset(
+        new __gnu_cxx::stdio_filebuf<char>(_infd,ios_base::out,1024));
+    _instream_ap.reset(new ostream(_inbuf_ap.get()));
+}
+
+void Process::setOutFd(int val)
+{
+    _outfd = val;
+    _outbuf_ap.reset(
+        new __gnu_cxx::stdio_filebuf<char>(_outfd,ios_base::in,1024));
+    _outstream_ap.reset(new istream(_outbuf_ap.get()));
+}
+
+void Process::setErrFd(int val)
+{
+    _errfd = val;
+    _errbuf_ap.reset(
+        new __gnu_cxx::stdio_filebuf<char>(_errfd,ios_base::in,0));
+    _errstream_ap.reset(new istream(_errbuf_ap.get()));
+}
+
+void Process::closeIn()
+{
+    _instream_ap.reset(0);
+    _inbuf_ap.reset(0);
+    if (_infd >= 0) ::close(_infd);
+    _infd = -1;
+}
+
+void Process::closeOut()
+{
+    _outstream_ap.reset(0);
+    _outbuf_ap.reset(0);
+    if (_outfd >= 0) ::close(_outfd);
+    _outfd = -1;
+}
+
+void Process::closeErr()
+{
+    _errstream_ap.reset(0);
+    _errbuf_ap.reset(0);
+    if (_errfd >= 0) ::close(_errfd);
+    _errfd = -1;
+}
+
+/* static */
+Process Process::spawn(const string& cmd,
+    const vector<string>& args,
+    const map<string,string>& env,
+    int niceval) throw(IOException)
+{
+    int pid;
+    int infd[2];
+    int outfd[2];
+    int errfd[2];
+
+    if (pipe(infd) < 0)
+        throw IOException(cmd,"stdin pipe",errno);
+    if (pipe(outfd) < 0)
+        throw IOException(cmd,"stdout pipe",errno);
+    if (pipe(errfd) < 0)
+        throw IOException(cmd,"stderr pipe",errno);
+
+    switch (pid=vfork()) {
+    case 0: // child
+    {
+        ::close(0);
+        dup(infd[0]);
+
+        ::close(1);
+        dup(outfd[1]);
+
+        ::close(2);
+        dup(errfd[1]);
+
+        /* Close other open file descriptors */
+        unsigned int i;
+        struct rlimit rl;
+        rl.rlim_max = 0;
+        getrlimit(RLIMIT_NOFILE, &rl);
+        for (i = 3; i < rl.rlim_max; i++) (void) ::close(i);
+
+        // since we're overwriting this process we don't need to delete [] this.
+        const char **newargs = new const char*[args.size()+2];
+
+        newargs[0] = cmd.c_str();
+        for (i = 0; i < args.size(); i++)
+          newargs[i+1] = args[i].c_str();
+        newargs[i+1] = 0;
+
+        /* There isn't an execvpe where one can pass env variables,
+         * so we do a putenvs before the exec.
+         */
+        if (env.size() > 0) {
+            // since we're overwriting this process we don't need to delete [] this.
+            char **newenv = new char*[env.size()];
+            map<string,string>::const_iterator mi = env.begin();
+            for (i = 0 ; mi != env.end(); ++mi,i++) {
+                const string& name = mi->first;
+                const string& val = mi->second;
+                string envvar = name + '=' + val;
+                newenv[i] = strdup(envvar.c_str());
+                putenv(newenv[i]);
+            }
+        }
+        nice(niceval);
+
+        execvp(cmd.c_str(),(char *const *)newargs); // shouldn't return
+        perror(cmd.c_str());
+        _exit(1);
+    }
+    case -1:
+        throw IOException(cmd,"vfork",errno);
+    default:      // parent
+        break;
+
+    }
+
+    Process proc(pid);
+    proc.setInFd(infd[1]);
+    ::close(infd[0]);
+    proc.setOutFd(outfd[0]);
+    ::close(outfd[1]);
+    proc.setErrFd(errfd[0]);
+    ::close(errfd[1]);
+    return proc;
+}
+
+/* static */
+Process Process::spawn(const string& cmd,
+    const vector<string>& args,
+    int niceval) throw(IOException)
+{
+    map<string,string> env = map<string,string>();
+    return spawn(cmd,args,env,niceval);
+}
+
+/* static */
+Process Process::spawn(const string& cmd) throw(IOException)
+{
+    int pid;
+    int infd[2];
+    int outfd[2];
+    int errfd[2];
+
+    if (pipe(infd) < 0)
+        throw IOException(cmd,"stdin pipe",errno);
+    if (pipe(outfd) < 0)
+        throw IOException(cmd,"stdout pipe",errno);
+    if (pipe(errfd) < 0)
+        throw IOException(cmd,"stderr pipe",errno);
+
+    switch (pid=vfork()) {
+    case 0: // child
+    {
+        ::close(0);
+        dup(infd[0]);
+
+        ::close(1);
+        dup(outfd[1]);
+
+        ::close(2);
+        dup(errfd[1]);
+
+        /* Close other open file descriptors */
+        unsigned int i;
+        struct rlimit rl;
+        rl.rlim_max = 0;
+        getrlimit(RLIMIT_NOFILE, &rl);
+        for (i = 3; i < rl.rlim_max; i++) (void) ::close(i);
+
+        const char **newargs = new const char*[4];
+
+        newargs[0] = "sh";
+        newargs[1] = "-c";
+        newargs[2] = cmd.c_str();
+        newargs[3] = 0;
+
+        execvp("sh",(char *const *)newargs); // shouldn't return
+        perror(cmd.c_str());
+        _exit(1);
+    }
+    case -1:
+        throw IOException(cmd,"vfork",errno);
+    default:      // parent
+        break;
+    }
+
+    Process proc(pid);
+    proc.setInFd(infd[1]);
+    ::close(infd[0]);
+    proc.setOutFd(outfd[0]);
+    ::close(outfd[1]);
+    proc.setErrFd(errfd[0]);
+    ::close(errfd[1]);
+    return proc;
+}
+
+void Process::kill(int sig) throw(IOException)
+{
+    // ESRCH The  pid or process group does not exist.  Note that an existing
+    //   process might be a zombie, a  process  which  already  committed
+    //   termination, but has not yet been wait(2)ed for.
+    if (_pid > 0 && ::kill(_pid,sig) < 0) {
+        ostringstream pname;
+        pname << "process id " << _pid;
+        throw IOException(pname.str(),"kill",errno);
+    }
+}
+
+int Process::wait(bool hang,int* status) throw(IOException)
+{
+
+    if (_pid <= 0) return -1;
+    int res;
+    int options = 0;    // WNOHANG, WUNTRACED,WCONTINUED
+    if (!hang) options |= WNOHANG;
+    if ((res = ::waitpid(_pid,status,options)) < 0) {
+        ostringstream pname;
+        pname << "process id " << _pid;
+        throw IOException(pname.str(),"wait",errno);
+    }
+    if (res > 0 && WIFEXITED(*status)) {
+        _pid = -1;
+        closeIn();
+        closeOut();
+        closeErr();
+    }
+    return res;
 }
 
 /* static */
--- src/nidas/util/Process.h
+++ src/nidas/util/Process.h
@@ -16,15 +16,182 @@
 
 #include <nidas/util/IOException.h>
 
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
+#include <signal.h>
 #include <sys/types.h>
 
-namespace nidas { namespace util {
+#include <ext/stdio_filebuf.h>
 
+namespace nidas { namespace util {
 class Process {
 public:
 
     /**
-     * Check if another process is running using the same process id file.
+     * Construct a Process.
+     * @ param pid: pid should be > 0 and be a valid process id.
+     * Unix system calls support doing kills and waits on a pid
+     * less than or equal to 0, but those capabilities are
+     * not supported in this class.
+     */
+    Process(int pid);
+
+    /**
+     * Default constructor. Does not point to any process.
+     */
+    Process();
+
+    /**
+     * Copy constructor.  The new process will inherit
+     * the file descriptors and iostreams from x.
+     * The file descriptors and iostreams of the copied
+     * Process (x) are no longer valid after this copy.
+     */
+    Process(const Process& x);
+
+    /**
+     * Assignment operator.  The assigned process (on LHS of =)
+     * will inherit the file descriptors and iostreams from x.
+     * The file descriptors and iostreams of the RHS Process (x)
+     * are no longer valid after this assignment.
+     */
+    Process& operator=(const Process& x);
+
+    /**
+     * Destructor, which does very little. It does not do a
+     * kill or wait on the process, or close
+     * any file descriptors that are connected to the process.
+     */
+    ~Process();
+
+    /**
+     * Fork and execute a command and associated arguments and
+     * environment. Return the Process object that can be
+     * used to communicate with or control the sub-process.
+     * @param niceval: The desired nice value, typically a positive
+     *      value so that the spawned process has a higher nice
+     *      value (lower priority).  If niceval is negative
+     *      the and the user does not have sufficient permisions
+     *      to lower the nice value, the error is ignored.
+     */
+    static Process spawn(const std::string& cmd,
+        const std::vector<std::string>& args,
+        const std::map<std::string, std::string>& env,
+        int niceval=0) throw(IOException);
+
+    /**
+     * Fork and execute a command and associated arguments.
+     * Return the Process object that can be
+     * used to communicate with or control the sub-process.
+     */
+    static Process spawn(const std::string& cmd,
+        const std::vector<std::string>& args,
+        int niceval=0) throw(IOException);
+
+    /**
+     * Execute a command by forking and executing command from the
+     * shell command line using "sh -c cmd".  Return the Process.
+     */
+    static Process spawn(const std::string& cmd) throw(IOException);
+
+    /**
+     * Send a signal to the process.
+     */
+    void kill(int signal) throw(IOException);
+
+    /**
+     * Do a system wait on a process.
+     * @return Process id of waited on process, or 0
+     *  if hang=false and process has not finished.
+     *  or -1 if process does not exist.
+     *
+     * @param hang: If true, wait for process state to change. If
+     *     hang=false, don't wait, return 0 if process state has not changed.
+     * @param status: status of process. See man 2 waitpid.
+     * After a successfull wait, any file descriptors connected to the
+     * process will be closed.
+     */
+    int wait(bool hang, int *status) throw(IOException);
+
+    /**
+     * Get the file descriptor of the pipe that is connected
+     * to the standard in of the Process.  This file descriptor
+     * is only valid after a spawn() of a Process.
+     * Bytes writen to this file descriptor can be then
+     * read by the Process.
+     */
+    int getInFd() const { return _infd; }
+
+    /**
+     * Get the ostream of the pipe that is connected
+     * to the standard in of the Process.  This ostream is
+     * only valid after a spawn() of a Process.
+     */
+    std::ostream& inStream() { return *_instream_ap.get(); }
+
+    /**
+     * Close the file descriptor and ostream of the pipe
+     * connected to the standard in of the Process.
+     * The Process will then receive an EOF if it
+     * reads from the other end of the pipe.
+     */
+    void closeIn();
+
+    /**
+     * Get the file descriptor of the pipe that is connected
+     * to the standard out of the Process.  This file descriptor
+     * is only valid after a spawn() of a Process.
+     * The output of the Process can be read from this file descriptor.
+     */
+    int getOutFd() const { return _outfd; }
+
+    /**
+     * Get the istream of the pipe that is connected
+     * to the standard out of the Process.  This istream
+     * is only valid after a spawn() of a Process.
+     * The output of the Process can be read from this istream.
+     */
+    std::istream& outStream() { return *_outstream_ap.get(); }
+
+    /**
+     * Close the file descriptor and ostream of the pipe
+     * connected to the standard out of the Process.
+     */
+    void closeOut();
+
+    /**
+     * Get the file descriptor of the pipe that is connected
+     * to the standard error of the Process.  This file descriptor
+     * is only valid after a spawn() of a Process.
+     * The error output of the Process can be read from this file descriptor.
+     */
+    int getErrFd() const { return _errfd; }
+
+    /**
+     * Get the istream of the pipe that is connected
+     * to the standard error of the Process.  This istream
+     * is only valid after a spawn() of a Process.
+     * The error output of the Process can be read from this istream.
+     */
+    std::istream& errStream() { return *_errstream_ap.get(); }
+
+    /**
+     * Close the file descriptor and ostream of the pipe
+     * connected to the standard error of the Process.
+     */
+    void closeErr();
+
+    /**
+     * The process id of the Process.
+     * @return: Will be -1 if this Process is not associated with
+     *      a valid process.
+     */
+    pid_t getPid() const { return _pid; }
+
+    /**
+     * Check if another process is running, using the named process id file.
      * A process id file typically has a name like /var/run/xxxx.pid,
      * or /tmp/xxxx.pid.  This function is useful when there should only be
      * one instance of a process running on a system - for
@@ -32,15 +199,19 @@ public:
      * The process name, "xxxx" should not be used by any other
      * process on the system.
      *
-     * @return 0: if no other process which uses file pidFile
-     *      appears to be running, meaning pidFile either doesn't exist,
-     *      is empty, does not contain a number, or a current process with the
-     *      given number doesn't exist.
-     *        >0: pid of process that either holds a lock on pidFile,
+     * @return 0: means that no other process which uses file pidFile
+     *      appears to be running. The pidFile was then created, containing
+     *      the process id of the current process.
+     *        >0: pid of a process that either holds a lock on pidFile,
      *          or pidFile contains a pid of a process that is still running.
-     * checkPidFile also calls atexit() to schedule the removePidFile()
-     * function to be run when the process exits.
-     * One typically does not have to call removePidFile explicitly.
+     *
+     *  If the return is 0, it means that the pidFile either didn't exist,
+     *  was empty, didn't contain a number, or a current process with the
+     *  given number doesn't exist. In this case, the file is created
+     *  if necessary, and will contain the process id of the current
+     *  process. checkPidFile also calls atexit() to schedule the
+     *  removePidFile() function to be run when the current process exits.
+     *  One typically does not have to call removePidFile explicitly.
      */
     static pid_t checkPidFile(const std::string& pidFile)
         throw(IOException);
@@ -55,6 +226,50 @@ private:
 
     static std::string _pidFile;
 
+    pid_t _pid;
+
+    /**
+     * File descriptor that is connected via a pipe to the
+     * standard in file descriptor of a spawned process.
+     * It is declared mutable to allow invalidating x._infd
+     * in the copy constructor and assignment operators
+     * that take a const Process& argument.
+     */
+    mutable int _infd;
+
+    void setInFd(int val);
+
+    /**
+     * GNU extension filebuf which is needed to create a ostream
+     * from a file descriptor.
+     */
+    mutable std::auto_ptr<__gnu_cxx::stdio_filebuf<char> > _inbuf_ap;
+
+    mutable std::auto_ptr<std::ostream> _instream_ap;
+
+    /**
+     * File descriptor that is connected via a pipe to the
+     * standard out file descriptor of a spawned process.
+     */
+    mutable int _outfd;
+
+    void setOutFd(int val);
+
+    mutable std::auto_ptr<__gnu_cxx::stdio_filebuf<char> > _outbuf_ap;
+
+    mutable std::auto_ptr<std::istream> _outstream_ap;
+
+    /**
+     * File descriptor that is connected via a pipe to the
+     * standard error file descriptor of a spawned process.
+     */
+    mutable int _errfd;
+
+    void setErrFd(int val);
+
+    mutable std::auto_ptr<__gnu_cxx::stdio_filebuf<char> > _errbuf_ap;
+
+    mutable std::auto_ptr<std::istream> _errstream_ap;
 };
 
 }}	// namespace nidas namespace util
