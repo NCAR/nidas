f43f61d105a6457badb038d17ecc9bd4822b07ea
Gordon Maclean <maclean@ucar.edu>
2009-01-22 21:59:05 +0000

    cleanup

--- scripts/make_doxy.sh
+++ scripts/make_doxy.sh
@@ -13,36 +13,22 @@
 ## $HeadURL$
 ##
 
-nidas_dir() {
-    if [ -z "$TMPDIR" ]; then
-	echo /tmp/nidas_doxy
-    else
-	echo $TMPDIR/nidas_doxy
-    fi
-}
+codir=${TMPDIR:-/tmp}/nidas_doxy/nidas
 
 svn_co_nidas() {
-    local dir=`nidas_dir`
-    if [ -d $dir ]; then
-	cd $dir
-	svn update nidas
+    if [ -d $codir ]; then
+	cd $codir || exit 1
+        if  ! svn update; then
+            svn cleanup || { cd $HOME; rm -rf $codir || exit 1; svn_co_nidas; }
+        fi
     else
-	mkdir -p $dir || exit 1
-	cd $dir || exit 1
+        [ -d ${codir%/*} ] || mkdir -p ${codir%/*}
+	cd ${codir%/*} || exit 1
 	svn co http://svn/svn/nidas/trunk nidas
     fi
 }
 
-codir=`nidas_dir`/nidas
-if [ ! -d $codir ]; then
-    svn_co_nidas
-else
-    cd $codir || exit 1
-    if  ! svn update; then
-        svn cleanup || { cd $HOME; rm -rf $codir; svn_co_nidas; }
-    fi
-fi
-
+svn_co_nidas
 cd $codir || exit 1
 
 doxygen doc/doxygen_conf/nidas.doxy
