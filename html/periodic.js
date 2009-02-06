f35d8a2167924f55f72dbfd3cabfeda52abf59b1
John Wasinger <wasinger@ucar.edu>
2009-02-06 21:35:15 +0000

    Increase the update rate of the status page.

--- html/periodic.js
+++ html/periodic.js
@@ -13,7 +13,7 @@ var periodic = {
 
   loop: function() {
     periodic.UpdateClocks();
-    if (++periodic.cnt > 3) {
+    if (++periodic.cnt > 1) {
       periodic.cnt=1;
       if (selectedDsm)
         recvResp( GetStatus[selectedDsm]() );
