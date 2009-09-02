213a9b6c9ec5c6da5b090a97bd1c589f7353fbdb
John Allison <jja@ucar.edu>
2009-09-02 17:12:49 +0000

    initial commit

---
+++ src/nidas/apps/config/Document.h
@@ -0,0 +1,14 @@
+/*
+ ********************************************************************
+    Copyright 2009 UCAR, NCAR, All Rights Reserved
+
+    $Id$
+ ********************************************************************
+ * Document.h
+ *  a nidas config xml document
+ */
+
+#ifndef _Document_h
+#define _Document_h
+
+#endif
