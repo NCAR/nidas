75ada691229b395d0fbb45655b38e32da08d7670
John Allison <jja@ucar.edu>
2009-09-02 17:29:16 +0000

    basic document declaration/definition

--- src/nidas/apps/config/Document.h
+++ src/nidas/apps/config/Document.h
@@ -11,4 +11,25 @@
 #ifndef _Document_h
 #define _Document_h
 
+
+class Document {
+
+public:
+
+    Document() {};
+    ~Document() { delete filename; };
+
+    const std::string getFilename() const { return filename; };
+    xercesc::DOMNode & getNode() const { return node; };
+
+    void setFilename(std::string f) { if (filename) delete filename; filename = new std::string(f); };
+    void setNode(xercesc::DOMNode & n) { node=n; };
+
+private:
+
+    std::string *filename;
+    xercesc::DOMNode node;
+
+};
+
 #endif
