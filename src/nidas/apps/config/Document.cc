a75a2ce12c64a2b9f95c92d2a20197226720c6a7
John Allison <jja@ucar.edu>
2009-09-11 00:11:10 +0000

    move xml code out of configwindow into Document

---
+++ src/nidas/apps/config/Document.cc
@@ -0,0 +1,124 @@
+
+#include "Document.h"
+
+
+#include <sys/param.h>
+#include <libgen.h>
+
+#include <xercesc/util/XMLUniDefs.hpp>
+#include <xercesc/dom/DOMWriter.hpp>
+#include <xercesc/dom/DOMImplementation.hpp>
+#include <xercesc/dom/DOMImplementationLS.hpp>
+
+#include <nidas/core/XMLParser.h>
+
+
+using namespace std;
+using namespace xercesc;
+using namespace nidas::core;
+
+
+
+static const XMLCh gLS[] = { chLatin_L, chLatin_S, chNull };
+static const XMLCh gNull[] = { chNull };
+
+
+
+const char *Document::getDirectory() const
+{
+static char buf[MAXPATHLEN];
+
+strncpy(buf,filename->c_str(),MAXPATHLEN-1);
+return dirname(buf);
+}
+
+
+
+void Document::writeDocument()
+{
+LocalFileFormatTarget *target;
+
+    try {
+        target = new LocalFileFormatTarget(filename->c_str());
+        if (!target) {
+            cerr << "target is null" << endl;
+            return;
+            }
+    } catch (...) {
+        cerr << "LocalFileFormatTarget new exception" << endl;
+        return;
+    }
+
+writeDOM(target,domdoc);
+delete target;
+return;
+}
+
+
+
+bool Document::writeDOM( XMLFormatTarget * const target, const DOMNode * node )
+{
+DOMImplementation *domimpl;
+DOMImplementationLS *lsimpl;
+DOMWriter *myWriter;
+
+
+    try {
+        domimpl = XMLImplementation::getImplementation();
+        //DOMImplementation *domimpl = DOMImplementationRegistry::getDOMImplementation(gLS);
+    } catch (...) {
+        cerr << "getImplementation exception" << endl;
+        return(false);
+    }
+        if (!domimpl) {
+            cerr << "xml implementation is null" << endl;
+            return(false);
+            }
+
+    try {
+        lsimpl =
+        // (DOMImplementationLS*)domimpl;
+         (domimpl->hasFeature(gLS,gNull)) ? (DOMImplementationLS*)domimpl : 0;
+    } catch (...) {
+        cerr << "hasFeature/cast exception" << endl;
+        return(false);
+    }
+
+        if (!lsimpl) {
+            cerr << "dom implementation is null" << endl;
+            return(false);
+            }
+
+    try {
+        myWriter = lsimpl->createDOMWriter();
+        if (!myWriter) {
+            cerr << "writer is null" << endl;
+            return(false);
+            }
+    } catch (...) {
+        cerr << "createDOMWriter exception" << endl;
+        return(false);
+    }
+
+        if (myWriter->canSetFeature(XMLUni::fgDOMWRTValidation, true))
+            myWriter->setFeature(XMLUni::fgDOMWRTValidation, true);
+        if (myWriter->canSetFeature(XMLUni::fgDOMWRTFormatPrettyPrint, true))
+            myWriter->setFeature(XMLUni::fgDOMWRTFormatPrettyPrint, true);
+
+        myWriter->setErrorHandler(&errorHandler);
+
+    try {
+        if (!myWriter->writeNode(target,*node)) {
+            cerr << "writeNode returns false" << endl;
+            }
+    } catch (...) {
+        cerr << "writeNode exception" << endl;
+        return(false);
+    }
+
+        target->flush();
+        myWriter->release();
+
+        return(true);
+}
+
--- src/nidas/apps/config/Document.h
+++ src/nidas/apps/config/Document.h
@@ -11,7 +11,18 @@
 #ifndef _Document_h
 #define _Document_h
 
-#include <libgen.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <xercesc/dom/DOMDocument.hpp>
+#include <xercesc/dom/DOMNode.hpp>
+#include <xercesc/dom/DOMErrorHandler.hpp>
+#include <xercesc/framework/LocalFileFormatTarget.hpp>
+
+
+using namespace std;
+
 
 
 class Document {
@@ -21,22 +32,31 @@ public:
     Document() { filename = 0; };
     ~Document() { delete filename; };
 
+    const char *getDirectory() const;
     const std::string getFilename() const { return *filename; };
-    xercesc::DOMDocument *getDomDocument() const { return domdoc; };
-
-    const char *getDirectory() {
-        static char buf[MAXPATHLEN];
-        strncpy(buf,filename->c_str(),MAXPATHLEN-1);
-        return dirname(buf);
-        }
-
     void setFilename(const std::string &f) { if (filename) delete filename; filename = new std::string(f); };
+
+    xercesc::DOMDocument *getDomDocument() const { return domdoc; };
     void setDomDocument(xercesc::DOMDocument *d) { domdoc=d; };
+    void writeDocument();
+
+        // can't be const because of errorhandler, we'd need to decouple that
+    bool writeDOM( xercesc::XMLFormatTarget * const target, const xercesc::DOMNode * node );
 
 private:
 
     std::string *filename;
     xercesc::DOMDocument *domdoc;
+
+    // stoopid error handler for development/testing
+    // can't be inner class so writeDOM can be const
+    class MyErrorHandler : public xercesc::DOMErrorHandler {
+    public:
+        bool handleError(const xercesc::DOMError & domError) {
+            cerr << domError.getMessage() << endl;
+            return true;
+        }
+    } errorHandler;
 
 };
 
