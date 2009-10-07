237b4f46c981ad1fda87abfee8ec4326215ec19a
John Allison <jja@ucar.edu>
2009-10-07 18:09:09 +0000

    initial commit of using Qt designer file/class

---
+++ src/nidas/apps/config/AddSensorDialog.h
@@ -0,0 +1,21 @@
+#ifndef _config_AddSensorDialog_h
+#define _config_AddSensorDialog_h
+
+#include "ui_AddSensorDialog.h"
+
+namespace config
+{
+
+class AddSensorDialog : public Ui_AddSensorDialog
+{
+    Q_OBJECT
+
+public:
+
+signals:
+
+};
+
+}
+
+#endif
