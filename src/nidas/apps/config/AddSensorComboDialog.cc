0a86b575c34dd0862ff144aed8140943214ee869
Tom Baltzer <tbaltzer@ucar.edu>
2009-10-26 20:39:28 +0000

    Verify that validation works.

--- src/nidas/apps/config/AddSensorComboDialog.cc
+++ src/nidas/apps/config/AddSensorComboDialog.cc
@@ -9,17 +9,20 @@ QRegExp _idRegEx("\\d+");
 AddSensorComboDialog::AddSensorComboDialog(QWidget *parent) :
     QDialog(parent)
 {
-setupUi(this);
-DeviceText->setValidator( new QRegExpValidator (_deviceRegEx, this ));
-IdText->setValidator( new QRegExpValidator ( _idRegEx, this));
+  setupUi(this);
+  DeviceText->setValidator( new QRegExpValidator (_deviceRegEx, this ));
+  IdText->setValidator( new QRegExpValidator ( _idRegEx, this));
 }
 
 
 void AddSensorComboDialog::accept()
 {
-
-        std::cerr << "AddSensorComboDialog::accept()\n";
-        std::cerr << " " + SensorBox->currentText().toStdString() + "\n";
-        std::cerr << " " + DeviceText->text().toStdString() + "\n";
-        std::cerr << " " + IdText->text().toStdString() + "\n";
+  if (DeviceText->hasAcceptableInput() && IdText->hasAcceptableInput()) {
+     std::cerr << "AddSensorComboDialog::accept()\n";
+     std::cerr << " " + SensorBox->currentText().toStdString() + "\n";
+     std::cerr << " " + DeviceText->text().toStdString() + "\n";
+     std::cerr << " " + IdText->text().toStdString() + "\n";
+  }  else {
+     std::cerr << "Unaccptable input in either Device or Id fields\n";
+  }
 }
