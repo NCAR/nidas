ffe5c8e87f34f67198bafd68a58477af75848c1d
Ling-Ling Dong <dongl@ucar.edu>
2009-11-16 20:40:42 +0000

    add TP01, all type data add dynamic in the var-infor structure add 0x0e in the cockpit plot
    ...

--- src/nidas/dynld/isff/WisardMote.cc
+++ src/nidas/dynld/isff/WisardMote.cc
@@ -118,7 +118,6 @@ void WisardMote::addSampleTag(SampleTag* stag) throw(n_u::InvalidParameterExcept
 
 		SampleTag* newtag = new SampleTag(*stag);
 		newtag->setSampleId(newtag->getSampleId()+id);
-
 		int nv = sizeof(_samps[i].variables)/sizeof(_samps[i].variables[0]);
 
 		//vars
@@ -130,8 +129,8 @@ void WisardMote::addSampleTag(SampleTag* stag) throw(n_u::InvalidParameterExcept
 			var->setName(vinf.name);
 			var->setUnits(vinf.units);
 			var->setLongName(vinf.longname);
+			var->setDynamic(vinf.dynamic);
 			var->setLength(len);
-			var->setDynamic(true);
 			var->setSuffix(newtag->getSuffix());
 
 			newtag->addVariable(var);
@@ -271,6 +270,37 @@ const unsigned char* WisardMote::readPicTm(const unsigned char* cp, const unsign
 	return cp;
 
 }
+
+/* type id 0x04 */
+const unsigned char* WisardMote::readGenShort(const unsigned char* cp, const unsigned char* eos)
+{
+	/* unpack  16 bit gen-short */
+	int	val = missValue;
+	if (cp + sizeof(uint16_t) <= eos) val = _fromLittle->uint16Value(cp);
+	if (val!= missValue)
+		_data.push_back(val/1.0);
+	else
+		_data.push_back(floatNAN);
+	cp += sizeof(uint16_t);
+	return cp;
+
+}
+
+/* type id 0x05 */
+const unsigned char* WisardMote::readGenLong(const unsigned char* cp, const unsigned char* eos)
+{
+	/* unpack  32 bit gen-long */
+	int	val = missLongValue;
+	if (cp + sizeof(uint32_t) <= eos) val = _fromLittle->uint32Value(cp);
+	if (val!= missLongValue)
+		_data.push_back(val/1.0);
+	else
+		_data.push_back(floatNAN);
+	cp += sizeof(uint16_t);
+	return cp;
+
+}
+
 
 /* type id 0x0B */
 const unsigned char* WisardMote::readTmSec(const unsigned char* cp, const unsigned char* eos)
@@ -417,18 +447,56 @@ const unsigned char* WisardMote::readQsoilData(const unsigned char* cp, const un
 /* type id 0x2C-0x2F */
 const unsigned char* WisardMote::readTP01Data(const unsigned char* cp, const unsigned char* eos)
 {
-	// 3 are signed
-	for (int i=0; i<3; i++) {
+	// 5 signed
+	for (int i=0; i<5; i++) {
 		int val = (signed)0xFFFF8000;   // signed
 		if (cp + sizeof(int16_t) <= eos) val = _fromLittle->int16Value(cp);
 		cp += sizeof(int16_t);
-		if (val!= (signed)0xFFFF8000)
-			_data.push_back(val/1.0);
+		if (val!= (signed)0xFFFF8000){
+			switch (i) {
+			case 0: _data.push_back(val/10000.0);
+			case 1: _data.push_back(val/1.0);
+			case 2: _data.push_back(val/1.0);
+			case 3: _data.push_back(val/100.0);
+			case 4: _data.push_back(val/1000.0);
+			}
+		}
 		else
 			_data.push_back(floatNAN);
 	}
 	return cp;
 }
+
+/* type id 0x40 status-id */
+const unsigned char* WisardMote::readStatusData(const unsigned char* cp, const unsigned char* eos)
+{
+	int val = missByteValue;
+	if (cp + 1 <= eos) val = *cp++;
+	if (val!= missByteValue)
+		_data.push_back(val);
+	else
+		_data.push_back(floatNAN);
+	return cp;
+
+}
+
+/* type id 0x49 pwr */
+const unsigned char* WisardMote::readPwrData(const unsigned char* cp, const unsigned char* eos)
+{
+	for (int i=0; i<6; i++){
+		int val = missValue;
+		if (cp + sizeof(uint16_t) <= eos) val = _fromLittle->uint16Value(cp);
+		cp += sizeof(uint16_t);
+		if (val!= missValue) {
+			if (i==0 || i==3) 	_data.push_back(val/10.0);  //voltage 10th
+			else _data.push_back(val/1.0);					//miliamp
+		} else
+			_data.push_back(floatNAN);
+	}
+	return cp;
+}
+
+
 
 /* type id 0x50-0x53 */
 const unsigned char* WisardMote::readRnetData(const unsigned char* cp, const unsigned char* eos)
@@ -476,42 +544,15 @@ const unsigned char* WisardMote::readRlwData(const unsigned char* cp, const unsi
 
 
 /* type id 0x64-0x6B */
-const unsigned char* WisardMote::readRlwKData(const unsigned char* cp, const unsigned char* eos)
+const unsigned char* WisardMote::readRlwKZData(const unsigned char* cp, const unsigned char* eos)
 {
 	for (int i=0; i<2; i++) {
 		int val = (signed)0xFFFF8000;   // signed
 		if (cp + sizeof(int16_t) <= eos) val = _fromLittle->int16Value(cp);
 		cp += sizeof(int16_t);
 		if (val != (signed)0xFFFF8000 ) {
-			_data.push_back(val/100.0);          // RPile, tcase
-		} else
-			_data.push_back(floatNAN);
-	}
-	return cp;
-}
-
-/* type id 0x40 */
-const unsigned char* WisardMote::readStatusData(const unsigned char* cp, const unsigned char* eos)
-{
-	int val = missByteValue;
-	if (cp + 1 <= eos) val = *cp++;
-	if (val!= missByteValue)
-		_data.push_back(val);
-	else
-		_data.push_back(floatNAN);
-	return cp;
-
-}
-
-const unsigned char* WisardMote::readPwrData(const unsigned char* cp, const unsigned char* eos)
-{
-	for (int i=0; i<6; i++){
-		int val = missValue;
-		if (cp + sizeof(uint16_t) <= eos) val = _fromLittle->uint16Value(cp);
-		cp += sizeof(uint16_t);
-		if (val!= missValue) {
-			if (i==0 || i==3) 	_data.push_back(val/10.0);  //voltage 10th
-			else _data.push_back(val/1.0);
+			if (i==0) _data.push_back(val/10.0);          // RPile
+			else _data.push_back(val/100.0);          	// tcase
 		} else
 			_data.push_back(floatNAN);
 	}
@@ -522,6 +563,9 @@ const unsigned char* WisardMote::readPwrData(const unsigned char* cp, const unsi
 void WisardMote::initFuncMap() {
 	if (! _functionsMapped) {
 		_nnMap[0x01] = &WisardMote::readPicTm;
+		_nnMap[0x04] = &WisardMote::readGenShort;
+		_nnMap[0x05] = &WisardMote::readGenLong;
+
 		_nnMap[0x0B] = &WisardMote::readTmSec;
 		_nnMap[0x0C] = &WisardMote::readTmCnt;
 		_nnMap[0x0D] = &WisardMote::readTm100thSec;
@@ -576,148 +620,194 @@ void WisardMote::initFuncMap() {
 		_nnMap[0x62] = &WisardMote::readRlwData;
 		_nnMap[0x63] = &WisardMote::readRlwData;
 
-		_nnMap[0x64] = &WisardMote::readRlwKData;
-		_nnMap[0x65] = &WisardMote::readRlwKData;
-		_nnMap[0x66] = &WisardMote::readRlwKData;
-		_nnMap[0x67] = &WisardMote::readRlwKData;
+		_nnMap[0x64] = &WisardMote::readRlwKZData;
+		_nnMap[0x65] = &WisardMote::readRlwKZData;
+		_nnMap[0x66] = &WisardMote::readRlwKZData;
+		_nnMap[0x67] = &WisardMote::readRlwKZData;
 
-		_nnMap[0x68] = &WisardMote::readRlwKData;
-		_nnMap[0x69] = &WisardMote::readRlwKData;
-		_nnMap[0x6A] = &WisardMote::readRlwKData;
-		_nnMap[0x6B] = &WisardMote::readRlwKData;
+		_nnMap[0x68] = &WisardMote::readRlwKZData;
+		_nnMap[0x69] = &WisardMote::readRlwKZData;
+		_nnMap[0x6A] = &WisardMote::readRlwKZData;
+		_nnMap[0x6B] = &WisardMote::readRlwKZData;
 		_functionsMapped = true;
 	}
 }
 
 SampInfo WisardMote::_samps[] = {
+		{0x0E, {{"TTime-kicks","secs","Total Time kick", true},}},
+
 		{0x20,{
-				{"Tsoil.a.1","degC","Soil Temperature"},
-				{"Tsoil.a.2","degC","Soil Temperature"},
-				{"Tsoil.a.3","degC","Soil Temperature"},
-				{"Tsoil.a.4","degC","Soil Temperature"}, }
+				{"Tsoil.a.1","degC","Soil Temperature", true},
+				{"Tsoil.a.2","degC","Soil Temperature", true},
+				{"Tsoil.a.3","degC","Soil Temperature", true},
+				{"Tsoil.a.4","degC","Soil Temperature", true}, }
 		},
 		{0x21,{
-				{"Tsoil.b.1","degC","Soil Temperature"},
-				{"Tsoil.b.2","degC","Soil Temperature"},
-				{"Tsoil.b.3","degC","Soil Temperature"},
-				{"Tsoil.b.4","degC","Soil Temperature"}, }
+				{"Tsoil.b.1","degC","Soil Temperature", true},
+				{"Tsoil.b.2","degC","Soil Temperature", true},
+				{"Tsoil.b.3","degC","Soil Temperature", true},
+				{"Tsoil.b.4","degC","Soil Temperature", true}, }
 		},
 		{0x22,{
-				{"Tsoil.c.1","degC","Soil Temperature"},
-				{"Tsoil.c.2","degC","Soil Temperature"},
-				{"Tsoil.c.3","degC","Soil Temperature"},
-				{"Tsoil.c.4","degC","Soil Temperature"}, }
+				{"Tsoil.c.1","degC","Soil Temperature", true},
+				{"Tsoil.c.2","degC","Soil Temperature", true},
+				{"Tsoil.c.3","degC","Soil Temperature", true},
+				{"Tsoil.c.4","degC","Soil Temperature", true}, }
 		},
 		{0x23,{
-				{"Tsoil.d.1","degC","Soil Temperature"},
-				{"Tsoil.d.2","degC","Soil Temperature"},
-				{"Tsoil.d.3","degC","Soil Temperature"},
-				{"Tsoil.d.4","degC","Soil Temperature"}, }
+				{"Tsoil.d.1","degC","Soil Temperature", true},
+				{"Tsoil.d.2","degC","Soil Temperature", true},
+				{"Tsoil.d.3","degC","Soil Temperature", true},
+				{"Tsoil.d.4","degC","Soil Temperature", true}, }
 		},
 
-		{0x24, {{"Gsoil.a", "W/m^2", "Soil Heat Flux"},}},
-		{0x25, {{"Gsoil.b", "W/m^2", "Soil Heat Flux"},}},
-		{0x26, {{"Gsoil.c", "W/m^2", "Soil Heat Flux"},}},
-		{0x27, {{"Gsoil.d", "W/m^2", "Soil Heat Flux"},}},
+		{0x24, {{"Gsoil.a", "W/m^2", "Soil Heat Flux", true},}},
+		{0x25, {{"Gsoil.b", "W/m^2", "Soil Heat Flux", true},}},
+		{0x26, {{"Gsoil.c", "W/m^2", "Soil Heat Flux", true},}},
+		{0x27, {{"Gsoil.d", "W/m^2", "Soil Heat Flux", true},}},
 
-		{0x28,{{"QSoil.a", "vol%", "Soil Moisture"},}},
-		{0x29,{{"QSoil.b", "vol%", "Soil Moisture"},}},
-		{0x2A,{{"QSoil.c", "vol%", "Soil Moisture"},}},
-		{0x2B,{{"QSoil.d", "vol%", "Soil Moisture"},}},
+		{0x28,{{"QSoil.a", "vol%", "Soil Moisture", true},}},
+		{0x29,{{"QSoil.b", "vol%", "Soil Moisture", true},}},
+		{0x2A,{{"QSoil.c", "vol%", "Soil Moisture", true},}},
+		{0x2B,{{"QSoil.d", "vol%", "Soil Moisture", true},}},
 
 		{0x2C,{
-				{"Vpile.a","V","Soil Thermal, transducer volt"},
-				{"Vheat.a","V","Soil Thermal, heat volt"},
-				{"Tau63.a","secs","Soil Thermal, time diff"}, }
+				{"Vheat.a","V","Soil Thermal, heat volt", true},
+				{"Vpile-on.a","microV","Soil Thermal, transducer volt", true},
+				{"Vpile-off.a","microV","Soil Thermal, heat volt", true},
+				{"Tau63.a","secs","Soil Thermal, time diff", true},
+				{"L.a","W/mDegk","Thermal property", true}, }
 		},
 		{0x2D,{
-				{"Vpile.b","V","Soil Thermal, transducer volt"},
-				{"Vheat.b","V","Soil Thermal, heat volt"},
-				{"Tau63.b","secs","Soil Thermal, time diff"}, }
+				{"Vheat.b","V","Soil Thermal, heat volt", true},
+				{"Vpile-on.b","microV","Soil Thermal, transducer volt", true},
+				{"Vpile-off.b","microV","Soil Thermal, heat volt", true},
+				{"Tau63.b","secs","Soil Thermal, time diff", true},
+				{"L.b","W/mDegk","Thermal property", true}, }
 		},
 		{0x2E,{
-				{"Vpile.c","V","Soil Thermal, transducer volt"},
-				{"Vheat.c","V","Soil Thermal, heat volt"},
-				{"Tau63.c","secs","Soil Thermal, time diff"}, }
+				{"Vheat.c","V","Soil Thermal, heat volt", true},
+				{"Vpile-on.c","microV","Soil Thermal, transducer volt", true},
+				{"Vpile-off.c","microV","Soil Thermal, heat volt", true},
+				{"Tau63.c","secs","Soil Thermal, time diff", true},
+				{"L.c","W/mDegk","Thermal property", true}, }
 		},
 		{0x2F,{
-				{"Vpile.d","V","Soil Thermal, transducer volt"},
-				{"Vheat.d","V","Soil Thermal, heat volt"},
-				{"Tau63.d","secs","Soil Thermal, time diff"}, }
+				{"Vheat.d","V","Soil Thermal, heat volt", true},
+				{"Vpile-on.d","microV","Soil Thermal, transducer volt", true},
+				{"Vpile-off.d","microV","Soil Thermal, heat volt", true},
+				{"Tau63.d","secs","Soil Thermal, time diff", true},
+				{"L.d","W/mDegk","Thermal property", true}, }
 		},
 
-		{0x50, {{"Rnet.a","W/m^2","Net Radiation"},}},
-		{0x51, {{"Rnet.b","W/m^2","Net Radiation"},}},
-		{0x52, {{"Rnet.c","W/m^2","Net Radiation"},}},
-		{0x53, {{"Rnet.d","W/m^2","Net Radiation"},}},
+		{0x40, {{"StatusId","Count","Sampling mode", true},}},
 
-		{0x54, {{"Rsw.in.a","W/m^2","Incoming Short Wave"},}},
-		{0x55, {{"Rsw.in.b","W/m^2","Incoming Short Wave"},}},
-		{0x56, {{"Rsw.in.c","W/m^2","Incoming Short Wave"},}},
-		{0x57, {{"Rsw.in.d","W/m^2","Incoming Short Wave"},}},
+		{0x50, {{"Rnet.a","W/m^2","Net Radiation", true},}},
+		{0x51, {{"Rnet.b","W/m^2","Net Radiation", true},}},
+		{0x52, {{"Rnet.c","W/m^2","Net Radiation", true},}},
+		{0x53, {{"Rnet.d","W/m^2","Net Radiation", true},}},
 
-		{0x58, {{"Rsw.out.a","W/m^2","Outgoing Short Wave"},}},
-		{0x59, {{"Rsw.out.b","W/m^2","Outgoing Short Wave"},}},
-		{0x5A, {{"Rsw.out.c","W/m^2","Outgoing Short Wave"},}},
-		{0x5B, {{"Rsw.out.d","W/m^2","Outgoing Short Wave"},}},
+		{0x54, {{"Rsw.i.a","W/m^2","Incoming Short Wave", true},}},
+		{0x55, {{"Rsw.i.b","W/m^2","Incoming Short Wave", true},}},
+		{0x56, {{"Rsw.i.c","W/m^2","Incoming Short Wave", true},}},
+		{0x57, {{"Rsw.i.d","W/m^2","Incoming Short Wave", true},}},
+
+		{0x58, {{"Rsw.o.a","W/m^2","Outgoing Short Wave", true},}},
+		{0x59, {{"Rsw.o.b","W/m^2","Outgoing Short Wave", true},}},
+		{0x5A, {{"Rsw.o.c","W/m^2","Outgoing Short Wave", true},}},
+		{0x5B, {{"Rsw.o.d","W/m^2","Outgoing Short Wave", true},}},
 
 		{0x5C,{
-				{"Rlw.in.tpile.a","degC","Incoming Long Wave"},
-				{"Rlw-in.tcase.a","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome1.a","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome2.a","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome3.a","degC","Incoming Long Wave"},}
+				{"Rlw.i.tpile.a","W/m^2","Incoming Long Wave", true},
+				{"Rlw.i.tcase.a","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome1.a","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome2.a","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome3.a","degC","Incoming Long Wave", true},}
 		},
 		{0x5D,{
-				{"Rlw.in.tpile.b","degC","Incoming Long Wave"},
-				{"Rlw-in.tcase.b","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome1.b","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome2.b","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome3.b","degC","Incoming Long Wave"},}
+				{"Rlw.i.tpile.b","W/m^2","Incoming Long Wave", true},
+				{"Rlw.i.tcase.b","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome1.b","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome2.b","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome3.b","degC","Incoming Long Wave", true},}
 		},
 		{0x5E,{
-				{"Rlw.in.tpile.c","degC","Incoming Long Wave"},
-				{"Rlw-in.tcase.c","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome1.c","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome2.c","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome3.c","degC","Incoming Long Wave"},}
+				{"Rlw.i.tpile.c","W/m^2","Incoming Long Wave", true},
+				{"Rlw.i.tcase.c","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome1.c","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome2.c","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome3.c","degC","Incoming Long Wave", true},}
 		},
 		{0x5F,{
-				{"Rlw.in.tpile.d","degC","Incoming Long Wave"},
-				{"Rlw-in.tcase.d","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome1.d","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome2.d","degC","Incoming Long Wave"},
-				{"Rlw-in.tdome3.d","degC","Incoming Long Wave"},}
+				{"Rlw.i.tpile.d","W/m^2","Incoming Long Wave", true},
+				{"Rlw.i.tcase.d","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome1.d","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome2.d","degC","Incoming Long Wave", true},
+				{"Rlw.i.tdome3.d","degC","Incoming Long Wave", true},}
 		},
 
 		{0x60,{
-				{"Rlw.out.tpile.a","degC","Outgoing Long Wave"},
-				{"Rlw-out.tcase.a","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome1.a","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome2.a","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome3.a","degC","Outgoing Long Wave"},}
+				{"Rlw-o.tpile.a","W/m^2","Outgoing Long Wave", true},
+				{"Rlw-o.tcase.a","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome1.a","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome2.a","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome3.a","degC","Outgoing Long Wave", true},}
 		},
 		{0x61,{
-				{"Rlw.out.tpile.b","degC","Outgoing Long Wave"},
-				{"Rlw-out.tcase.b","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome1.b","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome2.b","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome3.b","degC","Outgoing Long Wave"},}
+				{"Rlw-o.tpile.b","W/m^2","Outgoing Long Wave", true},
+				{"Rlw-o.tcase.b","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome1.b","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome2.b","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome3.b","degC","Outgoing Long Wave", true},}
 		},
 		{0x62,{
-				{"Rlw.out.tpile.c","degC","Outgoing Long Wave"},
-				{"Rlw-out.tcase.c","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome1.c","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome2.c","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome3.c","degC","Outgoing Long Wave"},}
+				{"Rlw-o.tpile.c","W/m^2","Outgoing Long Wave", true},
+				{"Rlw-o.tcase.c","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome1.c","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome2.c","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome3.c","degC","Outgoing Long Wave", true},}
 		},
 		{0x63,{
-				{"Rlw.out.tpile.d","degC","Outgoing Long Wave"},
-				{"Rlw-out.tcase.d","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome1.d","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome2.d","degC","Outgoing Long Wave"},
-				{"Rlw-out.tdome3.d","degC","Outgoing Long Wave"},}
+				{"Rlw-o.tpile.d","W/m^2","Outgoing Long Wave", true},
+				{"Rlw-o.tcase.d","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome1.d","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome2.d","degC","Outgoing Long Wave", true},
+				{"Rlw-o.tdome3.d","degC","Outgoing Long Wave", true},}
 		},
+
+		{0x64,{
+				{"Rlwk.i.Rpile.a","W/m^2","Incoming Long Wave K&Z", true},
+				{"Rlwk.i.tcase.a","degC","Incoming Long Wave K&Z", true},}
+		},
+		{0x65,{
+				{"Rlwk.i.Rpile.b","W/m^2","Incoming Long Wave K&Z", true},
+				{"Rlwk.i.tcase.b","degC","Incoming Long Wave K&Z", true},}
+		},
+		{0x66,{
+				{"Rlwk.i.Rpile.c","W/m^2","Incoming Long Wave K&Z", true},
+				{"Rlwk.i.tcase.c","degC","Incoming Long Wave K&Z", true},}
+		},
+		{0x67,{
+				{"Rlwk.i.Rpile.d","W/m^2","Incoming Long Wave K&Z", true},
+				{"Rlwk.i.tcase.d","degC","Incoming Long Wave K&Z", true},}
+		},
+		{0x68,{
+				{"Rlwk.o.Rpile.a","W/m^2","Outgoing  Long Wave K&Z", true},
+				{"Rlwk.o.tcase.a","degC","Outgoing  Long Wave K&Z", true},}
+		},
+		{0x69,{
+				{"Rlwk.o.Rpile.b","W/m^2","Outgoing  Long Wave K&Z", true},
+				{"Rlwk.o.tcase.b","degC","Outgoing  Long Wave K&Z", true},}
+		},
+		{0x6A,{
+				{"Rlwk.o.Rpile.c","W/m^2","Outgoing  Long Wave K&Z", true},
+				{"Rlwk.o.tcase.c","degC","Outgoing  Long Wave K&Z", true},}
+		},
+		{0x6B,{
+				{"Rlwk.o.Rpile.d","W/m^2","Outgoing  Long Wave K&Z", true},
+				{"Rlwk.o.tcase.d","degC","Outgoing  Long Wave K&Z", true},}
+		},
+
 		{0,{{},}}
 
 };
--- src/nidas/dynld/isff/WisardMote.h
+++ src/nidas/dynld/isff/WisardMote.h
@@ -37,15 +37,16 @@ using namespace nidas::dynld;
 
 struct VarInfo
 {
-    const char* name;
-    const char* units;
-    const char* longname;
+	const char* name;
+	const char* units;
+	const char* longname;
+	bool 		dynamic;
 };
 
 struct SampInfo
 {
-    unsigned int id;
-    struct VarInfo variables[5];
+	unsigned int id;
+	struct VarInfo variables[5];
 };
 
 
@@ -53,104 +54,108 @@ struct SampInfo
 class WisardMote: public DSMSerialSensor {
 public:
 	static const  int missValue = 0x8001;
+	static const  int missLongValue = 0xFFFF8001;
 	static const  int missByteValue = 0x80;
 
 	WisardMote();
 
-    virtual ~WisardMote() {};
+	virtual ~WisardMote() {};
 
-    bool process(const Sample* insamp,  list<const Sample*>& results) throw() ;
+	bool process(const Sample* insamp,  list<const Sample*>& results) throw() ;
 
-    typedef const unsigned char*(WisardMote::*readFunc)(const unsigned char* cp, const unsigned char* eos);
+	typedef const unsigned char*(WisardMote::*readFunc)(const unsigned char* cp, const unsigned char* eos);
 
 private:
-    static const nidas::util::EndianConverter* _fromLittle;
+	static const nidas::util::EndianConverter* _fromLittle;
 
-    /**
-     * Mote id, read from initial digits in message, up to colon.
-     * For example the number XX from "IDXX:"
-     */
-    int _moteId;
+	/**
+	 * Mote id, read from initial digits in message, up to colon.
+	 * For example the number XX from "IDXX:"
+	 */
+	int _moteId;
 
-    /**
-     * Sensor serial numbers, from message.
-     */
-    std::map<int,int> _sensorSerialNumbersByType;
+	/**
+	 * Sensor serial numbers, from message.
+	 */
+	std::map<int,int> _sensorSerialNumbersByType;
 
-    /**
-     * Version number of current message.
-     */
-    int _version;
+	/**
+	 * Version number of current message.
+	 */
+	int _version;
 
-    int _sequence;
+	int _sequence;
 
-    int _badCRCs;
+	int _badCRCs;
 
-    /**
-     * data unpacked from message.
-     */
-    vector<float> _data;
+	/**
+	 * data unpacked from message.
+	 */
+	vector<float> _data;
 
-    /**
-     * overwrite addSampleTag
-     * Add a sample to this sensor.
-     * Throw an exception the DSMSensor cannot support
-     * the sample (bad rate, wrong number of variables, etc).
-     * DSMSensor will own the pointer.
-     * Note that a SampleTag may be changed after it has
-     * been added. addSampleTag() is called when a sensor is initialized
-     * from the sensor catalog.  The SampleTag may be modified later
-     * if it is overridden in the actual sensor entry.
-     * For this reason, it is probably better to scan the SampleTags
-     * of a DSMSensor in the validate(), init() or open() methods.
-     */
-    void addSampleTag(SampleTag* val) throw(nidas::util::InvalidParameterException);
+	/**
+	 * overwrite addSampleTag
+	 * Add a sample to this sensor.
+	 * Throw an exception the DSMSensor cannot support
+	 * the sample (bad rate, wrong number of variables, etc).
+	 * DSMSensor will own the pointer.
+	 * Note that a SampleTag may be changed after it has
+	 * been added. addSampleTag() is called when a sensor is initialized
+	 * from the sensor catalog.  The SampleTag may be modified later
+	 * if it is overridden in the actual sensor entry.
+	 * For this reason, it is probably better to scan the SampleTags
+	 * of a DSMSensor in the validate(), init() or open() methods.
+	 */
+	void addSampleTag(SampleTag* val) throw(nidas::util::InvalidParameterException);
 
-    /**
-     * Check for correct EOM. Return pointer to the beginning of the eom,
-     * (which is one past the CRC) or NULL if a correct EOM is not found.
-     */
-    const unsigned char* checkEOM(const unsigned char* cp, const unsigned char* eom);
+	/**
+	 * Check for correct EOM. Return pointer to the beginning of the eom,
+	 * (which is one past the CRC) or NULL if a correct EOM is not found.
+	 */
+	const unsigned char* checkEOM(const unsigned char* cp, const unsigned char* eom);
 
-    /**
-     * Verify crc. Return pointer to the CRC (which is one past the end of the data),
-     * or NULL if a correct CRC is not found.
-     */
-    const unsigned char* checkCRC (const unsigned char* cp, const unsigned char* eom);
+	/**
+	 * Verify crc. Return pointer to the CRC (which is one past the end of the data),
+	 * or NULL if a correct CRC is not found.
+	 */
+	const unsigned char* checkCRC (const unsigned char* cp, const unsigned char* eom);
 
-    /**
-     * Read mote id, find ID#, :, seq#, and msgType. Return msgType.
-     */
-    int readHead(const unsigned char* &cp, const unsigned char* eom);
+	/**
+	 * Read mote id, find ID#, :, seq#, and msgType. Return msgType.
+	 */
+	int readHead(const unsigned char* &cp, const unsigned char* eom);
 
-    /* claim methods to retrieve sensorType data    */
-    const unsigned char* readPicTm(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readTmSec(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readTmCnt(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readTm100thSec(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readTm10thSec(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readPicDT(const unsigned char* cp, const unsigned char* eos);
+	/* claim methods to retrieve sensorType data    */
+	const unsigned char* readPicTm(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readGenShort(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readGenLong(const unsigned char* cp, const unsigned char* eos);
 
-    const unsigned char* readTsoilData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readGsoilData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readQsoilData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readTP01Data(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTmCnt(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTmSec(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTm100thSec(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTm10thSec(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readPicDT(const unsigned char* cp, const unsigned char* eos);
 
-    const unsigned char* readRnetData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readRswData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readRlwData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readRlwKData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTsoilData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readGsoilData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readQsoilData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readTP01Data(const unsigned char* cp, const unsigned char* eos);
 
-    const unsigned char* readStatusData(const unsigned char* cp, const unsigned char* eos);
-    const unsigned char* readPwrData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readRnetData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readRswData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readRlwData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readRlwKZData(const unsigned char* cp, const unsigned char* eos);
 
-    static SampInfo _samps[];
+	const unsigned char* readStatusData(const unsigned char* cp, const unsigned char* eos);
+	const unsigned char* readPwrData(const unsigned char* cp, const unsigned char* eos);
 
-    static bool _functionsMapped;
+	static SampInfo _samps[];
 
-    static std::map<unsigned char, WisardMote::readFunc> _nnMap;
+	static bool _functionsMapped;
 
-    static void initFuncMap();
+	static std::map<unsigned char, WisardMote::readFunc> _nnMap;
+
+	static void initFuncMap();
 };
 }}} // nidas::dynld::isff
 #endif /* WISARDMOTE_H_ */
