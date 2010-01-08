3326b750eef0c104e93243a51c4045e02fd3828f
Gordon Maclean <maclean@ucar.edu>
2010-01-08 19:33:47 +0000

    fix pointer to memset

--- src/nidas/linux/arinc/main.c
+++ src/nidas/linux/arinc/main.c
@@ -336,10 +336,10 @@ static int arinc_open(struct inode *inode, struct file *filp)
         memset(&dev->read_state,0,
             sizeof(struct sample_read_state));
 	memset(&dev->status,0,sizeof(dsm_arinc_status));
-        memset(&dev->rate,0,sizeof(dev->rate));
+        memset(dev->rate,0,sizeof(dev->rate));
         // don't think it is necessary to zero msg_id and arcfgs
-        // memset(&dev->msg_id,0,sizeof(dev->msg_id));
-        // memset(&dev->arcfgs,0,sizeof(dev->arcfgs));
+        // memset(dev->msg_id,0,sizeof(dev->msg_id));
+        // memset(dev->arcfgs,0,sizeof(dev->arcfgs));
         dev->nArcfg = 0;
         dev->nSweeps = 0;
         dev->sim_xmit = 0;
