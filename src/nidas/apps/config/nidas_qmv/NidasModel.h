b2c9f90b6f2ef8a4b0190afefd3f308307b96359
John Allison <jja@ucar.edu>
2010-01-21 18:43:49 +0000

    remove unused decl

--- src/nidas/apps/config/nidas_qmv/NidasModel.h
+++ src/nidas/apps/config/nidas_qmv/NidasModel.h
@@ -21,8 +21,6 @@ public:
 
     QVariant data(const QModelIndex &index, int role) const;
     Qt::ItemFlags flags(const QModelIndex &index) const;
-    QVariant headerData(int section, Qt::Orientation orientation,
-                        int role = Qt::DisplayRole) const;
     QModelIndex index(int row, int column,
                       const QModelIndex &parent = QModelIndex()) const;
     QModelIndex parent(const QModelIndex &child) const;
