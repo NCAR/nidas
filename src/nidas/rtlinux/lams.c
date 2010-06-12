29a3ba8bd7595a68f01a8e6d7bda78407778c3af
Gordon Maclean <maclean@ucar.edu>
2010-06-12 03:48:45 +0000

    Updated macros in rtlinux driver to match new lams header. Didn't I do this weeks ago???

--- src/nidas/rtlinux/lams.c
+++ src/nidas/rtlinux/lams.c
@@ -68,16 +68,14 @@ static rtl_sem_t threadSem;
 static rtl_pthread_t lamsThread = 0;
 static int fd_lams_data = 0;
 static struct lamsPort _lamsPort;
-static struct lams_status status = {0};
 
 static struct ioctlCmd ioctlcmds[] = {
-   { GET_NUM_PORTS,       _IOC_SIZE(GET_NUM_PORTS) },
-   { LAMS_SET_CHN,        _IOC_SIZE(LAMS_SET_CHN)  },
+   { GET_NUM_PORTS,  _IOC_SIZE(GET_NUM_PORTS) },
+   { LAMS_SET_CHN,   _IOC_SIZE(LAMS_SET_CHN)  },
    { LAMS_N_AVG,          _IOC_SIZE(LAMS_N_AVG)         },
    { LAMS_N_PEAKS,        _IOC_SIZE(LAMS_N_PEAKS)       },
    { LAMS_TAS_BELOW,      _IOC_SIZE(LAMS_TAS_BELOW)     },
    { LAMS_TAS_ABOVE,      _IOC_SIZE(LAMS_TAS_ABOVE)     },
-   { LAMS_GET_STATUS,     _IOC_SIZE(LAMS_GET_STATUS)   },
 };
 static int nioctlcmds = sizeof(ioctlcmds) / sizeof(struct ioctlCmd);
 
@@ -120,7 +118,7 @@ static int openRTfifo()
 }
 
 unsigned int channel = 0;
-unsigned int nAVG   = 80;
+unsigned int nAVG   = 40;
 unsigned int nPEAKS = 2000;
 
 // -- THREAD -------------------------------------------------------------------
@@ -169,6 +167,7 @@ static unsigned int lams_isr (unsigned int irq, void* callbackPtr,
    static unsigned int nGlyph=0; 
    static unsigned int nAvg=0;
    static unsigned int nPeaks=0;
+      
 
    if (++nPeaks >= nPEAKS) {
       inw(baseAddr + PEAK_CLEAR_OFFSET);
@@ -204,29 +203,27 @@ static unsigned int lams_isr (unsigned int irq, void* callbackPtr,
                      nGlyph, _lamsPort.avrg[nGlyph], peak[nGlyph]);
    }
 
-   if (++nAvg >= nAVG) {
-      for (i = 0; i < LAMS_SPECTRA_SIZE; i++) {
-         _lamsPort.peak[i] = (unsigned short)peak[i];
-         word = (unsigned long)(sum[i] / nAvg);
-         _lamsPort.avrg[i] = word;
-      }
-      memset((void *)sum, 0, sizeof(sum));
-      nAvg = 0;
-      if (channel)
-         rtl_sem_post( &threadSem );
-   }
+   if (++nAvg >= 100) {
+    for (i = 0; i < LAMS_SPECTRA_SIZE; i++) {
+       _lamsPort.peak[i] = (unsigned short)peak[i];
+       word = (unsigned long)(sum[i] / nAvg);
+       _lamsPort.avrg[i] = word;
+    }
+    memset((void *)sum, 0, sizeof(sum));
+    nAvg = 0;
+    if (channel)
+      rtl_sem_post( &threadSem );
+
+  }
 
    return 0;
 }
 
 // -- IOCTL --------------------------------------------------------------------
- /*
-  * Return: negative Linux errno (not RTLinux errnos), or 0=OK
-  */
 static int ioctlCallback(int cmd, int board, int chn,
                          void *buf, rtl_size_t len)
 {
-   int err, ret = -EINVAL;
+   int err, ret = len;
    struct lams_set* lams_ptr;
 
    switch (cmd)
@@ -234,14 +231,12 @@ static int ioctlCallback(int cmd, int board, int chn,
       case GET_NUM_PORTS:
          DSMLOG_DEBUG("GET_NUM_PORTS\n");
          *(int *) buf = N_CHANNELS;
-         ret = len;
          break;
 
       case LAMS_SET_CHN:
          DSMLOG_DEBUG("LAMS_SET_CHN\n");
          lams_ptr = (struct lams_set*) buf;
          channel = lams_ptr->channel;
-         ret = len;
          DSMLOG_DEBUG("channel:       %d\n", channel);
 
          if (channel == 0) {
@@ -257,33 +252,25 @@ static int ioctlCallback(int cmd, int board, int chn,
       case LAMS_TAS_BELOW:
          inw(baseAddr + TAS_BELOW_OFFSET);
          DSMLOG_DEBUG("TAS_BELOW\n");
-         ret = len;
          break;
 
       case LAMS_TAS_ABOVE:
          inw(baseAddr + TAS_ABOVE_OFFSET);
          DSMLOG_DEBUG("TAS_ABOVE\n");
-         ret = len;
          break;
 
       case LAMS_N_AVG:
          nAVG = *(unsigned int*) buf;
          DSMLOG_DEBUG("nAVG:          %d\n", nAVG);
-         ret = len;
          break;
 
       case LAMS_N_PEAKS:
          nPEAKS = *(unsigned int*) buf;
          DSMLOG_DEBUG("nPEAKS:        %d\n", nPEAKS);
-         ret = len;
-         break;
-      case LAMS_GET_STATUS:
-         if (len != sizeof (status)) break;
-         *((struct lams_status*) buf) = status;
-         ret = len;
          break;
   
       default:
+         ret = -RTL_EIO;
          break;
    }
    return ret;
