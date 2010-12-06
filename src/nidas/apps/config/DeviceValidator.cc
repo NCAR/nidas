cf361be1c0326809d055b046fc3f5b46623c421b
Tom Baltzer <tbaltzer@ucar.edu>
2010-12-06 18:35:53 +0000

    There should be a maximum of 3 A2D cards allowed for.

--- src/nidas/apps/config/DeviceValidator.cc
+++ src/nidas/apps/config/DeviceValidator.cc
@@ -8,7 +8,7 @@ const char * DeviceValidator::_DeviceDefinitionStruct::_InterfaceLabels[] = { "C
 DeviceValidator::_DeviceDefinitionStruct DeviceValidator::_Definitions[] = {
 
 {"", "", 0, 0, DeviceValidator::_DeviceDefinition::SERIAL},
-{"Analog", "/dev/ncar_a2d", 0, 9, DeviceValidator::_DeviceDefinition::ANALOG},
+{"Analog", "/dev/ncar_a2d", 0, 2, DeviceValidator::_DeviceDefinition::ANALOG},
 {"ADC-GV", "/dev/arinc", 0, 9, DeviceValidator::_DeviceDefinition::SERIAL},
 {"Butanol_CN_Counter", "/dev/ttyS", 0, 9, DeviceValidator::_DeviceDefinition::SERIAL},
 {"CCN", "/dev/ttyS", 0, 9, DeviceValidator::_DeviceDefinition::SERIAL},
