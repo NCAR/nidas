fa71f8e0730a5251296d81dc60450a97108ec5f1
Gordon Maclean <maclean@ucar.edu>
2011-10-20 23:23:18 +0000

    Added extern for floatNAN.

--- src/nidas/core/SamplePool.h
+++ src/nidas/core/SamplePool.h
@@ -62,119 +62,119 @@ private:
  * getInstance() static member function.
  */
 template <typename SampleType>
-class SamplePool : public SamplePoolInterface {
+    class SamplePool : public SamplePoolInterface {
 
-private:
-    // static SamplePool<SampleType>* instance;
-    static SamplePool* instance;
-    static nidas::util::Mutex instanceLock;
+    private:
+        // static SamplePool<SampleType>* instance;
+        static SamplePool* instance;
+        static nidas::util::Mutex instanceLock;
 
-public:
+    public:
 
-    SamplePool();
-    ~SamplePool();
+        SamplePool();
+        ~SamplePool();
 
-    /**
-     * Get a pointer to the singleton instance.
-     */
-    static SamplePool *getInstance();
+        /**
+         * Get a pointer to the singleton instance.
+         */
+        static SamplePool *getInstance();
 
-    /**
-     * Singleton cleanup on program exit.
-     */
-    static void deleteInstance();
+        /**
+         * Singleton cleanup on program exit.
+         */
+        static void deleteInstance();
 
-    /**
-     * Get a sample of at least len elements from the pool.
-     */
-    SampleType *getSample(unsigned int len) throw(SampleLengthException);
+        /**
+         * Get a sample of at least len elements from the pool.
+         */
+        SampleType *getSample(unsigned int len) throw(SampleLengthException);
 
-    /**
-     * Return a sample to the pool.
-     */
-    void putSample(const SampleType *);
+        /**
+         * Return a sample to the pool.
+         */
+        void putSample(const SampleType *);
 
-    int getNSamplesAlloc() const { return _nsamplesAlloc; }
+        int getNSamplesAlloc() const { return _nsamplesAlloc; }
 
-    int getNSamplesOut() const { return _nsamplesOut; }
+        int getNSamplesOut() const { return _nsamplesOut; }
 
-    int getNSmallSamplesIn() const { return _nsmall; }
+        int getNSmallSamplesIn() const { return _nsmall; }
 
-    int getNMediumSamplesIn() const { return _nmedium; }
+        int getNMediumSamplesIn() const { return _nmedium; }
 
-    int getNLargeSamplesIn() const { return _nlarge; }
+        int getNLargeSamplesIn() const { return _nlarge; }
 
-protected:
-    SampleType *getSample(SampleType** vec,int *veclen, unsigned int len)
-	throw(SampleLengthException);
-    void putSample(const SampleType *,SampleType*** vecp,int *veclen, int* nalloc);
+    protected:
+        SampleType *getSample(SampleType** vec,int *veclen, unsigned int len)
+            throw(SampleLengthException);
+        void putSample(const SampleType *,SampleType*** vecp,int *veclen, int* nalloc);
 
-    SampleType** _smallSamples;
-    SampleType** _mediumSamples;
-    SampleType** _largeSamples;
+        SampleType** _smallSamples;
+        SampleType** _mediumSamples;
+        SampleType** _largeSamples;
 
-    int _smallSize;
-    int _mediumSize;
-    int _largeSize;
+        int _smallSize;
+        int _mediumSize;
+        int _largeSize;
 
-    nidas::util::Mutex poolLock;
+        nidas::util::Mutex poolLock;
 
-    /**
-     * maximum number of elements in a small sample
-     */
-    const static unsigned int SMALL_SAMPLE_MAXSIZE = 64;
+        /**
+         * maximum number of elements in a small sample
+         */
+        const static unsigned int SMALL_SAMPLE_MAXSIZE = 64;
 
-    /**
-     * maximum number of elements in a medium sized sample
-     */
-    const static unsigned int MEDIUM_SAMPLE_MAXSIZE = 512;
+        /**
+         * maximum number of elements in a medium sized sample
+         */
+        const static unsigned int MEDIUM_SAMPLE_MAXSIZE = 512;
 
-public:
-    int _nsmall;
-    int _nmedium;
-    int _nlarge;
+    public:
+        int _nsmall;
+        int _nmedium;
+        int _nlarge;
 
-    int _nsamplesOut;
+        int _nsamplesOut;
 
-    int _nsamplesAlloc;
+        int _nsamplesAlloc;
 
-};
-
-/* static */
-template<class SampleType>
-SamplePool<SampleType>* SamplePool<SampleType>::instance = 0;
+    };
 
 /* static */
 template<class SampleType>
-nidas::util::Mutex SamplePool<SampleType>::instanceLock = nidas::util::Mutex();
+    SamplePool<SampleType>* SamplePool<SampleType>::instance = 0;
 
 /* static */
 template<class SampleType>
-SamplePool<SampleType> *SamplePool<SampleType>::getInstance()
-{
-    if (!instance) {
-	nidas::util::Synchronized pooler(instanceLock);
-	if (!instance) {
-	    instance = new SamplePool<SampleType>();
-	    SamplePools::getInstance()->addPool(instance);
-	}
+    nidas::util::Mutex SamplePool<SampleType>::instanceLock = nidas::util::Mutex();
+
+/* static */
+template<class SampleType>
+    SamplePool<SampleType> *SamplePool<SampleType>::getInstance()
+    {
+        if (!instance) {
+            nidas::util::Synchronized pooler(instanceLock);
+            if (!instance) {
+                instance = new SamplePool<SampleType>();
+                SamplePools::getInstance()->addPool(instance);
+            }
+        }
+        return instance;
     }
-    return instance;
-}
 
 /* static */
 template<class SampleType>
-void SamplePool<SampleType>::deleteInstance()
-{
-    nidas::util::Synchronized pooler(instanceLock);
-    SamplePools::getInstance()->removePool(instance);
-    delete instance;
-    instance = 0;
-}
+    void SamplePool<SampleType>::deleteInstance()
+    {
+        nidas::util::Synchronized pooler(instanceLock);
+        SamplePools::getInstance()->removePool(instance);
+        delete instance;
+        instance = 0;
+    }
 
 template<class SampleType>
-SamplePool<SampleType>::SamplePool():
-    _nsmall(0),_nmedium(0),_nlarge(0),_nsamplesOut(0),_nsamplesAlloc(0)
+    SamplePool<SampleType>::SamplePool():
+        _nsmall(0),_nmedium(0),_nlarge(0),_nsamplesOut(0),_nsamplesAlloc(0)
 {
     // Initial size of pool of small samples around 16K bytes
     _smallSize = 16384 / (sizeof(SampleType) + SMALL_SAMPLE_MAXSIZE * SampleType::sizeofDataType());
@@ -197,154 +197,156 @@ SamplePool<SampleType>::SamplePool():
 }
 
 template<class SampleType>
-SamplePool<SampleType>::~SamplePool() {
-    int i;
-    for (i = 0; i < _nsmall; i++) delete _smallSamples[i];
-    delete [] _smallSamples;
-    for (i = 0; i < _nmedium; i++) delete _mediumSamples[i];
-    delete [] _mediumSamples;
-    for (i = 0; i < _nlarge; i++) delete _largeSamples[i];
-    delete [] _largeSamples;
-}
+    SamplePool<SampleType>::~SamplePool() {
+        int i;
+        for (i = 0; i < _nsmall; i++) delete _smallSamples[i];
+        delete [] _smallSamples;
+        for (i = 0; i < _nmedium; i++) delete _mediumSamples[i];
+        delete [] _mediumSamples;
+        for (i = 0; i < _nlarge; i++) delete _largeSamples[i];
+        delete [] _largeSamples;
+    }
 
 template<class SampleType>
-SampleType* SamplePool<SampleType>::getSample(unsigned int len)
+    SampleType* SamplePool<SampleType>::getSample(unsigned int len)
     throw(SampleLengthException)
-{
+    {
 
-    nidas::util::Synchronized pooler(poolLock);
+        nidas::util::Synchronized pooler(poolLock);
 
-    // Shouldn't get back more than I've dealt out
-    // If we do, that's an indication that reference counting
-    // is screwed up.
+        // Shouldn't get back more than I've dealt out
+        // If we do, that's an indication that reference counting
+        // is screwed up.
 
-    assert(_nsamplesOut >= 0);
+        assert(_nsamplesOut >= 0);
 
-    // conservation of sample numbers:
-    // total number allocated must equal:
-    //		number in the pool (_nsmall + _nmedium + _nlarge), plus
-    //		number held by others.
-    assert(_nsamplesAlloc == _nsmall + _nmedium + _nlarge + _nsamplesOut);
+        // conservation of sample numbers:
+        // total number allocated must equal:
+        //		number in the pool (_nsmall + _nmedium + _nlarge), plus
+        //		number held by others.
+        assert(_nsamplesAlloc == _nsmall + _nmedium + _nlarge + _nsamplesOut);
 
-    // get a sample from the appropriate pool, unless is is empty
-    // and there are 2 or more available from the next larger pool.
-    if (len < SMALL_SAMPLE_MAXSIZE && (_nsmall > 0 ||
-        (_nmedium + _nlarge) < 4))
-        return getSample((SampleType**)_smallSamples,&_nsmall,len);
-    else if (len < MEDIUM_SAMPLE_MAXSIZE && (_nmedium > 0 || _nlarge < 2))
-        return getSample((SampleType**)_mediumSamples,&_nmedium,len);
-    else return getSample((SampleType**)_largeSamples,&_nlarge,len);
-}
+        // get a sample from the appropriate pool, unless is is empty
+        // and there are 2 or more available from the next larger pool.
+        if (len < SMALL_SAMPLE_MAXSIZE && (_nsmall > 0 ||
+                    (_nmedium + _nlarge) < 4))
+            return getSample((SampleType**)_smallSamples,&_nsmall,len);
+        else if (len < MEDIUM_SAMPLE_MAXSIZE && (_nmedium > 0 || _nlarge < 2))
+            return getSample((SampleType**)_mediumSamples,&_nmedium,len);
+        else return getSample((SampleType**)_largeSamples,&_nlarge,len);
+    }
 
 template<class SampleType>
-SampleType* SamplePool<SampleType>::getSample(SampleType** vec,
-    	int *n, unsigned int len) throw(SampleLengthException)
-{
+    SampleType* SamplePool<SampleType>::getSample(SampleType** vec,
+            int *n, unsigned int len) throw(SampleLengthException)
+    {
 
-    SampleType *sample;
+        SampleType *sample;
 #ifdef DEBUG
-    std::cerr << "getSample, this=" << std::hex << this <<
-    	" pool=" << vec << std::dec <<
-    	" *n=" << *n << std::endl;
+        std::cerr << "getSample, this=" << std::hex << this <<
+            " pool=" << vec << std::dec <<
+            " *n=" << *n << std::endl;
 #endif
-    int i = *n - 1;
+        int i = *n - 1;
 
-    if (i >= 0) {
-      sample = vec[i];
-      if (sample->getAllocLength() < len) sample->allocateData(len);
+        if (i >= 0) {
+            sample = vec[i];
+            if (sample->getAllocLength() < len) sample->allocateData(len);
 #ifndef NDEBUG
-      else if (sample->getAllocLength() > len) {
-          // If the sample has been previously allocated, and its length
-          // is at least one more than we need, set the one-past-the-end
-          // data value to a noticable value. Then if a buggy process method
-          // reads past the end of a sample, they'll get a value that should
-          // raise questions about the results, rather than something
-          // that might go unnoticed.
+            else if (sample->getAllocLength() > len) {
+                // If the sample has been previously allocated, and its length
+                // is at least one more than we need, set the one-past-the-end
+                // data value to a noticable value. Then if a buggy process method
+                // reads past the end of a sample, they'll get a value that should
+                // raise questions about the results, rather than something
+                // that might go unnoticed.
 
-          // valgrind won't complain in these situations unless one reads
-          // past the allocated size.
+                // valgrind won't complain in these situations unless one reads
+                // past the allocated size.
 
-          // For character data (sizeof(T) == 1), we'll use up to
-          // 4 '\x80's as the weird value.
-          // For larger sizes, we'll use floatNAN. This will convert to
-          // 0 for integer samples.
+                // For character data (sizeof(T) == 1), we'll use up to
+                // 4 '\x80's as the weird value.
+                // For larger sizes, we'll use floatNAN. This will convert to
+                // 0 for integer samples.
+                
+                extern const float floatNAN;
 
-          if (sample->sizeofDataType() == 1) {
-              static const char weird[4] = { '\x80','\x80','\x80','\x80' };
-              int nb = std::min(4U,sample->getAllocLength()-len);
-              memcpy((char*)sample->getVoidDataPtr()+len,weird,nb);
-          }
-          else sample->setDataValue(len,floatNAN);  // NAN converted to the data type.
-      }
+                if (sample->sizeofDataType() == 1) {
+                    static const char weird[4] = { '\x80','\x80','\x80','\x80' };
+                    int nb = std::min(4U,sample->getAllocLength()-len);
+                    memcpy((char*)sample->getVoidDataPtr()+len,weird,nb);
+                }
+                else sample->setDataValue(len,floatNAN);  // NAN converted to the data type.
+            }
 #endif
-      sample->setDataLength(len);
-      *n = i;
-      sample->holdReference();
-      _nsamplesOut++;
-      return sample;
+            sample->setDataLength(len);
+            *n = i;
+            sample->holdReference();
+            _nsamplesOut++;
+            return sample;
+        }
+
+        sample = new SampleType();
+        sample->allocateData(len);
+        sample->setDataLength(len);
+        _nsamplesAlloc++;
+        _nsamplesOut++;
+        return sample;
+
     }
-
-    sample = new SampleType();
-    sample->allocateData(len);
-    sample->setDataLength(len);
-    _nsamplesAlloc++;
-    _nsamplesOut++;
-    return sample;
-
-}
 
 template<class SampleType>
-void SamplePool<SampleType>::putSample(const SampleType *sample) {
+    void SamplePool<SampleType>::putSample(const SampleType *sample) {
 
-    nidas::util::Synchronized pooler(poolLock);
+        nidas::util::Synchronized pooler(poolLock);
 
-    assert(_nsamplesOut >= 0);
-    assert(_nsamplesAlloc == _nsmall + _nmedium + _nlarge + _nsamplesOut);
+        assert(_nsamplesOut >= 0);
+        assert(_nsamplesAlloc == _nsmall + _nmedium + _nlarge + _nsamplesOut);
 
-    unsigned int len = sample->getAllocLength();
-    if (len < SMALL_SAMPLE_MAXSIZE) {
+        unsigned int len = sample->getAllocLength();
+        if (len < SMALL_SAMPLE_MAXSIZE) {
 #ifdef DEBUG
-        DLOG(("put small sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nsmall,_smallSize));
+            DLOG(("put small sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nsmall,_smallSize));
 #endif
-	putSample(sample,(SampleType***)&_smallSamples,&_nsmall,&_smallSize);
-    }
-    else if (len < MEDIUM_SAMPLE_MAXSIZE) {
+            putSample(sample,(SampleType***)&_smallSamples,&_nsmall,&_smallSize);
+        }
+        else if (len < MEDIUM_SAMPLE_MAXSIZE) {
 #ifdef DEBUG
-        DLOG(("put medium sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nmedium,_mediumSize));
+            DLOG(("put medium sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nmedium,_mediumSize));
 #endif
-        putSample(sample,(SampleType***)&_mediumSamples,&_nmedium,&_mediumSize);
-    }
-    else {
+            putSample(sample,(SampleType***)&_mediumSamples,&_nmedium,&_mediumSize);
+        }
+        else {
 #ifdef DEBUG
-        DLOG(("put large sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nlarge,_largeSize));
+            DLOG(("put large sample, len=%d,bytelen=%d,n=%d,size=%d",len,sample->getAllocByteLength(),_nlarge,_largeSize));
 #endif
-        putSample(sample,(SampleType***)&_largeSamples,&_nlarge,&_largeSize);
+            putSample(sample,(SampleType***)&_largeSamples,&_nlarge,&_largeSize);
+        }
     }
-}
 
 template<class SampleType>
-void SamplePool<SampleType>::putSample(const SampleType *sample,
-    	SampleType ***vec,int *n, int *nalloc)
-{
+    void SamplePool<SampleType>::putSample(const SampleType *sample,
+            SampleType ***vec,int *n, int *nalloc)
+    {
 
-    // increase by 50%
-    if (*n == *nalloc) {
-	// cerr << "reallocing, n=" << *n << " nalloc=" << *nalloc << endl;
-	// increase by 50%
-	int newalloc = *nalloc + (*nalloc >> 1);
+        // increase by 50%
+        if (*n == *nalloc) {
+            // cerr << "reallocing, n=" << *n << " nalloc=" << *nalloc << endl;
+            // increase by 50%
+            int newalloc = *nalloc + (*nalloc >> 1);
 #ifdef DEBUG
-        DLOG(("*nalloc=%d, newalloc=%d",*nalloc,newalloc));
+            DLOG(("*nalloc=%d, newalloc=%d",*nalloc,newalloc));
 #endif
-	SampleType **newvec = new SampleType*[newalloc];
-	::memcpy(newvec,*vec,*nalloc * sizeof(SampleType*));
-	delete [] *vec;
-	*vec = newvec;
-	*nalloc = newalloc;
-    }
+            SampleType **newvec = new SampleType*[newalloc];
+            ::memcpy(newvec,*vec,*nalloc * sizeof(SampleType*));
+            delete [] *vec;
+            *vec = newvec;
+            *nalloc = newalloc;
+        }
 
-    (*vec)[(*n)++] = (SampleType*) sample;
-    _nsamplesOut--;
-}
+        (*vec)[(*n)++] = (SampleType*) sample;
+        _nsamplesOut--;
+    }
 
 }}	// namespace nidas namespace core
 
