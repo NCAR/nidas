c7c06ba3abd4ee959fe0564e2c024bc9f6dcd4ab
John Wasinger <wasinger@ucar.edu>
2011-11-09 21:48:38 +0000

    Merged process back into Serial sensor class.  Setting devicename to a socket port creates a MessageStreamScanner that can process message seperators.

--- src/nidas/dynld/GPS_NMEA_Serial.h
+++ src/nidas/dynld/GPS_NMEA_Serial.h
@@ -17,14 +17,12 @@
 #define NIDIS_DYNLD_GPS_NMEA_SERIAL_H
 
 #include <nidas/dynld/DSMSerialSensor.h>
-#include <nidas/dynld/GPS_NMEA_Process.h>
 
 namespace nidas { namespace dynld {
 
 /**
- * A class for reading NMEA records from a GPS attached to
- * a serial port.  The process() method parses GPGGA and GPRMC
- * NMEA messages and generates floating point samples.
+ * A class for reading NMEA records from a GPS.  The process() method parses
+ * GGA, RMC, and HDT NMEA messages and generates floating point samples.
  */
 class GPS_NMEA_Serial: public DSMSerialSensor
 {
@@ -35,12 +33,79 @@ public:
     void addSampleTag(SampleTag* stag)
         throw(nidas::util::InvalidParameterException);
 
+    /**
+     * Virtual method that is called to convert a raw sample containing
+     * an ASCII NMEA message to a processed floating point sample.
+     * These processed samples contain double precision rather than
+     * single precision values because the latitude and longitude
+     * reported by GPS's may have more than 7 digits of precision.
+     */
     bool process(const Sample* samp,std::list<const Sample*>& results)
         throw();
 
+    SampleScanner* buildSampleScanner()
+    	throw(nidas::util::InvalidParameterException);
+
 private:
 
-    GPS_NMEA_Process _processor;
+    dsm_time_t parseGGA(const char* input,double *dout,int nvars,dsm_time_t tt) 
+        throw();
+
+    dsm_time_t parseRMC(const char* input,double *dout,int nvars,dsm_time_t tt)
+        throw();
+
+    dsm_time_t parseHDT(const char* input,double *dout,int nvars,dsm_time_t tt)
+        throw();
+
+    /**
+     * Timetag set by parseGGA and parseRMC, used by parseHDT.
+     */
+    dsm_time_t _ttgps;
+
+    /**
+     * Number of variables requested from GGA record (sample id == 1)
+     */
+    int _ggaNvars;
+
+    /**
+     * Full sample id of GGA variables.
+     */
+    dsm_sample_id_t _ggaId;
+
+    /**
+     * Number of variables requested from RMC record (sample id == 2)
+     */
+    int _rmcNvars;
+
+    /**
+     * Full sample id of RMC variables.
+     */
+    dsm_sample_id_t _rmcId;
+
+    /**
+     * Number of variables requested from HDT record (sample id == 3)
+     */
+    int _hdtNvars;
+
+    /**
+     * Full sample id of HDT variables.
+     */
+    dsm_sample_id_t _hdtId;
+
+    /**
+     * Id of sample from GGA NMEA record.  Fixed at 1.
+     */
+    static const int GGA_SAMPLE_ID;
+
+    /**
+     * Id of sample from RMC NMEA record.  Fixed at 2.
+     */
+    static const int RMC_SAMPLE_ID;
+
+    /**
+     * Id of sample from HDT NMEA record.  Fixed at 3.
+     */
+    static const int HDT_SAMPLE_ID;
 };
 
 }}	// namespace nidas namespace dynld
