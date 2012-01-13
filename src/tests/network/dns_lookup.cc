2b7eefcc36ba23664440e60860ce791244aca357
Gordon Maclean <maclean@ucar.edu>
2012-01-13 05:50:15 +0000

    tests/network/dns_lookup.cc: updates for the rework of nidas time headers     a month (or two) ago.  This test was still succeeding on the buildbot     because apparently it was finding an old installed nidas/core/DSMTime.h.     Sometimes things work when they shouldn't...

--- src/tests/network/dns_lookup.cc
+++ src/tests/network/dns_lookup.cc
@@ -1,5 +1,6 @@
 #include <nidas/core/SocketAddrs.h>
-#include <nidas/core/DSMTime.h>
+#include <nidas/core/Sample.h>
+#include <nidas/util/UTime.h>
 #include <nidas/util/Inet4Address.h>
 
 #include <iostream>
@@ -22,21 +23,21 @@ int main(int argc, char**argv)
 
     try {
         host = NIDAS_MULTICAST_ADDR;
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         addr = n_u::Inet4Address::getByName(host);
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "host \"" << host << "\" to addr: " << addr.getHostAddress() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         revhost = addr.getHostName();
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "addr: " << addr.getHostAddress() << " to host \"" << revhost <<
             "\", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
     }
     catch(const n_u::UnknownHostException& e) {
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cerr << e.what() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
@@ -45,21 +46,21 @@ int main(int argc, char**argv)
 
     try {
         host = "quacka-quacka";
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         addr = n_u::Inet4Address::getByName(host);
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "host \"" << host << "\" to addr: " << addr.getHostAddress() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         revhost = addr.getHostName();
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "addr: " << addr.getHostAddress() << " to host \"" << revhost <<
             "\", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
     }
     catch(const n_u::UnknownHostException& e) {
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cerr << e.what() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
@@ -68,21 +69,21 @@ int main(int argc, char**argv)
 
     try {
         host = "www.google.com";
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         addr = n_u::Inet4Address::getByName(host);
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "host \"" << host << "\" to addr: " << addr.getHostAddress() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
-        t1 = getSystemTime();
+        t1 = n_u::getSystemTime();
         revhost = addr.getHostName();
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cout << "addr: " << addr.getHostAddress() << " to host \"" << revhost <<
             "\", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
     }
     catch(const n_u::UnknownHostException& e) {
-        t2 = getSystemTime();
+        t2 = n_u::getSystemTime();
         cerr << e.what() <<
             ", lookup delay=" << fixed << setprecision(2) << setw(7) <<
                 (float)(t2-t1)/USECS_PER_SEC << " sec" << endl;
