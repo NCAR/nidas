de432961ab9b9f86bbe149b2c5fbf9bc15093b7d
Gordon Maclean <maclean@ucar.edu>
2012-03-05 00:54:54 +0000

    Added inw_w, out_w macros, which on the Vulcan are performed on (address + 0x1000) for 16 bit transfers.

--- src/nidas/linux/isa_bus.h
+++ src/nidas/linux/isa_bus.h
@@ -1,3 +1,5 @@
+/* -*- mode: C; indent-tabs-mode: nil; c-basic-offset: 8; tab-width: 8; -*-
+ * vim: set shiftwidth=8 softtabstop=8 expandtab: */
 /*
   *******************************************************************
   Copyright 2005 UCAR, NCAR, All Rights Reserved
@@ -26,6 +28,14 @@
 #include <asm/arch/viper.h>
 #define SYSTEM_ISA_IOPORT_BASE VIPER_PC104IO_BASE
 #define SYSTEM_ISA_IOMEM_BASE 0x3c000000
+
+/*
+ * "Windowed" versions of 16 bit I/O operations, provided
+ * so that it can be redefined for VULCANs.
+ */
+#define inw_w(a)        inw(a)
+#define insw_w(a,p,n)   insw(a,p,n)
+#define outw_w(v,a)     outw(v,a)
 
 /* The viper maps ISA irq 3,4,5,... to viper interrupts 104,105,106,etc.
  * See <linux_2.6_source>/arch/arm/mach-pxa/viper.c.
@@ -58,7 +68,26 @@
 
 #include <asm/irq.h>
 #define SYSTEM_ISA_IOPORT_BASE 0x0
-#define SYSTEM_ISA_IOMEM_BASE 0x0  /* ? */
+#define SYSTEM_ISA_IOMEM_BASE 0x049000000       /* 8 bit memory space. 16 bit is at 0x4a000000 */
+
+/*
+ * On VULCANs I/O window 1 of the PCI1520 bridge chip is configured for 16 bit
+ * accesses, and starts at address 0x1000.  0x1000 is subtracted from addresses
+ * that are placed on the ISA bus, so that if an outw/inw is done to address 0x1XXX
+ * the address on the PC104 bus will be 0x0XXX.
+ */
+#define VULCAN_IO_WINDOW_1_START 0x1000
+
+/*
+ * "Windowed" versions of 16 bit I/O operations. Add 0x1000
+ * to the addresses on VULCANs so that the 16 bit operations
+ * happen in I/O window 1 on the PCI1520, which is configured
+ * for 16 bit accesses.
+ */
+#define inw_w(a)        inw((a)+VULCAN_IO_WINDOW_1_START)
+#define insw_w(a,p,n)   insw((a)+VULCAN_IO_WINDOW_1_START,p,n)
+#define outw_w(v,a)     outw(v,(a)+VULCAN_IO_WINDOW_1_START)
+
 /* 
  * On the Mercury/Vulcan, most of the ISA interrupts are routed to GPIO
  * pins on the processor.  Handle those mappings here.
@@ -81,6 +110,10 @@
 #define SYSTEM_ISA_IOPORT_BASE 0x0
 #define SYSTEM_ISA_IOMEM_BASE 0x0
 #define GET_SYSTEM_ISA_IRQ(x) (x)
+
+#define inw_w(a) inw(a)
+#define insw_w(a,p,n) insw(a,p,n)
+#define outw_w(v,a) outw(v,a)
 
 #endif
 
