819e6df9daeb86e1413174e0a56364a42233a6a6
Gordon Maclean <maclean@ucar.edu>
2012-04-19 18:15:42 +0000

    Fixes to script which can be used to do a force update of nidas rpms.

--- src/scripts/nidas_rpm_update.sh
+++ src/scripts/nidas_rpm_update.sh
@@ -1,42 +1,48 @@
 #!/bin/sh
 
-# distroverpkg=fedora-release
-# cf=/etc/yum.conf
-# if grep -q "^distroverpkg" /etc/yum.conf; then
-#     distroverpkg=`grep "^distroverpkg" $df | cut -d= -f 2`
-# fi
-# releasever=`rpm -q --queryformat "%{VERSION}\n" $distroverpkg`
-# basearch=`uname -i`
+# Check if nidas, nidas-libs or nidas-devel RPMs are installed.
+# If so, it then run yumdownloader to download the current
+# version of those RPMs from the EOL repository, and then do an
+# "rpm -Uhv --force" to force update those RPMs.
+
+# The version and release number of these rpms are not updated on
+# every subversion checkin. Currently they are 1.1-0. Therefore
+# a "yum update" typically won't result in an update to the latest RPM 
+# on the repository. Instead, do a yumdownload and force update.
 
 script=`basename $0`
 
-tmpdir=/tmp/$script_$$
-mkdir $tmpdir
+tmpdir=`mktemp -d`
 trap "{ rm -rf $tmpdir; }" EXIT
 
 cd $tmpdir || exit 1
 
-# set -x
+cleaned=false
 
 # yumdownloader --urls nidas-bin nidas-bin-devel
 
-yumdownloader nidas nidas-libs
+for rpm in nidas nidas-libs nidas-devel; do
+    if rpm -q $rpm; then
+        if ! yumdownloader $rpm; then
+            if ! $cleaned; then
+                echo "doing yum clean all"
+                yum clean all
+                cleaned=true
+            fi
+            yumdownloader --enablerepo=eol $rpm
+        fi
+    fi
+done
 
 shopt -s nullglob
-rpms=(nidas-bin-*.rpm)
-if [ ${#rpms[*]} -eq 0 ]; then
-    echo "Doing yum clean metadata"
-    # Don't do "sudo yum clean metadata". You want to
-    # clean the user's yum cache, not root's.
-    yum clean metadata
-    yumdownloader --enablerepo=eol nidas-bin nidas-bin-devel
-    rpms=(nidas-bin-*.rpm)
-fi
+rpms=(*.rpm)
 
 if [ ${#rpms[*]} -eq 0 ]; then
-    echo "No rpms successfully downloaded"
+    echo "No nidas rpms found or successfully downloaded"
     exit 1
 fi
+
+# echo ${rpms[*]}
 
 echo "Doing sudo yum -Uhv --force ${rpms[*]}"
 sudo rpm -Uhv --force ${rpms[*]}
