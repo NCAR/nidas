318aa943a6ecb0ee19d2ffb7de2930145627068d
Gordon Maclean <maclean@ucar.edu>
2012-04-20 20:16:19 +0000

    update doxygen header

--- src/nidas/nidas.h
+++ src/nidas/nidas.h
@@ -18,65 +18,54 @@
   \mainpage NIDAS
   \section intro_sec Introduction
   NCAR In-Situ Data Acquisition Software
-  \section install_both How to Build and Install NIDAS for ARM and Intel X86
+
+  \section install_both How to Build and Install NIDAS for ARM and Intel Systems
 
   NIDAS uses the xerces-c and xmlrpc++ software packages. Building NIDAS
   requires access to the xerces-c and xmlrpc++ header files and libraries.
 
-  The path the installation of the X86 version of these packages can
-  be specified with the OTHER_X86 option to scons. Set OTHER_X86
-  to the name of the parent directory of the 'include' and 'lib' directories
-  containing the xerces-c and xmlrpc++ header files and libraries.
-
-  The default value of OTHER_X86 is $PREFIX/x86, where PREFIX can
-  be set as the destination the installation.  The default value
-  of PREFIX is /opt/local/nidas.
-
-  The path the installation of the ARM version of these packages can
-  be specified with the OTHER_ARM option to scons. 
-  The default value of OTHER_ARM is $PREFIX/arm.
-
-  When building for ARM, make sure arm-linux-g++, and the other arm tools are available and in your path. For example:
-  - sh/bash:  PATH=/opt/arcom/bin:$PATH
-  - csh/tcsh: set path = (/opt/arcom/bin $path)
-
-  The RTLinux modules in nidas/rtlinux are also built when building for arm.  The the RTLinux ARM cross-development tools must be installed in /opt/rtldk-2.2.
-
-  - cd src
-  - scons PREFIX=/install_path OTHER_X86=/usr/local OTHER_ARM=/usr/local_arm
-  - scons install
-
-  If the xerces-c and xmlrpc++ software is installed somewhere other than $PREFIX/x86 and $PREFIX/arm, use the OTHER_X86 and OTHER_ARM options:
-
-  - cd src
-  - scons PREFIX=/install_path OTHER_X86=/usr/local OTHER_ARM=/usr/local_arm
-  - scons install
-
-  /install_path is where you want to install NIDAS. The default value for PREFIX is /opt/local/nidas.
-  Executables, libraries and header files will be installed as follows:
-
+  \section directory Directory of NIDAS Installation
+  $PREFIX defaults to /opt/nidas. It can be changed with a scons runstring variable
   <dl>
+  <dt>$PREFIX/bin</dt>	<dd>Executables for the host system</dd>
+  <dt>$PREFIX/lib</dt>	<dd>Libraries for the host (libnidas.so, libnidas_dynld.so)</dd>
+  <dt>$PREFIX/modules</dt>	<dd>Kernel modules for the host</dd>
+  <dt>$PREFIX/include</dt>	<dd>C/C++ header files</dd>
   <dt>$PREFIX/arm/bin</dt>	<dd>Executables for ARM</dd>
   <dt>$PREFIX/arm/lib</dt>	<dd>Libraries for ARM (libnidas.so, libnidas_dynld.so)</dd>
-  <dt>$PREFIX/arm/include</dt>	<dd>Headers</dd>
-
-  <dt>$PREFIX/x86/bin</dt>	<dd>Executables for X86</dd>
-  <dt>$PREFIX/x86/lib</dt>	<dd>Libraries for X86 (libnidas.so, libnidas_dynld.so)</dd>
-  <dt>$PREFIX/x86/include</dt>	<dd>Headers</dd>
+  <dt>$PREFIX/arm/modules/viper</dt>	<dd>Kernel modules for Eurotech Viper CPU</dd>
+  <dt>$PREFIX/arm/modules/titan</dt>	<dd>Kernel modules for Eurotech Titan CPU</dd>
+  <dt>$PREFIX/armbe/bin</dt>	<dd>Executables for big-endian ARM</dd>
+  <dt>$PREFIX/armbe/lib</dt>	<dd>Libraries for big-endian ARM (libnidas.so, libnidas_dynld.so)</dd>
+  <dt>$PREFIX/armbe/modules/vulcan</dt>	<dd>Kernel modules for Eurotech Vulcan CPU</dd>
   </dl>
 
-  \section install_x86 How to Build NIDAS for Intel X86
+  \section install_all How to Build and Install NIDAS for All Supported Hosts
   - cd src
-  - scons PREFIX=/install_path
-  - scons x86_install
+  - scons install
+
+  \section prefix Setting install destination with PREFIX
+  - cd src
+  - scons PREFIX=/usr install
+
+  \section install_x86 How to Build NIDAS for Intel hosts
+  - cd src
+  - scons BUILDS=host
+  - scons BUILDS=host install
 
   \section install_arm How to Build NIDAS for ARM
+  When building for ARM, make sure arm-linux-g++, and the other arm tools are available and in your path. For example:
+
   - sh/bash:  PATH=/opt/arcom/bin:$PATH
   - csh/tcsh: set path = (/opt/arcom/bin $path)
 
   - cd src
-  - scons PREFIX=/install_path
-  - scons arm_install
+  - scons BUILDS=arm
+  - scons BUILDS=arm install
+
+  - cd src
+  - scons BUILDS=armbe
+  - scons BUILDS=armbe install
 
   \namespace nidas
     \brief Root namespace for the NCAR In-Situ Data Acquisition Software.
