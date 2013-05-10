316df9e6fbfe1bd4632fce51305ceb2c6516d42f
Gordon Maclean <maclean@ucar.edu>
2013-05-10 22:42:11 +0000

    In read method with timeout, use ppoll instead of pselect if available.

--- src/nidas/core/UnixIODevice.cc
+++ src/nidas/core/UnixIODevice.cc
@@ -13,7 +13,15 @@
 
 */
 
+#include <nidas/Config.h>   // HAVE_PPOLL
+
 #include <nidas/core/UnixIODevice.h>
+
+#ifdef HAVE_PPOLL
+#include <poll.h>
+#else
+#include <sys/select.h>
+#endif
 
 #include <signal.h>
 
@@ -31,18 +39,45 @@ size_t UnixIODevice::read(void *buf, size_t len, int msecTimeout) throw(nidas::u
     // unblock SIGUSR1 in ppoll/pselect
     sigdelset(&sigmask,SIGUSR1);
 
+#ifdef HAVE_PPOLL
+    struct pollfd fds;
+    fds.fd =  _fd;
+#ifdef POLLRDHUP
+    fds.events = POLLIN | POLLRDHUP;
+#else
+    fds.events = POLLIN;
+#endif
+#else
     fd_set fdset;
     FD_ZERO(&fdset);
     FD_SET(_fd, &fdset);
+#endif
 
     struct timespec tmpto = { msecTimeout / MSECS_PER_SEC,
         (msecTimeout % MSECS_PER_SEC) * NSECS_PER_MSEC };
 
     int res;
+
+#ifdef HAVE_PPOLL
+    if ((res = ::ppoll(&fds,1,&tmpto,&sigmask)) < 0)
+        throw nidas::util::IOException(getName(),"read",errno);
+    if (res == 0)
+        throw nidas::util::IOTimeoutException(getName(),"read");
+
+    if (fds.revents & POLLERR)
+        throw nidas::util::IOException(getName(),"read",errno);
+
+#ifdef POLLRDHUP
+    if (fds.revents & (POLLHUP | POLLRDHUP)) return 0;
+#else
+    if (fds.revents & POLLHUP) return 0;
+#endif
+#else
     if ((res = ::pselect(_fd+1,&fdset,0,0,&tmpto,&sigmask)) < 0) {
         throw nidas::util::IOException(getName(),"read",errno);
     }
     if (res == 0)
         throw nidas::util::IOTimeoutException(getName(),"read");
+#endif
     return read(buf,len);
 }
