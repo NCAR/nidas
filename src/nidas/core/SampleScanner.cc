fa8b30c649da7faf4966b97c84d7b31e6fe60a19
Gordon Maclean <maclean@ucar.edu>
2015-10-17 21:38:38 +0000

    Commit some old comments

--- src/nidas/core/SampleScanner.cc
+++ src/nidas/core/SampleScanner.cc
@@ -544,17 +544,17 @@ Sample* MessageStreamScanner::nextSampleSepBOM(DSMSensor* sensor)
      * scanner will be in one of these states:
      * 1. first call, no chars scanned, _osamp==NULL
      * 2. last sample exceeded MAX_MESSAGE_STREAM_SAMPLE_SIZE before
-     *    finding the next BOM.  _osamp==NULL
-     * 3. last call scanned an entire BOM separator and returned
-     *    the sample previous to the separator. Therefore we're
-     *    currently reading the portion after the separator. If
+     *    finding the next BOM. That bogus sample was returned, and now
+     *    _osamp==NULL.
+     * 3. last call successfully matched the BOM separator and returned
+     *    the sample previous to the separator.
+     *    In this case _osamp != NULL, _separatorCnt == _separatorLen.
+     *    Now to read the portion after the separator into _osamp. If
      *    getMessageLength() > 0, memcpy available characters, up to
      *    the message length, then start scanning for the next BOM.
-     *    _osamp != NULL.
-     * 4. last call returned 0, meaning there was a partial sample
-     *      at end of the previous buffer. osamp then contains
-     *      a partial sample. We may or may not be done scanning
-     *      for the BOM separator.  _osamp != NULL.
+     * 4. last call returned 0, meaning we have consumed some
+     *    characters after the last BOM, but haven't found the next BOM.
+     *    In this case, _osamp != NULL and _separatorCnt < _separatorLen.
      */
 
     if (!_osamp) {
