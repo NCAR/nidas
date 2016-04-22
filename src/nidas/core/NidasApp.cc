35baa3696c1559bc2a90772929424dfadba5c790
Gary Granger <granger@ucar.edu>
2016-04-22 12:17:13 -0600

    split SampleMatcher into its own source files

--- src/nidas/core/NidasApp.cc
+++ src/nidas/core/NidasApp.cc
@@ -21,26 +21,8 @@ using nidas::util::LogScheme;
 
 using namespace std;
 
-SampleMatcher::
-RangeMatcher::
-RangeMatcher(int d1, int d2, int s1, int s2, int inc) :
-  dsm1(d1), dsm2(d2), sid1(s1), sid2(s2), include(inc)
-{}
-
-
-SampleMatcher::
-SampleMatcher() :
-  _ranges(),
-  _lookup(),
-  _startTime(LONG_LONG_MIN),
-  _endTime(LONG_LONG_MAX)
-{
-}
-
-
 namespace 
 {
-
   int
   int_from_string(const std::string& text)
   {
@@ -54,129 +36,6 @@ namespace
     }
     return num;
   }
-
-
-  void
-  parse_range(const std::string& rngstr, int& rngid1, int& rngid2)
-  {
-    string::size_type ic;
-    if (rngstr.length() > 1 && (ic = rngstr.find('-',1)) != string::npos)
-    {
-      rngid1 = int_from_string(rngstr.substr(0,ic));
-      rngid2 = int_from_string(rngstr.substr(ic+1));
-    }
-    else if (rngstr == "*")
-    {
-      rngid1 = rngid2 = -1;
-    }
-    else
-    {
-      rngid1 = rngid2 = int_from_string(rngstr);
-    }
-  }
-}
-
-
-bool
-SampleMatcher::
-addCriteria(const std::string& ctext)
-{
-  bool include = true;
-  int dsmid1,dsmid2;
-  int snsid1,snsid2;
-  string soptarg(ctext);
-  if (soptarg.empty())
-    return false;
-  if (soptarg[0] == '^')
-  {
-    include = false;
-    soptarg = soptarg.substr(1);
-  }
-  string::size_type ic = soptarg.find(',');
-  if (ic == string::npos) 
-    return false;
-  string dsmstr = soptarg.substr(0,ic);
-  string snsstr = soptarg.substr(ic+1);
-  try
-  {
-    parse_range(dsmstr, dsmid1, dsmid2);
-    parse_range(snsstr, snsid1, snsid2);
-  }
-  catch (std::invalid_argument& err)
-  {
-    return false;
-  }
-  _lookup.clear();
-  _ranges.push_back(RangeMatcher(dsmid1, dsmid2, snsid1, snsid2, include));
-  return true;
-}
-
-bool
-SampleMatcher::
-match(dsm_sample_id_t id)
-{
-  id_lookup_t::iterator it = _lookup.find(id);
-  if (it != _lookup.end())
-  {
-    return it->second;
-  }
-  bool all_excludes = true;
-  range_matches_t::iterator ri;
-  for (ri = _ranges.begin(); ri != _ranges.end(); ++ri)
-  {
-    all_excludes = all_excludes && (!ri->include);
-    // See if the id is in this range, in which case the result depends
-    // upon whether this range is included or excluded.
-    int did = GET_DSM_ID(id);
-    int sid = GET_SHORT_ID(id);
-    if ((ri->dsm1 == -1 || (did >= ri->dsm1 && did <= ri->dsm2)) && 
-	(ri->sid1 == -1 || (sid >= ri->sid1 && sid <= ri->sid2)))
-    {
-      break;
-    }
-  }
-  // If there are no ranges, then the sample is implicitly included.  If
-  // there are only excluded ranges and this sample did not match any of
-  // them, then the sample is implicitly included.
-  bool result = false;
-  if (_ranges.begin() == _ranges.end())
-    result = true;
-  else if (ri != _ranges.end())
-    result = ri->include;
-  else if (all_excludes)
-    result = true;
-  _lookup[id] = result;
-  return result;
-}
-
-
-bool
-SampleMatcher::
-match(const Sample* samp)
-{
-  dsm_time_t tt = samp->getTimeTag();
-  dsm_sample_id_t sampid = samp->getId();
-  DLOG(("sampid=") << samp->getDSMId() << ',' << samp->getSpSId());
-  if (tt < _startTime.toUsecs() || tt > _endTime.toUsecs() || !match(sampid))
-  {
-    return false;
-  }
-  return true;
-}
-
-
-bool
-SampleMatcher::
-exclusiveMatch()
-{
-  bool xc = false;
-  if (_ranges.size() == 1)
-  {
-    range_matches_t::iterator ri = _ranges.begin();
-    xc = ((ri->dsm1 == ri->dsm2 && ri->dsm1 != -1) &&
-	  (ri->sid1 == ri->sid2 && ri->sid1 != -1));
-  }
-  return xc;
 }
 
 
--- src/nidas/core/NidasApp.h
+++ src/nidas/core/NidasApp.h
@@ -2,22 +2,14 @@
 // vim: set shiftwidth=4 softtabstop=4 expandtab:
 /*
  ********************************************************************
-    Copyright 2014 UCAR, NCAR, All Rights Reserved
-
-    $LastChangedDate: 2014-08-11 14:42:08 -0600 (Mon, 11 Aug 2014) $
-
-    $LastChangedRevision: 7091 $
-
-    $LastChangedBy: granger $
-
-    $HeadURL: http://svn.eol.ucar.edu/svn/nidas/trunk/src/nidas/core/DSMSensor.h $
+    Copyright 2016 UCAR, NCAR, All Rights Reserved
  ********************************************************************
-
 */
 #ifndef NIDAS_CORE_NIDASAPP_H
 #define NIDAS_CORE_NIDASAPP_H
 
 #include "SampleTag.h"
+#include "SampleMatcher.h"
 #include <nidas/util/UTime.h>
 #include <nidas/util/Socket.h>
 
@@ -28,124 +20,6 @@
 namespace nidas { namespace core {
 
 class Project;
-
-/**
- * Match samples according to DSM and Sample ID ranges, and configure the
- * ranges with criteria in text format.
- **/
-class SampleMatcher
-{
-    struct RangeMatcher
-    {
-        /**
-         * Construct a RangeMatcher with the given range endpoints: d1 <=
-         * DSM ID <= d2, s1 <= Sample ID <= s2, and if @p inc is false,
-         * then matching sample IDs are excluded rather than included.
-         **/ 
-        RangeMatcher(int d1, int d2, int s1, int s2, int inc);
-
-        int dsm1;
-        int dsm2;
-        int sid1;
-        int sid2;
-        bool include;
-    };
-
-public:
-
-    /**
-     * Construct an empty SampleMatcher with no ranges.  An empty
-     * SampleMatcher implicitly matches all samples.
-     **/
-    SampleMatcher();
-
-    /**
-     * Add a sample range using this syntax:
-     * @verbatim
-     *   [^]{<d1>[-<d2>|*},{<s1>[-<s2>]|*}
-     * @endverbatim
-     * The leading '^' will exclude any sample IDs in the given range
-     * instead of including them.  The '*' matches any ID.  The older
-     * convention where -1 matches all IDs is deprecated but still
-     * supported.
-     **/
-    bool
-    addCriteria(const std::string& ctext);
-
-    /**
-     * Return true if the given @p id satisfies the current range criteria.
-     * Search the ranges for one which includes this id, then return true
-     * if the range is an inclusion and otherwise false.  The outcome is
-     * cached for future lookups, but the cache is cleared if the criteria
-     * change.
-     **/
-    bool
-    match(dsm_sample_id_t id);
-
-    /**
-     * Return true if this sample matches all the criteria in this matcher,
-     * both sample ids and time range.
-     **/
-    bool
-    match(const Sample* samp);
-
-    /**
-     * Return true if this matcher can only match a single ID pair
-     * (DSM,SID), meaning only one range has been added and it specifies
-     * two specific positive IDs.
-     **/
-    bool
-    exclusiveMatch();
-
-    /**
-     * The number of ranges added to this SampleMatcher.
-     **/
-    int 
-    numRanges()
-    {
-        return _ranges.size();
-    }
-
-    /**
-     * Set the time before which samples will not match.
-     **/
-    void
-    setStartTime(nidas::util::UTime start)
-    {
-        _startTime = start;
-    }
-
-    nidas::util::UTime
-    getStartTime()
-    {
-        return _startTime;
-    }
-
-    /**
-     * Set the time after which samples will not match.
-     **/
-    void
-    setEndTime(nidas::util::UTime end)
-    {
-        _endTime = end;
-    }
-
-    nidas::util::UTime
-    getEndTime()
-    {
-        return _endTime;
-    }
-
-private:
-    typedef std::map<dsm_sample_id_t, bool> id_lookup_t;
-    typedef std::vector<RangeMatcher> range_matches_t;
-
-    range_matches_t _ranges;
-    id_lookup_t _lookup;
-    nidas::util::UTime _startTime;
-    nidas::util::UTime _endTime;
-};
-
 
 /**
  * The NidasApp class throws a NidasAppException when command-line options
