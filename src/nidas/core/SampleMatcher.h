d436c46600b8c9ec0f07a2a2d982836b9d1beff8
Gary Granger <granger@ucar.edu>
2016-04-23 14:40:00 -0600

    oops, add the SampleMatcher and SampleTracer source files
    those should have been added a few revisions ago...

---
+++ src/nidas/core/SampleMatcher.h
@@ -0,0 +1,138 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4; -*-
+// vim: set shiftwidth=4 softtabstop=4 expandtab:
+/*
+ ********************************************************************
+    Copyright 2016 UCAR, NCAR, All Rights Reserved
+ ********************************************************************
+*/
+#ifndef NIDAS_CORE_SAMPLEMATCHER_H
+#define NIDAS_CORE_SAMPLEMATCHER_H
+
+#include "SampleTag.h"
+#include <nidas/util/UTime.h>
+
+namespace nidas { namespace core {
+
+/**
+ * Match samples according to DSM and Sample ID ranges, and configure the
+ * ranges with criteria in text format.
+ **/
+class SampleMatcher
+{
+    struct RangeMatcher
+    {
+        /**
+         * Construct a RangeMatcher with the given range endpoints: d1 <=
+         * DSM ID <= d2, s1 <= Sample ID <= s2, and if @p inc is false,
+         * then matching sample IDs are excluded rather than included.
+         **/ 
+        RangeMatcher(int d1, int d2, int s1, int s2, int inc);
+
+        int dsm1;
+        int dsm2;
+        int sid1;
+        int sid2;
+        bool include;
+    };
+
+public:
+
+    /**
+     * Construct an empty SampleMatcher with no ranges.  An empty
+     * SampleMatcher implicitly matches all samples.
+     **/
+    SampleMatcher();
+
+    /**
+     * Add a sample range using this syntax:
+     * @verbatim
+     *   [^]{<d1>[-<d2>|*},{<s1>[-<s2>]|*}
+     * @endverbatim
+     * The leading '^' will exclude any sample IDs in the given range
+     * instead of including them.  The '*' matches any ID.  The older
+     * convention where -1 matches all IDs is deprecated but still
+     * supported.
+     * 
+     * An empty criteria string is allowed and valid but changes nothing.
+     **/
+    bool
+    addCriteria(const std::string& ctext);
+
+    /**
+     * Return true if the given @p id satisfies the current range criteria.
+     * Search the ranges for one which includes this id, then return true
+     * if the range is an inclusion and otherwise false.  The outcome is
+     * cached for future lookups, but the cache is cleared if the criteria
+     * change.
+     **/
+    bool
+    match(dsm_sample_id_t id);
+
+    /**
+     * Return true if this sample matches all the criteria in this matcher,
+     * both sample ids and time range.
+     **/
+    bool
+    match(const Sample* samp);
+
+    /**
+     * Return true if this matcher can only match a single ID pair
+     * (DSM,SID), meaning only one range has been added and it specifies
+     * two specific positive IDs.
+     **/
+    bool
+    exclusiveMatch();
+
+    /**
+     * The number of ranges added to this SampleMatcher.
+     **/
+    int 
+    numRanges()
+    {
+        return _ranges.size();
+    }
+
+    /**
+     * Set the time before which samples will not match.
+     **/
+    void
+    setStartTime(nidas::util::UTime start)
+    {
+        _startTime = start;
+    }
+
+    nidas::util::UTime
+    getStartTime()
+    {
+        return _startTime;
+    }
+
+    /**
+     * Set the time after which samples will not match.
+     **/
+    void
+    setEndTime(nidas::util::UTime end)
+    {
+        _endTime = end;
+    }
+
+    nidas::util::UTime
+    getEndTime()
+    {
+        return _endTime;
+    }
+
+private:
+    typedef std::map<dsm_sample_id_t, bool> id_lookup_t;
+    typedef std::vector<RangeMatcher> range_matches_t;
+
+    range_matches_t _ranges;
+    id_lookup_t _lookup;
+    nidas::util::UTime _startTime;
+    nidas::util::UTime _endTime;
+};
+
+
+}}	// namespace nidas namespace core
+
+#endif  // NIDAS_CORE_SAMPLEMATCHER_H
---
+++ src/nidas/core/SampleTracer.h
@@ -0,0 +1,94 @@
+// -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4; -*-
+// vim: set shiftwidth=4 softtabstop=4 expandtab:
+/*
+ ********************************************************************
+    Copyright 2016 UCAR, NCAR, All Rights Reserved
+ ********************************************************************
+*/
+#ifndef NIDAS_CORE_SAMPLETRACER_H
+#define NIDAS_CORE_SAMPLETRACER_H
+
+#include "SampleMatcher.h"
+
+namespace nidas { namespace core {
+
+
+/**
+ * SampleTracer uses a Logger to log messages about samples as they are
+ * encountered in the code.  The samples are selected by ID using a
+ * SampleMatcher, whose criteria are specified in the 'trace_samples' log
+ * parameter.
+ **/
+class SampleTracer
+{
+public:
+    SampleTracer();
+
+    inline bool
+    active(const Sample* samp)
+    {
+        return _context.active() && _matcher.match(samp);
+    }
+
+    inline std::string
+    format_time(dsm_time_t tt)
+    {
+        return nidas::util::UTime(tt).format(true, "%Y %m %d %H:%M:%S.%3f");
+    }
+
+    /**
+     * Format a basic log message tracing the given the sample, using text
+     * as a prefix, then return a reference to this instance's LogMessage
+     * so more information can be streamed to it.  The 'endlog' manipulator
+     * must be streamed to the return value to actually log the message.
+     * For example:
+     *
+     * @code
+     * static SampleTracer st;
+     * ...
+     * 
+     * if (st.active(samp))
+     *    st.msg(samp, "received") << endlog;
+     * @endcode
+     *
+     * With no arguments, no message is added, just the LogMessage
+     * reference is returned, allowing more information to be streamed to
+     * an existing message.
+     **/
+    inline nidas::util::LogMessage&
+    msg(const Sample* samp = 0, const std::string& text = "")
+    {
+        if (samp)
+        {
+            dsm_time_t tt = samp->getTimeTag();
+            dsm_sample_id_t sid = samp->getId();
+            _msg << text
+                 << "[" << GET_DSM_ID(sid) << ',' << GET_SPS_ID(sid) << "]"
+                 << "@" << format_time(tt);
+        }
+        return _msg;
+    }
+
+private:
+    nidas::util::LogContext _context;
+    nidas::util::LogMessage _msg;
+    nidas::core::SampleMatcher _matcher;
+
+};
+
+
+SampleTracer::
+SampleTracer() :
+    _context(LOG_VERBOSE, "trace_samples"),
+    _msg(&_context),
+    _matcher()
+{
+    nidas::util::Logger* logger = nidas::util::Logger::getInstance();
+    nidas::util::LogScheme scheme = logger->getScheme();
+    std::string value = scheme.getParameter("trace_samples");
+    _matcher.addCriteria(value);
+}
+
+}}	// namespace nidas namespace core
+
+#endif  // NIDAS_CORE_SAMPLETRACER_H
