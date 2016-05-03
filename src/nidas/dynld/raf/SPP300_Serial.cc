770c6aab153edd134acd44801c1b339b1d35aa44
Chris Webster <cjw@ucar.edu>
2016-05-03 03:56:15 -0600

    Remove _model member.  Unused.

--- src/nidas/dynld/raf/SPP300_Serial.cc
+++ src/nidas/dynld/raf/SPP300_Serial.cc
@@ -69,11 +69,6 @@ SPP300_Serial::SPP300_Serial(): SppSerial("SPP300"),
     _nChannels = 0; // back to zero until it gets set via configuration
 
     //
-    // Model number is fixed
-    //
-    _model = 300;
-
-    //
     // This number should match the housekeeping added in ::process, so that
     // an output sample of the correct size is created.
     //
