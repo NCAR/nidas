dfbaea5bc29f6ecf1b9707ba900d4efce7c9aee8
Chris Webster <cjw@ucar.edu>
2017-02-28 07:13:45 -0700

    - Change to inherit from DSMSerialSensor not CharacterSensor (we used   LamsNetSensor class as a starting point). - Remove unused #includes - Correct doxygen class description comment. - move crcCheck to protected. - cruft removal (leftover LAMS stuff).

--- src/nidas/dynld/raf/WatlowCLS208.h
+++ src/nidas/dynld/raf/WatlowCLS208.h
@@ -23,19 +23,12 @@
  **
  ********************************************************************
 */
-/* Watlow.h
- *
- */
-
 
 #ifndef NIDAS_DYNLD_RAF_WATLOAD_H
 #define NIDAS_DYNLD_RAF_WATLOAD_H
 
-#include <iostream>
-#include <iomanip>
 
-#include "SppSerial.h"
-#include <nidas/dynld/UDPSocketSensor.h>
+#include <nidas/dynld/DSMSerialSensor.h>
 #include <nidas/util/EndianConverter.h>
 
 #include <nidas/util/InvalidParameterException.h>
@@ -45,44 +38,24 @@ namespace nidas { namespace dynld { namespace raf {
 using namespace nidas::core;
 
 /**
- * Sensor class supporting the NCAR/EOL Laser Air Motion Sensor (LAMS 3-beam)
+ * Sensor class supporting the NCAR/ACOM Watlow CLS208 Intrument.
  */
-class Watlow : public CharacterSensor
+class Watlow : public DSMSerialSensor
 {
 public:
-    //Watlow();
-   // ~Watlow();
-   // unsigned short crcCheck(const int16_t * pkt, int len);
-           
-    uint16_t crcCheck(unsigned char * input, int messageLength, int start)
-         throw();
+
     bool process(const Sample* samp,std::list<const Sample*>& results)
         throw();
 
+protected:
+
+    uint16_t crcCheck(unsigned char * input, int messageLength, int start)
+         throw();
+
 private:
 
-/*    static const int LAMS_SPECTRA_SIZE = 512;
-
-    const Sample *_saveSamps[nBeams];
-
-    size_t _unmatchedSamples;
-
-    size_t _outOfSequenceSamples;
-
-    /// beams come in two packets, so need to re-enter process().
-    size_t _beam;
-
-    uint32_t _prevSeqNum[nBeams];
-*/
     static const nidas::util::EndianConverter * _fromBig;
 
-//    int numOutValues;
-    /** No copying. */
-   // Watlow(const Watlow&);
-
-    /** No assignment. */
-//    Watlow& operator=(const Watlow&);
- 
 };
 
 }}}
