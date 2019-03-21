d266fea73745b31a62361eb977522e3770d0afa5
Paul Ourada <ourada@ucar.edu>
2019-03-21 15:01:22 -0600

    Output port value read using scanf format, instead of cout.

--- src/nidas/util/XcvrGPIO.h
+++ src/nidas/util/XcvrGPIO.h
@@ -205,7 +205,7 @@ public:
         unsigned char retval = adjustBitPosition(_shadow, true);
         if (ifaceFound()) {
             retval = adjustBitPosition(_pFtdiDevice->read(), true);
-            DLOG(("XcvrGPIO::read(): Actually read the device and the value is: ") << retval);
+            DLOG(("XcvrGPIO::read(): Actually read the device and the value is: 0x%02x", retval));
         }
         return retval;
     }
