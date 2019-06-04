3dbf504b7c49aba154c95f92c512d2f3e93faa1c
Gordon Maclean <maclean@users.noreply.github.com>
2019-06-04 09:15:13 -0600

    Clean up comments

--- src/nidas/apps/tee_i2c.cc
+++ src/nidas/apps/tee_i2c.cc
@@ -24,11 +24,8 @@
  ********************************************************************
 */
 /*
-
     Simple program to "tee" the byte stream from an I2C device
     to one or more pseudo-terminals.
-
-    TODO: As necessary, support other device protocols.
 
     Currently it is targeted at a ublox NEO 5Q GPS, that
     is attached to a Raspberry Pi2.
@@ -37,20 +34,31 @@
 
     u-blox5_Protocol_Specifications(GPS.G5-X-07036).pdf
 
-    Things work in a minimal, but perhaps sufficient, way.
-    Can read the stream of NMEA packets from address 0xff.
-    Reads of the length bytes at addresses 0xfd and 0xfe just
-    return characters from the NMEA stream.
-    Attempts at sending configure packets fail, such as trying
-    to disable some NMEA messages.
-    Have to put a usleep in the read loop, otherwise after several
-    minutes, all I2C reads fail, with timeout errors until power cycle.
-    Soft reboot doesn't fix it.
-    This was a power cycle of both the RPI2 and the ublox.
-    Don't know if we have the capability to power cycle only the
-    ublox. Until we get writes to work, can't reset it by software.
+    The I2C protocol used here does not provide blocking reads. The
+    read system calls are implemented with ioctls(), which return
+    any data available.  So the device must be polled. Currently the
+    poll is done every 1/10 second.
 
-    Need to test i2c_block_reads again, without reading the length
+    Our implementation of ublox 5/6 over I2C on an RPi suffers from data
+    corruption. This may be related to the clock-stretching bug:
+    https://www.advamation.com/knowhow/raspberrypi/rpi-i2c-bug.html,
+    but this has not been verified.
+
+    Also, for some reason on an RPi, the reads are subject to
+    errors: EIO, EREMOTEIO.  Unless EXIT_ON_ERRORS is defined,
+    just keep going, without throwing exceptions, logging the error
+    or exiting. Perhaps this is related to the clock-stetching problem.
+
+    This program also has code for the UBX protocol, the result of someone
+    not RTFM. The ublox 5/6 do not support the binary UBX protocol over I2C,
+    only RS232.  Only the ASCII UBX and NMEA messages are supported with I2C.
+    The UBX protocol code has been left in for possible future RS232 
+    implementation.
+
+    The ublox program in nidas/apps sends UBX,40 messages to
+    enable/disable individual NMEA messages.
+
+    TODO: test i2c_block_reads again, without reading the length
     bytes from 0xfd, 0xfe.
 */
 
@@ -85,139 +93,6 @@ bool interrupted = false;
 // before reading the message stream in 0xff.
 #define READ_LEN_REGS
 
-/**
- * UBX configure packets.
- */
-struct ubx_cfg_msg {
-    unsigned char header[2];
-    unsigned char cls;
-    unsigned char id;
-    unsigned short length;
-    unsigned char payload[8];
-    unsigned char cksum[2];
-};
-
-struct ubx_cfg_prt {
-    unsigned char header[2];
-    unsigned char cls;
-    unsigned char id;
-    unsigned short length;
-    unsigned char payload[20];
-    unsigned char cksum[2];
-};
-
-struct cfg_prt_payload {
-    unsigned char portID;
-    unsigned char res0[3];
-    unsigned int mode;
-    unsigned int res1;
-    unsigned short inProtoMask;
-    unsigned short outProtoMask;
-    unsigned short flags;
-    unsigned short res2;
-};
-
-void ubx_cksum(const unsigned char* cp, unsigned char* cksum)
-{
-    unsigned char ck_a = 0, ck_b = 0;
-    for ( ; cp < cksum; cp++) {
-        ck_a += *cp;
-        ck_b += ck_a;
-    }
-    cksum[0] = ck_a;
-    cksum[1] = ck_b;
-}
-
-/**
- * Attempt at sending configuration packets to a ublox NEO-5Q.
- * Doesn't work. Writes just return timeout errors, and no
- * acks are received back.  Not sure where the problem lies. 
- */
-void ubx_config(int fd, const string& name) throw(n_u::IOException)
-{
-    unsigned char *mp;
-    int wrres;
-
-    struct ubx_cfg_prt pmsg;
-    struct cfg_prt_payload ppay;
-    pmsg.header[0] = 0xb5;
-    pmsg.header[1] = 0x62;
-    pmsg.cls = 0x06;
-    pmsg.id = 0x00;
-    pmsg.length = 20;
-    memset(&ppay,0,sizeof(ppay));
-    ppay.mode = 0x42;           // i2c address
-    ppay.inProtoMask = 0x3;     // NMEA and UBX
-    ppay.outProtoMask = 0x3;        // NMEA and UBX
-    memcpy(pmsg.payload,&ppay,20);
-    ubx_cksum(&pmsg.cls,&pmsg.cksum[0]);
-    mp = (unsigned char*) &pmsg;
-
-#define DO_SMBUS_WRITE
-#ifdef DO_SMBUS_WRITE
-    for ( ; mp <= &pmsg.cksum[1]; mp++) {
-        wrres = i2c_smbus_write_byte(fd, *mp);
-        if (wrres < 0) {
-            n_u::IOException e(name,"write",errno);
-            cerr << "Write error: " << e.what() << endl;
-        }
-    }
-#else
-    wrres = write(fd, mp, sizeof(pmsg));
-    if (wrres < 0) {
-        n_u::IOException e(name,"write",errno);
-        cerr << "Write error: " << e.what() << endl;
-    }
-#endif
-    for (int i = 0; i < 10; i++) {
-        int db = i2c_smbus_read_byte(fd);
-        if (db < 0) {
-            n_u::IOException e(name,"read",errno);
-            cerr << "read error: " << e.what() << endl;
-        }
-        cerr << "db=" << db << ' ' <<  hex << ((unsigned int)db & 0xff) << dec << endl;
-    }
-
-    struct ubx_cfg_msg msg;
-    msg.header[0] = 0xb5;
-    msg.header[1] = 0x62;
-    msg.cls = 0x06;
-    msg.id = 0x01;
-    msg.length = 8;
-    msg.payload[0] = 0xf0;
-    msg.payload[1] = 0x05;  // turn off VTG
-    for (int i = 2; i < 7; i++) msg.payload[i] = 0;
-    msg.payload[7] = 1;
-
-    ubx_cksum(&msg.cls,&msg.cksum[0]);
-    cerr << "cksum=" << hex << (unsigned int) msg.cksum[0] <<
-        ' ' << (unsigned int) msg.cksum[1] << dec << endl;
-
-    mp = (unsigned char*) &msg;
-#ifdef DO_SMBUS_WRITE
-    for ( ; mp <= &msg.cksum[1]; mp++) {
-        wrres = i2c_smbus_write_byte(fd, *mp);
-        if (wrres < 0) {
-            n_u::IOException e(name,"write",errno);
-            cerr << "Write error: " << e.what() << endl;
-        }
-    }
-#else
-    wrres = write(fd, mp, sizeof(msg));
-    if (wrres < 0) {
-        n_u::IOException e(name,"write",errno);
-        cerr << "Write error: " << e.what() << endl;
-    }
-#endif
-    for (int i = 0; i < 10; i++) {
-        int db = i2c_smbus_read_byte(fd);
-        if (db < 0) {
-            n_u::IOException e(name,"read",errno);
-            cerr << "read error: " << e.what() << endl;
-        }
-        cerr << "db=" << db << ' ' <<  hex << ((unsigned int)db & 0xff) << dec << endl;
-    }
-}
 
 class TeeI2C {
 public:
@@ -276,7 +151,9 @@ TeeI2C::TeeI2C():
     Priority("-p,--priority", "priority",
              "Set FIFO priority: 0-99, where 0 is low and 99 is highest.\n"
              "If process lacks sufficient permissions,\n"
-             "a warning will be logged but program will continue."),
+             "a warning will be logged but program will continue.\n"
+             "Note: since this just polls the I2C at a fairly slow rate,\n"
+             "there isn't much to be gained by increasing the priority.\n"),
     Foreground("-f,--foreground", "", "Run foreground, not as background daemon"),
     KeepMSB("-k,--keepmsb", "",
             "By default the most-significant bit is cleared, to work\n"
@@ -450,21 +327,9 @@ int TeeI2C::run() throw()
             throw n_u::IOException(_i2cname, ost.str(), errno);
         }
 
-        // ubx_config(_i2cfd, _i2cname);
-
         for (interrupted = false; !interrupted; ) {
 
-            /* Sleep a bit before next read
-             * This seems to be important. Without it
-             * reads eventually continually fail with timeout errors,
-             * after several minutes; no data until a power cycle.
-             * Posts on the net talk about ublox GPSs
-             * "applying arbitrary clock stretches"
-             * whatever that means.  Some folks have resorted to
-             * doing direct bit-banging. 
-             *
-             * The i2c chip/driver on the RPi seems to be a minimal
-             * implementation not really supporting blocking reads.
+            /* Sleep a 1/10 second before next read.
              */
             usleep(USECS_PER_SEC / 10);
 
@@ -479,6 +344,97 @@ int TeeI2C::run() throw()
         result = 1;
     }
     return result;
+}
+
+void TeeI2C::i2c_byte_reads() throw(n_u::IOException)
+{
+    unsigned char i2cbuf[8192];
+    // Unless KeepMSB is set, clear the high bit before writing to ptys.
+    unsigned int mask = 0x7f;
+    unsigned int msbfound = 0;
+
+    if (KeepMSB.asBool())
+        mask = 0xff;
+
+    int lena = (signed)sizeof(i2cbuf)-1;
+
+#ifdef READ_LEN_REGS
+    // registers 0xfd, 0xfe are the (big-endian) number of bytes
+    // in the ublox buffer.  After selecting register 0xfd,
+    // it is auto-incremented on a read, and once it reaches
+    // 0xff (the buffer), remains there.
+
+    // number of bytes available, high byte
+    int hb = i2c_smbus_read_byte_data(_i2cfd, 0xfd);
+
+
+    if (hb < 0)
+#ifdef EXIT_ON_IOERRORS
+        throw n_u::IOException(_i2cname,"read",errno);
+#else
+        return;
+#endif
+
+    // ublox buffer is 4kB, high len byte can't be 0xff.
+    // perhaps 0xff is an indication of no data available.
+    if (hb == 0xff) return;
+
+    // next address 0xfe, number of bytes available, low byte
+    int lb = i2c_smbus_read_byte(_i2cfd);
+    if (lb < 0)
+#ifdef EXIT_ON_IOERRORS
+        throw n_u::IOException(_i2cname,"read",errno);
+#else
+        return;
+#endif
+
+    lena = (hb & 0xff) << 8 | (lb & 0xff);
+
+    if (lena == 0) return;
+    // screen bad values
+    if (lena < 0) lena = (signed)sizeof(i2cbuf)-1;
+    else lena = std::min(lena, (signed)sizeof(i2cbuf)-1);
+#endif
+
+    int len;
+    for (len = 0; len < lena; len++) {
+
+        int db;
+#ifndef READ_LEN_REGS
+        if (len == 0)
+            db = i2c_smbus_read_byte_data(_i2cfd, 0xff);
+        else
+#endif
+            db = i2c_smbus_read_byte(_i2cfd);
+
+#ifdef EXIT_ON_IOERRORS
+        if (db < 0) 
+            throw n_u::IOException(_i2cname,"read_byte",errno);
+#else
+        if (db < 0) break;
+#endif
+        if (db == 0xff) break;
+        if (db > 0xff)
+        {
+            ++msbfound;
+            VLOG(("i2c returned %u bytes > 0xff: %0x '%c' '%c'",
+                  msbfound, db, (db >> 8) & 0x7f, db & 0x7f));
+            if (msbfound % 100 == 1)
+            {
+                NLOG(("%d bytes > 0xff from i2c so far", msbfound));
+            }
+        }
+        i2cbuf[len] = (db & mask);
+    }
+    // XXX what should happen if 0xff never found?  Do we really want
+    // to dump 8192 bytes of unterminated i2c data to the ptys?
+    // cerr << "len=" << len << endl;
+    if (len > 0) {
+        i2cbuf[len] = '\0';
+        VLOG(("writing ") << len << " bytes to ptys: "
+             << n_u::addBackslashSequences(string(i2cbuf, i2cbuf+len)));
+        writeptys(i2cbuf, len);
+    }
 }
 
 void TeeI2C::i2c_block_reads() throw(n_u::IOException)
@@ -551,103 +507,6 @@ void TeeI2C::i2c_block_reads() throw(n_u::IOException)
     cerr << "buf= \"" << string((const char*)i2cbuf,l2) << "\"" << endl;
     writeptys(i2cbuf,l2);
 }
-
-void TeeI2C::i2c_byte_reads() throw(n_u::IOException)
-{
-    unsigned char i2cbuf[8192];
-    // Unless KeepMSB is set, clear the high bit before writing to ptys.
-    unsigned int mask = 0x7f;
-    unsigned int msbfound = 0;
-
-    if (KeepMSB.asBool())
-        mask = 0xff;
-
-    int lena = (signed)sizeof(i2cbuf)-1;
-
-#ifdef READ_LEN_REGS
-    // registers 0xfd, 0xfe are the (big-endian) number of bytes
-    // in the ublox buffer.  After selecting register 0xfd,
-    // it is auto-incremented on a read, and once it reaches
-    // 0xff (the buffer), remains there.
-
-    // number of bytes available, high byte
-    int hb = i2c_smbus_read_byte_data(_i2cfd, 0xfd);
-
-    // These reads are subject to
-    // IOException: /dev/i2c-1: read: Remote I/O
-    // IOException: /dev/i2c-1: read: Input/output error
-    // Unless EXIT_ON_ERRORS is defined, just keep going,
-    // without throwing, logging the error or exiting, 
-
-    if (hb < 0)
-#ifdef EXIT_ON_IOERRORS
-        throw n_u::IOException(_i2cname,"read",errno);
-#else
-        return;
-#endif
-
-    // ublox buffer is 4kB, high len byte can't be 0xff.
-    // perhaps 0xff is an indication of no data available.
-    if (hb == 0xff) return;
-
-    // next address 0xfe, number of bytes available, low byte
-    int lb = i2c_smbus_read_byte(_i2cfd);
-    if (lb < 0)
-#ifdef EXIT_ON_IOERRORS
-        throw n_u::IOException(_i2cname,"read",errno);
-#else
-        return;
-#endif
-
-    lena = (hb & 0xff) << 8 | (lb & 0xff);
-
-    if (lena == 0) return;
-    // screen bad values
-    if (lena < 0) lena = (signed)sizeof(i2cbuf)-1;
-    else lena = std::min(lena, (signed)sizeof(i2cbuf)-1);
-#endif
-
-    int len;
-    for (len = 0; len < lena; len++) {
-
-        int db;
-#ifndef READ_LEN_REGS
-        if (len == 0)
-            db = i2c_smbus_read_byte_data(_i2cfd, 0xff);
-        else
-#endif
-            db = i2c_smbus_read_byte(_i2cfd);
-
-#ifdef EXIT_ON_IOERRORS
-        if (db < 0) 
-            throw n_u::IOException(_i2cname,"read_byte",errno);
-#else
-        if (db < 0) break;
-#endif
-        if (db == 0xff) break;
-        if (db > 0xff)
-        {
-            ++msbfound;
-            VLOG(("i2c returned %u bytes > 0xff: %0x '%c' '%c'",
-                  msbfound, db, (db >> 8) & 0x7f, db & 0x7f));
-            if (msbfound % 100 == 1)
-            {
-                NLOG(("%d bytes > 0xff from i2c so far", msbfound));
-            }
-        }
-        i2cbuf[len] = (db & mask);
-    }
-    // XXX what should happen if 0xff never found?  Do we really want
-    // to dump 8192 bytes of unterminated i2c data to the ptys?
-    // cerr << "len=" << len << endl;
-    if (len > 0) {
-        i2cbuf[len] = '\0';
-        VLOG(("writing ") << len << " bytes to ptys: "
-             << n_u::addBackslashSequences(string(i2cbuf, i2cbuf+len)));
-        writeptys(i2cbuf, len);
-    }
-}
-
 void TeeI2C::writeptys(const unsigned char* buf, int len)
     throw(n_u::IOException)
 {
@@ -705,3 +564,139 @@ int main(int argc, char** argv)
     return tee.run();
 }
 
+/**
+ * UBX protocol configure packets, not supported in I2C.
+ * This code is left here for possible future RS232 implementations.
+ */
+struct ubx_cfg_msg {
+    unsigned char header[2];
+    unsigned char cls;
+    unsigned char id;
+    unsigned short length;
+    unsigned char payload[8];
+    unsigned char cksum[2];
+};
+
+struct ubx_cfg_prt {
+    unsigned char header[2];
+    unsigned char cls;
+    unsigned char id;
+    unsigned short length;
+    unsigned char payload[20];
+    unsigned char cksum[2];
+};
+
+struct cfg_prt_payload {
+    unsigned char portID;
+    unsigned char res0[3];
+    unsigned int mode;
+    unsigned int res1;
+    unsigned short inProtoMask;
+    unsigned short outProtoMask;
+    unsigned short flags;
+    unsigned short res2;
+};
+
+void ubx_cksum(const unsigned char* cp, unsigned char* cksum)
+{
+    unsigned char ck_a = 0, ck_b = 0;
+    for ( ; cp < cksum; cp++) {
+        ck_a += *cp;
+        ck_b += ck_a;
+    }
+    cksum[0] = ck_a;
+    cksum[1] = ck_b;
+}
+
+/**
+ * Attempt at sending configuration packets to a ublox NEO-5/6Q,
+ * using the UBX protocol.  This doesn't work over I2C, only RS232.
+ * With I2C, writes just return timeout errors, and no acks are
+ * received back. Leave the code here for possible future RS232
+ * implementations.
+ */
+void ubx_config(int fd, const string& name) throw(n_u::IOException)
+{
+    unsigned char *mp;
+    int wrres;
+
+    struct ubx_cfg_prt pmsg;
+    struct cfg_prt_payload ppay;
+    pmsg.header[0] = 0xb5;
+    pmsg.header[1] = 0x62;
+    pmsg.cls = 0x06;
+    pmsg.id = 0x00;
+    pmsg.length = 20;
+    memset(&ppay,0,sizeof(ppay));
+    ppay.mode = 0x42;           // i2c address
+    ppay.inProtoMask = 0x3;     // NMEA and UBX
+    ppay.outProtoMask = 0x3;        // NMEA and UBX
+    memcpy(pmsg.payload,&ppay,20);
+    ubx_cksum(&pmsg.cls,&pmsg.cksum[0]);
+    mp = (unsigned char*) &pmsg;
+
+#define DO_SMBUS_WRITE
+#ifdef DO_SMBUS_WRITE
+    for ( ; mp <= &pmsg.cksum[1]; mp++) {
+        wrres = i2c_smbus_write_byte(fd, *mp);
+        if (wrres < 0) {
+            n_u::IOException e(name,"write",errno);
+            cerr << "Write error: " << e.what() << endl;
+        }
+    }
+#else
+    wrres = write(fd, mp, sizeof(pmsg));
+    if (wrres < 0) {
+        n_u::IOException e(name,"write",errno);
+        cerr << "Write error: " << e.what() << endl;
+    }
+#endif
+    for (int i = 0; i < 10; i++) {
+        int db = i2c_smbus_read_byte(fd);
+        if (db < 0) {
+            n_u::IOException e(name,"read",errno);
+            cerr << "read error: " << e.what() << endl;
+        }
+        cerr << "db=" << db << ' ' <<  hex << ((unsigned int)db & 0xff) << dec << endl;
+    }
+
+    struct ubx_cfg_msg msg;
+    msg.header[0] = 0xb5;
+    msg.header[1] = 0x62;
+    msg.cls = 0x06;
+    msg.id = 0x01;
+    msg.length = 8;
+    msg.payload[0] = 0xf0;
+    msg.payload[1] = 0x05;  // turn off VTG
+    for (int i = 2; i < 7; i++) msg.payload[i] = 0;
+    msg.payload[7] = 1;
+
+    ubx_cksum(&msg.cls,&msg.cksum[0]);
+    cerr << "cksum=" << hex << (unsigned int) msg.cksum[0] <<
+        ' ' << (unsigned int) msg.cksum[1] << dec << endl;
+
+    mp = (unsigned char*) &msg;
+#ifdef DO_SMBUS_WRITE
+    for ( ; mp <= &msg.cksum[1]; mp++) {
+        wrres = i2c_smbus_write_byte(fd, *mp);
+        if (wrres < 0) {
+            n_u::IOException e(name,"write",errno);
+            cerr << "Write error: " << e.what() << endl;
+        }
+    }
+#else
+    wrres = write(fd, mp, sizeof(msg));
+    if (wrres < 0) {
+        n_u::IOException e(name,"write",errno);
+        cerr << "Write error: " << e.what() << endl;
+    }
+#endif
+    for (int i = 0; i < 10; i++) {
+        int db = i2c_smbus_read_byte(fd);
+        if (db < 0) {
+            n_u::IOException e(name,"read",errno);
+            cerr << "read error: " << e.what() << endl;
+        }
+        cerr << "db=" << db << ' ' <<  hex << ((unsigned int)db & 0xff) << dec << endl;
+    }
+}
