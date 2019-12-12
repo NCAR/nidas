6a8b483a296d65465637b31fd9707622e4132d92
Gary Granger <granger@ucar.edu>
2019-12-12 16:38:25 -0700

    oops, missed this file in last commit

--- src/tests/gps/tgps.cc
+++ src/tests/gps/tgps.cc
@@ -19,22 +19,28 @@ using std::vector;
 
 struct trec_t
 {
-  const char* input;
-  const char* stime;
-  bool ok;
-}
-trecs[] =
+    const char* input;
+    const char* stime;
+    bool ok;
+};
+
+struct trec_t trecs[] =
 {
-  {
-    "$GPGGA,044043.00,4012.55682,N,08824.39657,W,2,12,03,224.6,M,-33.2,M,,0000*6C\r\n",
-    "2018 10 01 04:40:43.0580",
-    true
-  },
-  {
-    "$GPRMC144634.00,A,4012.55779,N,08824.3962,W,0.010,,041018,,,D*6A\r\n",
-    "2018 10 04 14:46:34.0885",
-    false
-  }
+ {
+  "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A*6E\r\n",
+  "2019 12 12 22:00:09.0000",
+  true
+ },
+ {
+  "$GPGGA,044043.00,4012.55682,N,08824.39657,W,2,12,03,224.6,M,-33.2,M,,0000*6C\r\n",
+  "2018 10 01 04:40:43.0000",
+  true
+ },
+ {
+  "$GPRMC144634.00,A,4012.55779,N,08824.3962,W,0.010,,041018,,,D*6A\r\n",
+  "2018 10 04 14:46:34.0000",
+  false
+ }
 };
 
 
@@ -45,142 +51,111 @@ BOOST_AUTO_TEST_CASE(test_gps_time_rejects)
 
     GPS_NMEA_Serial* gps = new GPS_NMEA_Serial();
 
+    const char* ckmsg = trecs[0].input;
+    char cksum;
+    bool gotck = gps->findChecksum(cksum, ckmsg, strlen(ckmsg));
+    BOOST_CHECK_EQUAL(gotck, true);
+    BOOST_CHECK_EQUAL(cksum, 0x6e);
+    BOOST_CHECK_EQUAL(gps->checksumOK(ckmsg, strlen(ckmsg)), true);
+
+    vector<double> d(10);
+
     for (unsigned int i = 0; i < sizeof(trecs)/sizeof(trecs[0]); ++i)
     {
       BOOST_TEST_MESSAGE(">>> running test data #" << i << "...");
       trec_t& tc = trecs[i];
 
-      dsm_time_t tsys = UTime::parse(true, tc.stime,
+      dsm_time_t texp = UTime::parse(true, tc.stime,
 				     "%Y %m %d %H:%M:%S.%4f").toUsecs();
-      vector<double> d(10);
+      dsm_time_t traw = texp + USECS_PER_SEC/2;
       dsm_time_t tt;
       if (tc.input[3] == 'G')
-	tt = gps->parseGGA(tc.input, &(d[0]), d.size(), tsys);
+	tt = gps->parseGGA(tc.input+7, &(d[0]), d.size(), traw);
       else
-	tt = gps->parseRMC(tc.input, &(d[0]), d.size(), tsys);
+	tt = gps->parseRMC(tc.input+7, &(d[0]), d.size(), traw);
       string tgot = UTime(tt).format(true, "%Y %m %d %H:%M:%S.%4f");
+      string sraw = UTime(traw).format(true, "%Y %m %d %H:%M:%S.%4f");
       BOOST_TEST_MESSAGE("expected " << tc.stime << ", parsed time " << tgot);
-      BOOST_CHECK_EQUAL(tc.stime, tgot);
+      BOOST_CHECK_EQUAL(tc.ok ? tc.stime : sraw, tgot);
 
+#ifdef notdef
+      // There's no point to this until we can fake
+      // GPS_NMEA_Serial::process() into processing messages without a
+      // corresponding tag id, ie, _rmcId needs to be non-zero.
+
+      // Process a raw sample.  It should succeed, and the new sample time
+      // should match the parsed time instead of the raw time.
       SampleT<char>* sample = getSample<char>(strlen(tc.input)+1);
       strcpy(sample->getDataPtr(), tc.input);
-      sample->setTimeTag(tsys);
+      sample->setTimeTag(traw);
 
-      // Process a raw sample.
       std::list<const Sample*> results;
       bool result = gps->process(sample, results);
       BOOST_CHECK_EQUAL(tc.ok, result);
+      if (result)
+      {
+	  BOOST_CHECK_EQUAL(results.front()->getTimeTag(), texp);
+      }
       sample->freeReference();
+#endif
     }
 
+    const char* goodmsg =
+    "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A*6E";
 
-#ifdef notdef
-    nidas::util::auto_ptr<xercesc::DOMDocument>
-	doc(parseXMLConfigFile("test_gps.xml"));
+    // Make sure we can add checksum and things still work.
+    char msg[100] =
+    "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A";
+    cksum = gps->calcChecksum(msg, strlen(msg));
+    gps->appendChecksum(msg, strlen(msg), sizeof(msg));
+    BOOST_CHECK_EQUAL(string(msg), string(goodmsg));
+    BOOST_CHECK_EQUAL(gps->checksumOK(msg, strlen(msg)), true);
 
-    Project* project = Project::getInstance();
-    project->fromDOMElement(doc->getDocumentElement());
+    // Given an array of RMC messages with various errors, make sure none
+    // of them pass even though the checksum is correct.
+    char badmsgs[][128] =
+    {
+     "digit missing from time",
+     "$GPRMC,22009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A",
+     "digit missing from date",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,11219,,,A",
+     "digit replaced with random character",
+     "$GPRMC,22000a.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A",
+     "time digit replaced with negative sign",
+     "$GPRMC,-20009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A",
+     "year digit replaced with negative sign",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,1212-9,,,A",
+     "month digit replaced with negative sign",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,12-219,,,A",
+     "month out of range",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,121319,,,A",
+     "day out of range",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,001219,,,A",
+     "decimal replaced with number",
+     "$GPRMC,220009100,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A",
+     "missing decimal",
+     "$GPRMC,22000900,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A",
+     "good message",
+     "$GPRMC,220009.00,A,4002.29363,N,10514.51724,W,0.750,,121219,,,A"
+    };
 
-
-
-
-    dsm_sample_id_t sid = 0;
-    sid = SET_DSM_ID(sid, 34);
-    sid = SET_SPS_ID(sid, 102);
-
-    DSMSensor* sensor = project->findSensor(sid);
-    BOOST_CHECK(sensor);
-
-    NCAR_TRH* trh = static_cast<NCAR_TRH*>(sensor);
-    BOOST_CHECK(trh);
-    trh->init();
-
-    // Raw conversions should start out disabled.
-    std::vector<float> ta = trh->getRawTempCoefficients();
-    BOOST_CHECK_EQUAL(ta.size(), 0);
-    std::vector<float> ha = trh->getRawRHCoefficients();
-    BOOST_CHECK_EQUAL(ha.size(), 0);
-
-    // Create a raw sample.
-    const char* msg = "TRH8 15.65 44.43 5 0 1391 90 16\r\n";
-    SampleT<char>* sample = getSample<char>(strlen(msg)+1);
-    strcpy(sample->getDataPtr(), msg);
-    dsm_time_t tt = UTime(true, 2017, 4, 20, 23, 52, 06.488).toUsecs();
-    sample->setTimeTag(tt);
-
-    // Process a sample, which should trigger a read of the cal files
-    // and setting up raw conversions.
-    std::list<const Sample*> results;
-    BOOST_CHECK(trh->process(sample, results));
-
-    // 2017 apr 20 17:50:33 raw -40.69662       0.04097045      -3.429248e-07
-    // Make sure the TRH got the coefficients.
-    ta = trh->getRawTempCoefficients();
-    BOOST_CHECK_EQUAL(ta.size(), 3);
-    BOOST_CHECK_CLOSE(ta[0], -40.69662, 0.0001);
-    BOOST_CHECK_CLOSE(ta[1], 0.04097045, 0.0001);
-    BOOST_CHECK_CLOSE(ta[2], -3.429248e-07, 0.0001);
-
-    // 2017 apr 20 17:50:33 raw 401.0063 -2.896463 -0.0005389938 -16.3027 0.1413339
-    ha = trh->getRawRHCoefficients();
-    BOOST_CHECK_EQUAL(ha.size(), 5);
-    BOOST_CHECK_CLOSE(ha[0], 401.0063, 0.0001);
-    BOOST_CHECK_CLOSE(ha[1], -2.896463, 0.0001);
-    BOOST_CHECK_CLOSE(ha[2], -0.0005389938, 0.000001);
-    BOOST_CHECK_CLOSE(ha[3], -16.3027, 0.0001);
-    BOOST_CHECK_CLOSE(ha[4], 0.1413339, 0.00001);
-
-    // Make sure the values in the sample match the expected conversions.
-    const SampleT<float>* outs;
-    outs = static_cast<const SampleT<float>*>(results.front());
-    BOOST_CHECK(outs);
-    const float *fp = outs->getConstDataPtr();
-    float Traw = 1391;
-    float RHraw = 90;
-    float T = trh->tempFromRaw(Traw);
-    float RH = trh->rhFromRaw(RHraw, T);
-    BOOST_CHECK_CLOSE(fp[1], T, 0.00001);
-    BOOST_CHECK_CLOSE(fp[2], RH, 0.00001);
-    BOOST_CHECK_EQUAL(fp[4], 1391);
-    BOOST_CHECK_EQUAL(fp[5], 90);
-
-    // Test that we can reset raw conversions through the cal file.
-    tt = UTime(true, 2017, 4, 21, 8, 15, 0).toUsecs();
-    sample->setTimeTag(tt);
-    results.clear();
-    BOOST_CHECK(trh->process(sample, results));
-
-    ha = trh->getRawRHCoefficients();
-    ta = trh->getRawTempCoefficients();
-    BOOST_CHECK_EQUAL(ha.size(), 0);
-    BOOST_CHECK_EQUAL(ta.size(), 0);
-
-    outs = static_cast<const SampleT<float>*>(results.front());
-    BOOST_CHECK(outs);
-    fp = outs->getConstDataPtr();
-    BOOST_CHECK(isnan(fp[1]));
-    BOOST_CHECK(isnan(fp[2]));
-    BOOST_CHECK_EQUAL(fp[4], Traw);
-    BOOST_CHECK_EQUAL(fp[5], RHraw);
-
-    float xta[] = { 1, 2, 3 };
-    float xha[] = { 1, 2, 3, 4, 5 };
-    trh->setRawTempCoefficients(xta);
-    trh->setRawRHCoefficients(xha);
-    ha = trh->getRawRHCoefficients();
-    ta = trh->getRawTempCoefficients();
-    BOOST_CHECK_EQUAL(ta.size(), 3);
-    BOOST_CHECK_EQUAL(ha.size(), 5);
-    BOOST_CHECK_EQUAL(ta[2], 3);
-    BOOST_CHECK_EQUAL(ha[4], 5);
-
-    // Test that we can set and reset raw conversions through the API.
-    trh->setRawTempCoefficients();
-    trh->setRawRHCoefficients();
-    ha = trh->getRawRHCoefficients();
-    ta = trh->getRawTempCoefficients();
-    BOOST_CHECK_EQUAL(ha.size(), 0);
-    BOOST_CHECK_EQUAL(ta.size(), 0);
-#endif
+    dsm_time_t texp = UTime::parse(true, "2019 12 12 22:00:09.0000",
+				   "%Y %m %d %H:%M:%S.%4f").toUsecs();
+    unsigned long i = 0;
+    while (i < sizeof(badmsgs)/sizeof(badmsgs[0]))
+    {
+	char* desc = badmsgs[i];
+	std::cerr << desc << std::endl;
+	char* imsg = badmsgs[++i];
+	gps->appendChecksum(msg, strlen(imsg), sizeof(badmsgs[i]));
+	dsm_time_t tt;
+	tt = gps->parseRMC(imsg+7, &(d[0]), d.size(), 1);
+	// Returning the raw time means it did not parse.
+	if (strcmp(desc, "good message"))
+	    BOOST_CHECK_EQUAL(tt, 1);
+	else
+	    BOOST_CHECK_EQUAL(tt, texp);
+	++i;
+    }
 }
 
