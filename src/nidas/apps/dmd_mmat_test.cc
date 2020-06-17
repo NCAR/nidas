73bf370a1053b9f5f85837be8cca094ddac0bbf8
JCarnesNCAR <jcarnes@ucar.edu>
2020-06-16 18:34:00 -0600

    bug fixes, const vout support
    - corrected float calculation of outscale
    - added support for constant vout (without waveform generator) for when vstart==vend. Does not require onboard counter and values persist after utility closes
    - clarified usage description

--- src/nidas/apps/dmd_mmat_test.cc
+++ src/nidas/apps/dmd_mmat_test.cc
@@ -54,10 +54,11 @@ namespace n_u = nidas::util;
 
 struct waveout
 {
-    int chan;
-    int len;
-    float vmin;
-    float vmax;
+    int chan;      // Output Voltage Channel
+    int len;       // Ramp length
+    float vstart;  // Ramp min voltage
+    float vend;    // Ramp max voltage
+    bool constant; // Waveform is a constant value, T/F
 };
 
 class DMD_MMAT_test
@@ -81,19 +82,21 @@ DMD_MMAT_test::DMD_MMAT_test():
 
 int DMD_MMAT_test::usage(const char * argv0)
 {
-    cerr << "usage: " << argv0 << " [-a] [-r rate] [-w chan,len,vmin,vmax ...] devicename\n\
-    -a: perform MM32XAT A2D autocalibration sequence.\n\
+    cerr << "usage: " << argv0 << " [-a] [-r rate] [-w ch,len,vstart,vend] [-w ch,len,vstart,vend] devicename\n\
+    -a: perform MM32XAT A2D autocalibration sequence. Must target A2D devicename.\n\
     -r rate: waveform output rate in Hz\n\
-    -w chan,vmin,vmax: output a sawtooth waveform of given length on a channel, within the voltage range\n\
+    -w chan,len,vstart,vend: output a sawtooth waveform of given length on a channel, within the voltage range\n\
+           Multiple -w waveforms may be specified in the same command for different channels\n\
+    Note: specifying -a, -r, or vmax!=vmin may interfere with other applications using the A2D device\n\
     ";
     return 1;
 }
 
 int DMD_MMAT_test::parseRunstring(int argc, char * argv[])
 {
-    extern char *optarg;       /* set by getopt() */
-    extern int optind;       /* "  "     "     */
-    int opt_char;     /* option character */
+    extern char *optarg; /* set by getopt() */
+    extern int optind;   /* "  "     "     */
+    int opt_char;        /* option character */
 
     while ((opt_char = getopt(argc, argv, "ad:r:w:")) != -1) {
         switch (opt_char) {
@@ -119,10 +122,14 @@ int DMD_MMAT_test::parseRunstring(int argc, char * argv[])
                 if (comma != ',' || ist.fail()) return usage(argv[0]);
                 ist >> output.len >> comma;
                 if (comma != ',' || ist.fail()) return usage(argv[0]);
-                ist >> output.vmin >> comma;
+                ist >> output.vstart >> comma;
                 if (comma != ',' || ist.fail()) return usage(argv[0]);
-                ist >> output.vmax;
+                ist >> output.vend;
                 if (ist.fail()) return usage(argv[0]);
+                if (fabs(output.vend - output.vstart) < 0.001)
+                    output.constant = true;
+                else
+                    output.constant = false;
                 _waveforms.push_back(output);
             }
             break;
@@ -142,6 +149,7 @@ void DMD_MMAT_test::run() throw(n_u::IOException)
     int fd = ::open(_deviceName.c_str(),O_RDWR);
     if (fd < 0) throw n_u::IOException(_deviceName,"open",errno);
 
+    // Autocalibrate A2D if instructed
     if (_autocalA2D) {
         res = ::ioctl(fd,DMMAT_A2D_DO_AUTOCAL);
         if (res < 0) {
@@ -153,6 +161,13 @@ void DMD_MMAT_test::run() throw(n_u::IOException)
         return;
     }
 
+    // Stop D2A waveform generator
+    if ((res = ::ioctl(fd,DMMAT_STOP)) < 0) {
+        ::close(fd);
+        throw n_u::IOException(_deviceName,"ioctl(,DMMAT_STOP,)",errno);
+    }
+
+    // Get D2A setup config informations
     int nchan = ::ioctl(fd,DMMAT_D2A_GET_NOUTPUTS);
     if (nchan < 0) {
         ::close(fd);
@@ -166,13 +181,14 @@ void DMD_MMAT_test::run() throw(n_u::IOException)
         throw n_u::IOException(_deviceName,"ioctl(,DMMAT_D2A_GET_CONVERSION,)",errno);
     }
 
+    // Display D2A config info
     for (int i = 0; i < nchan; i++) {
         cout << "D2A conversion for chan= " << i << 
             ": vmin=" << conv.vmin[i] << ", vmax=" << conv.vmax[i] <<
             ", cmin=" << conv.cmin[i] << ", cmax=" << conv.cmax[i] << endl;
     }
 
-    // Set the output rate in _Hz.
+    // Set the output rate in [Hz].
     struct D2A_Config cfg;
     cfg.waveformRate = (int)rint(_rate);
     if ((res = ::ioctl(fd,DMMAT_D2A_SET_CONFIG, &cfg)) < 0) {
@@ -180,39 +196,83 @@ void DMD_MMAT_test::run() throw(n_u::IOException)
         throw n_u::IOException(_deviceName,"ioctl(,DMMAT_D2A_SET_CONFIG,)",errno);
     }
 
+    // Get the Output Configurations, initialize outputs struct
+    struct DMMAT_D2A_Outputs outputs;
+    res = ::ioctl(fd,DMMAT_D2A_GET,&outputs);
+    if (res < 0) {
+        ::close(fd);
+        throw n_u::IOException(_deviceName,"ioctl(,DMMAT_D2A_GET,)",errno);
+    }
+    outputs.nout = DMMAT_D2A_OUTPUTS_PER_BRD;
+
+    // Generate waveforms
+    //   Channel outputs not specified will remain unchanged
+    //   Unspecified waveforms will be stopped
+    bool start_waveform_generator = false;
+
     for (unsigned int iwave = 0; iwave < _waveforms.size(); iwave++) {
         int chan = _waveforms[iwave].chan;
         int wlen = _waveforms[iwave].len;
 
-        float outscale = (conv.cmax[chan] - conv.cmin[chan]) / (conv.vmax[chan] - conv.vmin[chan]);
+        float outscale = 1.0*(conv.cmax[chan] - conv.cmin[chan]) / (conv.vmax[chan] - conv.vmin[chan]);
+        float dv = (_waveforms[iwave].vend - _waveforms[iwave].vstart) / wlen;
 
-        float dv = (_waveforms[iwave].vmax - _waveforms[iwave].vmin) / wlen;
+        cerr << "Waveform, Ch = " << chan << endl;
+        cerr << "wlen = " << wlen << endl;
+        cerr << "vend = " << _waveforms[iwave].vend << endl;
+        cerr << "vstart = " << _waveforms[iwave].vstart << endl;
+        cerr << "dc/dv = " << outscale << endl;
+        cerr << "dv/dt = " << dv << endl;
 
-        D2A_WaveformWrapper wave(chan,wlen);
-        D2A_Waveform *wavep = wave.c_ptr();
+        if (_waveforms[iwave].constant == false)
+        {
+            D2A_WaveformWrapper wave(chan,wlen);
+            D2A_Waveform *wavep = wave.c_ptr();
 
-        // create a simple ramp from vmin to vmax
-        for(int ip = 0; ip < wlen; ip++){
-            float v = _waveforms[iwave].vmin + ip * dv;
-            wavep->point[ip] = (int)rint((v - conv.vmin[chan]) * outscale + conv.cmin[chan]);
+            // create a simple ramp from vstart to vend
+            for(int ip = 0; ip < wlen; ip++){
+                float v = _waveforms[iwave].vstart + ip * dv;
+                if (v < conv.vmin[chan]) v = conv.vmin[chan];
+                if (v > conv.vmax[chan]) v = conv.vmax[chan];
+                wavep->point[ip] = (int)rint((v - conv.vmin[chan]) * outscale + conv.cmin[chan]);
+            }
+
+            if ((res = ::ioctl(fd,DMMAT_ADD_WAVEFORM, wavep)) < 0) {
+                ::close(fd);
+                throw n_u::IOException(_deviceName,"ioctl(,DMMAT_ADD_WAVEFORM,)",errno);
+            }
+
+            outputs.counts[chan] = _waveforms[iwave].vstart; // Begin with start ramp value
+            outputs.active[chan] = 1; // Mark channel active
+            start_waveform_generator = true;
         }
-
-        if ((res = ::ioctl(fd,DMMAT_ADD_WAVEFORM, wavep)) < 0) {
-            ::close(fd);
-            throw n_u::IOException(_deviceName,"ioctl(,DMMAT_ADD_WAVEFORM,)",errno);
+        else // Constant waveform
+        {
+            int code = (int)rint((_waveforms[iwave].vstart - conv.vmin[chan]) * outscale + conv.cmin[chan]);
+            outputs.active[chan] = 1;
+            outputs.counts[chan] = code;
+            cerr << "DAC const code = " << code << endl;
         }
     }
 
-    // Tell the D2A device to start.	
-    if ((res = ::ioctl(fd,DMMAT_START)) < 0) {
+    // Output constant voltages on active channels
+    if ((res = ::ioctl(fd,DMMAT_D2A_SET, &outputs)) < 0) {
         ::close(fd);
-        throw n_u::IOException(_deviceName,"ioctl(,DMMAT_START,)",errno);
+        throw n_u::IOException(_deviceName,"ioctl(,DMMAT_D2A_SET,)",errno);
     }
 
-    cerr << "do ctrl-C to stop" << endl;
-    ::pause();
-    // sleep(30);
-    
+    // Start D2A waveform generator if requested
+    if (start_waveform_generator == true)
+    {	
+        if ((res = ::ioctl(fd,DMMAT_START)) < 0) {
+            ::close(fd);
+            throw n_u::IOException(_deviceName,"ioctl(,DMMAT_START,)",errno);
+        }
+
+        cerr << "Waveforms Running, [ctrl-C] to stop" << endl;
+        ::pause();
+    }    
+
     cerr << "closing " << _deviceName << endl;
     ::close(fd);
 }
