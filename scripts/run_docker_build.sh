e386b721f60ef48f3c5872628e7150332ff8dc08
Gordon Maclean <maclean@users.noreply.github.com>
2020-09-18 10:54:57 -0600

    Add -u user and -g group runstring options
    And some help for user and permission issues.

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -3,10 +3,24 @@
 # Start a Docker container to do cross-building of NIDAS for various
 # non-x86_64, non-redhat systems.
 
-if [ $# -lt 1 ]; then
-    echo "usage: ${0##*/} [ armel | armhf | armbe | viper=armel | titan=armel | rpi2=armhf | vulcan=armbe ]"
+defuser=builder
+defgroup=eol
+
+user=$defuser
+group=$defgroup
+
+usage() {
+    echo "usage: ${0##*/} [-u user] [-g group] [ armel | armhf | armbe | viper=armel | titan=armel | rpi2=armhf | vulcan=armbe ]
+
+If user id is numeric and not in /etc/passwd inside the container, you'll see a prompt of \"I have no name\", which isn't generally fatal. If it is a string and not in /etc/passwd, docker will fail.
+
+The user in the container should have have write permission on the file systems that are bind mounted in the container, which includes this nidas tree. If is often easiest to make everything writable by the group.
+
+Default user: $defuser, default group: $defgroup"
     exit 1
-fi
+}
+
+while [ $# -gt 0 ]; do
 
 case $1 in
     armel | viper | titan)
@@ -18,7 +32,21 @@ case $1 in
     armbe | vulcan)
         image=maclean/fedora25-armbe-cross:ael
         ;;
+    -u)
+        shift
+        [ $# -lt 1 ] && usage
+        user=$1
+        ;;
+    -g)
+        shift
+        [ $# -lt 1 ] && usage
+        group=$1
+        ;;
 esac
+    shift
+done
+
+[ -z $image ] && usage
 
 # The nidas tree is the parent of the directory containing this script.
 # It will be bind mounted to ~/nidas in the Docker container.
@@ -27,16 +55,11 @@ esac
 dir=$(dirname $0)
 cd $dir/..
 
+nowrite=$(find . \( \! -perm /020 -o \! -group $group \) -print -quit)
+[ -n "$nowrite" ] && echo "Warning, some files in $PWD don't have $group group write access. Do \"chgrp -R $group $PWD; chmod -R g+w $PWD\""
+
 # If the image is not already loaded, docker run will pull the image from
 # the Docker Hub.
-
-# Run the image as the current user and group=eol, so the image will have
-# permission to write to the source tree and the files will have the right
-# owner.  It is safe to disregard the "Who am I?" messages in the container
-# caused by the user id not existing in the /etc/passwd file.
-
-# group=$(id -g)
-group=eol
 
 # if embedded-linux is cloned next to nidas then mount that in the container
 # for building kernels
@@ -46,10 +69,11 @@ embdir=$PWD/../embedded-linux
 repo=/net/ftp/pub/archive/software/debian
 [ -d $repo ] && repoopt="--volume $repo:$repo:rw"
 
+echo "Running container as user $user. If it isn't listed in /etc/passwd in the container, you'll have a \"I have no name\" prompt, but it isn't necessarily a fatal problem."
 echo "Running container as group $group, which must have rwx permission on $PWD and /opt/nidas"
 
 set -x
-exec docker run --rm --user `id -u`:$group \
+exec docker run --rm --user $user:$group \
     --volume $PWD:/home/builder/nidas:rw \
     --volume /opt/nidas:/opt/nidas:rw \
     $repoopt $embopt \
