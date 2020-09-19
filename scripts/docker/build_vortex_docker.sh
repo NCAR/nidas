4776bcb653440436d35580ed26bbdbb361f3ebd3
Gordon Maclean <maclean@users.noreply.github.com>
2020-09-19 09:52:25 -0600

    Fix gid in vortex image build
    Minor tweaks to armbe stuff - hopefully never used again...

--- scripts/docker/build_vortex_docker.sh
+++ scripts/docker/build_vortex_docker.sh
@@ -2,13 +2,21 @@
 
 set -e
 
-# image will have a "builder" user, and a group eol:1342
+# A --build-arg overrides an ARG in the Dockerfile
+user=builder
+uid=1000
 group=eol
 gid=1342
 
+# Must do a #docker login" as $docuser before the push
+docuser=maclean
+
 image=ubuntu-i386:xenial
 
-docker build -t $image -f Dockerfile.ubuntu_i386_xenial .
+docker build -t $image \
+    --build-arg=user=$user --build-arg=uid=$uid \
+    --build-arg=group=$group --build-arg=gid=$gid \
+    -f Dockerfile.ubuntu_i386_xenial .
 
-docker tag $image maclean/$image
-docker push maclean/$image
+docker tag $image $docuser/$image
+docker push $docuser/$image
