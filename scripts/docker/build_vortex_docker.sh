965b3be7a5f4a4ad10bc24ad2247001fd6cb6387
Gordon Maclean <maclean@users.noreply.github.com>
2020-10-24 16:16:05 -0600

    Updates for Ubuntu bionic image
    Provide -n option in build_vortex_docker.sh to suppress installing
    local EOL images.  Use it to create an image when the local
    packages are not available yet.
    
    Install newer debhelper from ubuntu-backports to get around a
    cross-device rename problem in containers.

--- scripts/docker/build_vortex_docker.sh
+++ scripts/docker/build_vortex_docker.sh
@@ -3,12 +3,25 @@
 set -e
 
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 release
+    echo "Usage: $0 [-n] release
+    -n: don't install local EOL packages, in case they don't exist yet
     for example:  $0 bionic"
     exit 1
 fi
 
-release=$1
+dolocal=yes
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    -n)
+        dolocal=no
+        ;;
+    *)
+        release=$1
+        ;;
+    esac
+    shift
+done
 
 # A --build-arg overrides an ARG in the Dockerfile
 user=ads
@@ -26,6 +39,7 @@ image=nidas-build-ubuntu-i386:$release
 docker build -t $image \
     --build-arg=user=$user --build-arg=uid=$uid \
     --build-arg=group=$group --build-arg=gid=$gid \
+    --build-arg=dolocal=$dolocal \
     -f Dockerfile.ubuntu_i386_$release .
 
 docker tag $image $docuser/$image
