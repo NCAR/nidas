59709483b0af83ac62f49e411a76ec981af65821
Gary Granger <granger@ucar.edu>
2021-06-11 11:52:44 -0600

    reformat, no functional changes

--- src/nidas/dynld/WxtSensor.cc
+++ src/nidas/dynld/WxtSensor.cc
@@ -22,7 +22,7 @@
  ** 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  **
  ********************************************************************
-*/
+ */
 
 #include "WxtSensor.h"
 #include <nidas/core/AsciiSscanf.h>
@@ -48,85 +48,101 @@ NIDAS_CREATOR_FUNCTION(WxtSensor)
 
 WxtSensor::WxtSensor():
     _field_formats(),
-    _uName("U"),_vName("V"),
-    _speedIndex(-1),_dirIndex(-1),_uIndex(-1),_vIndex(-1),
-    _speedDirId(0),_uvId(0),_uvlen(0)
+    _uName("U"),
+    _vName("V"),
+    _speedIndex(-1),
+    _dirIndex(-1),
+    _uIndex(-1),
+    _vIndex(-1),
+    _speedDirId(0),
+    _uvId(0),
+    _uvlen(0)
 {
 }
 
-WxtSensor::~WxtSensor()
-{
-}
+WxtSensor::~WxtSensor() {}
 
-void WxtSensor::init() throw(nidas::util::InvalidParameterException)
+void
+WxtSensor::init() throw(nidas::util::InvalidParameterException)
 {
     CharacterSensor::init();
 
-    static struct ParamSet {
-        const char* name;	// parameter name
-        void (WxtSensor::* setFunc)(const string&);
-        			// ptr to setXXX member function
-				// for setting parameter.
+    static struct ParamSet
+    {
+        const char* name; // parameter name
+        void (WxtSensor::*setFunc)(const string&);
+        // ptr to setXXX member function
+        // for setting parameter.
     } paramSet[] = {
-	{ "u",			&WxtSensor::setUName },
-	{ "v",			&WxtSensor::setVName },
+        { "u", &WxtSensor::setUName },
+        { "v", &WxtSensor::setVName },
     };
 
     // Scan the parameters for this sensor, looking for those
     // with the above names. The value of those parameters
     // is the expected initial portion of the variable name
     // for the given quantity.
-    for (unsigned int i = 0; i < sizeof(paramSet) / sizeof(paramSet[0]); i++) {
-	const Parameter* param = getParameter(paramSet[i].name);
-	if (!param) continue;
-	if (param->getLength() != 1) 
-	    throw nidas::util::InvalidParameterException(getName(),
-		"parameter", string("bad length for ") + paramSet[i].name);
-	// invoke setXXX member function
-	(this->*paramSet[i].setFunc)(param->getStringValue(0));
+    for (unsigned int i = 0; i < sizeof(paramSet) / sizeof(paramSet[0]); i++)
+    {
+        const Parameter* param = getParameter(paramSet[i].name);
+        if (!param)
+            continue;
+        if (param->getLength() != 1)
+            throw nidas::util::InvalidParameterException(
+                getName(), "parameter",
+                string("bad length for ") + paramSet[i].name);
+        // invoke setXXX member function
+        (this->*paramSet[i].setFunc)(param->getStringValue(0));
     }
 
     unsigned int unl = getUName().length();
     unsigned int vnl = getVName().length();
 
-    for (SampleTagIterator si = getSampleTagIterator(); si.hasNext(); ) {
-	const SampleTag* stag = si.next();
+    for (SampleTagIterator si = getSampleTagIterator(); si.hasNext();)
+    {
+        const SampleTag* stag = si.next();
 
         // check the initial characters in the variable names to determine
         // which is wind u and v.  Rather than impose a policy on those
         // variable names, the names can be specified by the user as
         // parameters, above.
         VariableIterator vi = stag->getVariableIterator();
-        for (int i = 0; vi.hasNext(); i++) {
+        for (int i = 0; vi.hasNext(); i++)
+        {
             const Variable* var = vi.next();
             const string& vname = var->getName();
             dsm_sample_id_t uvId = 0;
 
             if (vname == getUName() ||
                 (vname.length() >= unl + 1 &&
-                 vname.substr(0,unl+1) == getUName() + '.')) {
+                 vname.substr(0, unl + 1) == getUName() + '.'))
+            {
                 _uIndex = i;
                 uvId = stag->getId();
                 _uvlen = stag->getVariables().size();
             }
             else if (vname == getVName() ||
-                (vname.length() >= vnl + 1 &&
-                 vname.substr(0,vnl+1) == getVName() + '.')) {
+                     (vname.length() >= vnl + 1 &&
+                      vname.substr(0, vnl + 1) == getVName() + '.'))
+            {
                 _vIndex = i;
                 uvId = stag->getId();
             }
-            if (uvId != 0) {
-                if (_uvId == 0) _uvId = uvId;
-                else if (_uvId != uvId) 
-                    throw nidas::util::InvalidParameterException(getName() +
-                            " derived U and V must be in one sample");
+            if (uvId != 0)
+            {
+                if (_uvId == 0)
+                    _uvId = uvId;
+                else if (_uvId != uvId)
+                    throw nidas::util::InvalidParameterException(
+                        getName() + " derived U and V must be in one sample");
             }
         }
     }
 
     const std::list<AsciiSscanf*>& sscanfers = getScanfers();
     std::list<AsciiSscanf*>::const_iterator si = sscanfers.begin();
-    for ( ; si != sscanfers.end(); ++si) {
+    for (; si != sscanfers.end(); ++si)
+    {
         AsciiSscanf* sscanf = *si;
         const SampleTag* stag = sscanf->getSampleTag();
 
@@ -139,18 +155,23 @@ void WxtSensor::init() throw(nidas::util::InvalidParameterException)
 
         vector<string>::iterator fi = field_formats.begin();
         int nfield = 0;
-        for ( ; fi != field_formats.end(); ++fi) {
+        for (; fi != field_formats.end(); ++fi)
+        {
             if (fi->find("%f") != string::npos &&
-                fi->rfind("%f") == fi->find("%f")) {
+                fi->rfind("%f") == fi->find("%f"))
+            {
 
-                if (fi->length() > 3) {
+                if (fi->length() > 3)
+                {
                     // look for Dm= field, which is wind direction mean
-                    if (fi->substr(0,3) == "Dm=") {
+                    if (fi->substr(0, 3) == "Dm=")
+                    {
                         _dirIndex = nfield;
                         _speedDirId = stag->getId();
                     }
                     // look for Sm= field, which is wind speed mean
-                    else if (fi->substr(0,3) == "Sm=") {
+                    else if (fi->substr(0, 3) == "Sm=")
+                    {
                         _speedIndex = nfield;
                     }
                 }
@@ -159,22 +180,25 @@ void WxtSensor::init() throw(nidas::util::InvalidParameterException)
         }
         if (nfield != sscanf->getNumberOfFields())
         {
-            throw nidas::util::InvalidParameterException
-                (getName(), "scanfFormat",
-                 "only %f conversions are supported for WxtSensor, "
-                 "and only 1 per comma separated field. Use %*f to skip a field");
+            throw nidas::util::InvalidParameterException(
+                getName(), "scanfFormat",
+                "only %f conversions are supported for WxtSensor, "
+                "and only 1 per comma separated field. Use %*f to skip a "
+                "field");
         }
     }
-    if (_uvId != 0 && (_dirIndex < 0 || _speedIndex < 0)) {
-        throw InvalidParameterException
-            (getName(), "scanfFormat",
-             "Sm and/or Dm fields are not found in scanfFormat. "
-             "Cannot derive wind U and V from speed and direction");
+    if (_uvId != 0 && (_dirIndex < 0 || _speedIndex < 0))
+    {
+        throw InvalidParameterException(
+            getName(), "scanfFormat",
+            "Sm and/or Dm fields are not found in scanfFormat. "
+            "Cannot derive wind U and V from speed and direction");
     }
 }
 
-
-int WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr, float* dataptr)
+int
+WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr,
+                      float* dataptr)
 {
     const SampleTag* stag = sscanf->getSampleTag();
     dsm_sample_id_t sid = stag->getId();
@@ -202,28 +226,26 @@ int WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr, float* data
         {
             VLOG(("out of sample fields, filling variable with nan: ")
                  << var->getName());
-	    *(dataptr++) = floatNAN;
+            *(dataptr++) = floatNAN;
             ++fi;
             ++vi;
             continue;
         }
 
-	// Exact match, so no specifiers, just keep looking for the next
-	// variable field.
-	if (*fi == *si)
-	{
-	    VLOG(("format match: ") << *fi << " == " << *si);
+        // Exact match, so no specifiers, just keep looking for the next
+        // variable field.
+        if (*fi == *si)
+        {
+            VLOG(("format match: ") << *fi << " == " << *si);
             ++fi;
             ++si;
-	    continue;
-	}
+            continue;
+        }
 
         // If it's the header, accept 1R0, 1r0, 0R0, and 0r0.
         const std::string& sf = *si;
-        if (fi == field_formats.begin() &&
-            fi->length() == si->length() &&
-            fi->length() == 3 &&
-            (sf[0] == '0' || sf[0] == '1') &&
+        if (fi == field_formats.begin() && fi->length() == si->length() &&
+            fi->length() == 3 && (sf[0] == '0' || sf[0] == '1') &&
             (std::toupper(sf[1]) == std::toupper((*fi)[1])) &&
             (sf[2] == (*fi)[2]))
         {
@@ -231,7 +253,7 @@ int WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr, float* data
                  << sf << " =~ " << (*fi));
             ++fi;
             ++si;
-	    continue;
+            continue;
         }
         else if (fi == field_formats.begin())
         {
@@ -251,33 +273,32 @@ int WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr, float* data
             // identifiers match.  The only question is whether a number
             // can be parsed using the full field specifier.
             ++nparsed;
-	    string full = (*fi) + "%n";
-	    int n = 0;
-	    if (std::sscanf(si->c_str(), full.c_str(), &value, &n) >= 1 &&
-		(unsigned)n == si->length())
-	    {
+            string full = (*fi) + "%n";
+            int n = 0;
+            if (std::sscanf(si->c_str(), full.c_str(), &value, &n) >= 1 &&
+                (unsigned)n == si->length())
+            {
                 // Require n to equal the full sample field length, so a
                 // value will not be parsed if the units character after
                 // the number does not match, especially if it's a #
                 // indicating a bad value.
-                VLOG(("field scanned (") << *fi << "): " << *si << " = " << value);
-	    }
+                VLOG(("field scanned (")
+                     << *fi << "): " << *si << " = " << value);
+            }
             else
             {
                 value = floatNAN;
-                VLOG(("field NOT scanned, set to NaN (") << *fi << "): " << *si
-                     << " = " << value);
+                VLOG(("field NOT scanned, set to NaN (")
+                     << *fi << "): " << *si << " = " << value);
             }
             ++si;
-	}
-	else
-	{
-	    DLOG(("") << getName()
-                 << ": format field "
-                 << *fi << " does not match next sample field "
-		 << *si
-                 << ", setting variable " << var->getName() << " to nan");
-	}
+        }
+        else
+        {
+            DLOG(("") << getName() << ": format field " << *fi
+                      << " does not match next sample field " << *si
+                      << ", setting variable " << var->getName() << " to nan");
+        }
         *(dataptr++) = value;
         ++fi;
         ++vi;
@@ -287,32 +308,38 @@ int WxtSensor::scanSample(AsciiSscanf* sscanf, const char* inputstr, float* data
     // returned data values should match up with each variables, where the
     // variable value is NaN if it did not scan.
     if (nparsed > 0)
-	return sscanf->getNumberOfFields();
+        return sscanf->getNumberOfFields();
     return 0;
 }
 
-bool WxtSensor::process(const Sample* samp,
-	std::list<const Sample*>& results) throw()
+bool
+WxtSensor::process(const Sample* samp,
+                   std::list<const Sample*>& results) throw()
 {
-
     std::list<const Sample*> vane;
-    SerialSensor::process(samp,results);
+    SerialSensor::process(samp, results);
 
-    if (results.empty()) return false;
+    if (results.empty())
+        return false;
     if (_speedIndex < 0 || _dirIndex < 0 || _uIndex < 0 || _vIndex < 0)
-    	return true;
+        return true;
 
     std::list<const Sample*>::const_iterator si = results.begin();
-    for ( ; si != results.end(); ++si) {
+    for (; si != results.end(); ++si)
+    {
 
-        // result from base class parsing of ASCII, and correction of any cal file
+        // result from base class parsing of ASCII, and correction of any cal
+        // file
         const Sample* csamp = *si;
-        if (csamp->getId() != _speedDirId) continue;
+        if (csamp->getId() != _speedDirId)
+            continue;
 
         unsigned int slen = csamp->getDataLength();
 
-        if ((signed) slen <= _speedIndex) continue;
-        if ((signed) slen <= _dirIndex) continue;
+        if ((signed)slen <= _speedIndex)
+            continue;
+        if ((signed)slen <= _dirIndex)
+            continue;
 
         float spd = csamp->getDataValue(_speedIndex);
         float dir = csamp->getDataValue(_dirIndex);
