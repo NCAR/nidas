cdc685358f56c05b49d12ccca57f886ec2f7330f
Gordon Maclean <maclean@users.noreply.github.com>
2021-08-10 14:55:18 -0600

    Updates to podman_save.sh

--- scripts/docker/podman_save.sh
+++ scripts/docker/podman_save.sh
@@ -1,6 +1,57 @@
 #!/bin/sh
 
-# save idas-build-debian-base-armel:jessie image.
-# I don't believe the armel cross-development tools are available in 
-# in debian repositories anymore, so we don't want to lose this image
-podman save --compress --format oci-dir -o base-armel.dir docker.io/ncar/nidas-build-debian-base-armel:jessie
+usage() {
+    echo "usage: ${0##*/} [-p] [ armel | armhf | armbe | bionic
+
+    Do
+        podman save --compress --format oci-dir -o ...
+    to save an archive of an imabge.
+
+    -p: pull image from docker.io"
+    exit 1
+}
+
+dopull=false
+dockerns=ncar   # namespace on docker.io
+
+while [ $# -gt 0 ]; do
+
+case $1 in
+    armel)
+        image=nidas-build-debian-armel
+        fullname=$dockerns/$image:jessie_v2
+        ;;
+    armhf)
+        image=nidas-build-debian-armhf
+        fullname=$dockerns/$image:jessie_v2
+        ;;
+    armbe)
+        image=nidas-build-ael-armbe
+        fullname=$dockerns/$image:ael_v1
+        ;;
+    bionic)
+        image=nidas-build-ubuntu-i386
+        fullname=$dockerns/$image:bionic
+        ;;
+    -p)
+        dopull=true
+        ;;
+    *)
+        usage
+        ;;
+esac
+    shift
+done
+
+[ -z $image ] && usage
+
+$dopull && podman pull docker.io/$fullname
+
+dir=$(dirname $0)
+cd $dir
+
+sdir=saved_images
+[ -d $sdir ] || mkdir $sdir
+
+podman save --compress --format oci-dir -o $sdir/$image.dir $fullname && \
+    echo "$fullname saved to $sdir/$image.dir"
