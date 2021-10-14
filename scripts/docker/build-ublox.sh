972800540b4a5723e97f80a15656c0f62cb57502
Gary Granger <granger@ucar.edu>
2021-10-14 06:55:55 -0600

    build a known source-compatible version of ublox

--- scripts/docker/build-ublox.sh
+++ scripts/docker/build-ublox.sh
@@ -4,17 +4,15 @@
 export buildDir="build"
 export NAME=cc.ublox.generated
 export URL="https://github.com/arobenko/cc.ublox.generated.git"
+tag="v0.20.2"
 
 #Clone it
-if [ ! -d "${NAME}" ]
-then
-    echo "cloning: $URL..."
-    git clone "$URL" 
-fi
-
-cd ${NAME}
+rm -rf "${NAME}"
+echo "cloning tag ${tag}: $URL..."
+git clone --depth 1 --branch "$tag" "$URL" "$NAME"
 
 # always a clean build...
+cd ${NAME}
 rm -rf $buildDir
 mkdir -p $buildDir && cd $buildDir
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/usr/local" ../
