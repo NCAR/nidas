b198693c182d7c60bf396595f79e7892692d25b7
Gary Granger <granger@ucar.edu>
2022-09-09 01:00:36 -0600

    initialize Sample data more conveniently
    ...allow initializer_list and constructing a
    SampleT<char> directly from a char buffer.

--- src/nidas/core/Sample.h
+++ src/nidas/core/Sample.h
@@ -41,6 +41,7 @@
 #include <climits>
 #include <iostream>
 #include <cstring>
+#include <initializer_list>
 
 #include <cmath>
 
@@ -417,6 +418,34 @@ public:
         _data(0),_allocLen(0)
     {}
 
+    void
+    setValues(std::initializer_list<DataT> values)
+    {
+        unsigned int len = values.size();
+        allocateData(len);
+        setDataLength(len);
+        unsigned int i = 0;
+        for (auto v : values)
+        {
+            setDataValue(i++, (DataT)v);
+        }
+    }
+
+    /**
+     * Construct a SampleT from an initializer list.  @see setValues().
+     *
+     * For example, this constructs a float sample with 4 values:
+     * @code
+     * SampleT<float> sample { 1, 2, 3, 4 };
+     * @endcode
+     */
+    SampleT(std::initializer_list<DataT> values) :
+        Sample(sample_type_traits<DataT>::sample_type_enum),
+        _data(0),_allocLen(0)
+    {
+        setValues(values);
+    }
+
     ~SampleT() { delete [] _data; }
 
     sampleType getType() const { return getSampleType(_data); }
@@ -593,6 +622,27 @@ protected:
 
     /** No assignment */
     SampleT& operator=(const SampleT&);
+};
+
+
+class SampleChar : public SampleT<char>
+{
+public:
+    /**
+     * Construct a SampleT<char> initialized to the bytes in buffer.
+     *
+     * The buffer must be null-terminated, and the null will be included in
+     * the Sample data.
+     *
+     * @param buffer null-terminated array of chars
+     */
+    SampleChar(const char* buffer)
+    {
+        unsigned int len = strlen(buffer) + 1;
+        allocateData(len);
+        setDataLength(len);
+        strcpy(getDataPtr(), buffer);
+    }
 };
 
 /**
