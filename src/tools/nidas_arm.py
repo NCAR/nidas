233d200e1ba6963523e5b21ffe689a311077205e
Gary Granger <granger@ucar.edu>
2023-02-16 13:11:35 -0700

    oops, scons tool needs generate()

--- src/tools/nidas_arm.py
+++ src/tools/nidas_arm.py
@@ -7,7 +7,7 @@ import re
 from SCons.Script import Environment
 
 
-def arm(env: Environment):
+def generate(env: Environment):
     env.Require(['armcross'])
     env['ARCHLIBDIR'] = 'lib'
     env['ARCHPREFIX'] ='$PREFIX/arm'
