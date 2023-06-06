4fc6a590fd89460fb2e02727dd707f958967f87d
Delia McKee-Gresham <cmckee@ucar.edu>
2023-06-06 14:14:59 -0600

    Adjusted loop, moved to seperate function.

--- src/nidas/apps/button_action.cc
+++ src/nidas/apps/button_action.cc
@@ -135,7 +135,46 @@ int readJson(){
 
 }
 
+int loop(){
+    auto hwi= HardwareInterface::getHardwareInterface();
+    HardwareDevice device=hwi->lookupDevice(Device);
+    if (device.isEmpty())
+    {
+        std::cerr << "unrecognized device: " << Device << endl;
+        return 2;
+    }
+    auto ioutput = device.iOutput();
+    if(!ioutput)
+    {
+        std::cerr<<"unable to open "<<Device<<endl;
+        return 3;
+    }
 
+    bool buttonDown=false;
+    do{
+        sleep(1);
+        auto ibutton = device.iButton();
+        buttonDown=ibutton->isDown();
+        
+    }while(!buttonDown);
+    buttonDown=false;
+    auto ledState=device.iOutput()->getState();
+    if(ledState.toString()=="off")
+    {
+        runaction(Device,false);
+        ioutput->on(); //turns LED on
+    }
+    else
+    {    
+        runaction(Device,true);
+        ioutput->off(); //turns LED off
+            
+    }
+        device.reset();
+        hwi.reset();
+        return 0;
+        
+}
 
 int main(int argc, char* argv[]) {
 
@@ -147,46 +186,12 @@ int main(int argc, char* argv[]) {
     int j=readJson();
         if(j!=0)
         {
-            return 3;
+            return 1;
         }    
     while(run){
-        auto hwi= HardwareInterface::getHardwareInterface();
-        HardwareDevice device=hwi->lookupDevice(Device);
-        if (device.isEmpty())
-        {
-            std::cerr << "unrecognized device: " << Device << endl;
-            return 2;
-        }
-        auto ioutput = device.iOutput();
-        if(!ioutput)
-        {
-            std::cerr<<"unable to open "<<Device<<endl;
-            return 3;
-        }
-
-        bool buttonDown=false;
-        while(!buttonDown){
-            sleep(1);
-            auto ibutton = device.iButton();
-            buttonDown=ibutton->isDown();
-            //cout<<buttonDown<<endl;
-        }
-        buttonDown=false;
-        auto ledState=device.iOutput()->getState();
-        if(ledState.toString()=="off")
-        {
-            runaction(Device,false);
-            ioutput->on(); //turns LED on
-        }
-        else
-        {    
-            runaction(Device,true);
-            ioutput->off(); //turns LED off
-                
-        }
-            device.reset();
-            sleep(5);
-        }
+        loop();
+        sleep(5);
+    }
     return 0;
 }
 
