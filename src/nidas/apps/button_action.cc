abacac12fe2c900c597823ea9c906d73b08cc83e
Delia McKee-Gresham <cmckee@ucar.edu>
2023-06-12 12:56:25 -0600

    Changed program to run with multithreading, as well as read device names from json file.

--- src/nidas/apps/button_action.cc
+++ src/nidas/apps/button_action.cc
@@ -1,10 +1,8 @@
-#include <algorithm>
+
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <iomanip>
-#include <vector>
-#include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <fstream>
@@ -12,7 +10,7 @@
 #include <nidas/core/NidasApp.h>
 #include <nidas/core/HardwareInterface.h>
 #include <nidas/util/Termios.h>
-
+#include <pthread.h>
 #include <json/json.h>
 
 using namespace nidas::core;
@@ -29,15 +27,19 @@ NidasApp app("button_action");
 
 Json::Value root;
 std::string Path;
+pthread_barrier_t barr;
+bool exitloop=false;
+std::string Device1;
+std::string Device2;
 
 void usage(){
-    cerr << R""""(Usage: button_action [wifi|p1] 
+    cerr << R""""(Usage: button_action [path]
 
-Read input from button and perform associated actions.
+Read input from button and depending on current state (indicated by led), turn associated functions on or off.
     
-    {wifi|p1}:
+    path:
 
-    Wait for press of specified button, and depending on current state (indicated by led), turn associated functions on or off.
+    File path to json file containing commands to be executed on button press.
     )""""
     <<endl<<app.usage()<<endl;
 }
@@ -120,98 +122,110 @@ int readJson(){
         return 1;
     }
     jFile.close();
+    auto devs=root.getMemberNames();
+    if(devs.size()<2){
+        cerr<<"Format error in json file."<<endl;
+        return 1;
+    }
+    Device1=devs[1];
+    Device2=devs[0];
     return 0;
 
 
 }
+struct threadargs{
+    long i;
+    std::shared_ptr<HardwareInterface> hwi;
+    std::string Device;
+};
 
-int loop(std::shared_ptr<HardwareInterface> hwi){
-    HardwareDevice wifi=hwi->lookupDevice("wifi");
-    HardwareDevice p1=hwi->lookupDevice("p1");
-    if (wifi.isEmpty())
-    {
-        std::cerr << "unrecognized device: wifi" << endl;
-        return 2;
-    }
-     if (p1.isEmpty())
-    {
-        std::cerr << "unrecognized device: p1" << endl;
-        return 2;
-    }
-    auto wifioutput = wifi.iOutput();
-    auto poutput=p1.iOutput();
-    if(!wifioutput)
-    {
-        std::cerr<<"unable to open wifi"<<endl;
-        return 3;
-    }
-    if(!poutput)
-    {
-        std::cerr<<"unable to open p1"<<endl;
-        return 3;
-    }
-    bool wbuttonDown=false;
-    bool pbuttonDown=false;
-    do{
-        sleep(1);
-        auto wbutton = wifi.iButton();
-        auto pbutton= p1.iButton();
-        wbuttonDown=wbutton->isDown();
-        pbuttonDown=pbutton->isDown();
-        
-    }while(!(wbuttonDown) && !(pbuttonDown));
-    auto wledState=wifioutput->getState();
-    auto pledState=poutput->getState();
-    if(wbuttonDown){
-        if(wledState==OutputState::OFF)
-        {
-            runaction("wifi",false);
-            wifioutput->on(); //turns LED on
-        }
-        else
-        {    
-            runaction("wifi",true);
-            wifioutput->off(); //turns LED off
-                
-        }
-    }
-    if(pbuttonDown){
-        if(pledState==OutputState::OFF)
-        {
-            runaction("p1",false);
-            poutput->on(); //turns LED on
-        }
-        else
-        {    
-            runaction("p1",true);
-            poutput->off(); //turns LED off
-                
-        }
 
-    }
+int loop(std::shared_ptr<HardwareInterface> hwi,std::string Device){
+    HardwareDevice device=hwi->lookupDevice(Device);
+    if(exitloop){
         return 0;
         
+    }
+    if (device.isEmpty())
+    {
+        std::cerr << "unrecognized device: " <<Device<< endl;
+        return 2;
+    }
+    auto output = device.iOutput();
+    if(!output)
+    {
+        std::cerr<<"unable to open "<<Device<<endl;
+        return 3;
+    }
+    bool buttonDown=false;
+    do{
+        if(exitloop){
+            return 0;
+        }
+        sleep(1);
+        auto button = device.iButton();
+        buttonDown=button->isDown();
+        
+    }while(!(buttonDown));
+    auto ledState=output->getState();
+        if(ledState==OutputState::OFF)
+        {
+            runaction(Device,false);
+            output->on(); //turns LED on
+        }
+        else
+        {    
+            runaction(Device,true);
+            output->off(); //turns LED off
+                
+        }
+        exitloop=true;
+        return 0;
 }
-
+void *loopHandler(void *args){
+    auto hwi=((struct threadargs*)args)->hwi;
+    auto Device=((struct threadargs*)args)->Device;
+    loop(hwi,Device);
+    pthread_barrier_wait(&barr);
+    long i=((struct threadargs*)args)->i;
+    pthread_exit((void*)i);
+}
 int main(int argc, char* argv[]) {
 
     if (parseRunString(argc, argv))
      {
         exit(1);
      }
-    bool run=true;
+    
     //app.setupDaemon();
     int j=readJson();
         if(j!=0)
         {
             return 1;
-        }    
-    while(run){
+        }
+    struct threadargs *button1=(struct threadargs *)malloc(sizeof(struct threadargs));
+    struct threadargs *button2=(struct threadargs *)malloc(sizeof(struct threadargs)); 
+    pthread_t thread1, thread2;
+    button1->Device=Device1;
+    button2->Device=Device2;
+    button1->i=0;
+    button2->i=1;
+    while(true){
         auto hwi= HardwareInterface::getHardwareInterface();
-        loop(hwi);
+        exitloop=false;
+        pthread_barrier_init(&barr,0,2);
+        button1->hwi=hwi;
+        button2->hwi=hwi;
+        pthread_create(&thread1,NULL,loopHandler,(void *)button1);
+        pthread_create(&thread2,NULL,loopHandler,(void *)button2);
+        pthread_join(thread1,0);
+        pthread_join(thread2,0);
         hwi.reset();
         sleep(5);
     }
+    pthread_barrier_destroy(&barr);
+    free(button1);
+    free(button2);
     return 0;
 }
 
