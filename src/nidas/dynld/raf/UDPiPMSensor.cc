ae0a370e80c3291846f7c1d83f20defedcf01659
Janine Aquino <janine@ucar.edu>
2023-08-08 13:48:56 -0600

    Change comment to reflect that addrInfo string now contains scale flag instead of numphases. No functional change

--- src/nidas/dynld/raf/UDPiPMSensor.cc
+++ src/nidas/dynld/raf/UDPiPMSensor.cc
@@ -102,7 +102,7 @@ void UDPiPMSensor::validate()
         p = getParameter("dev" + std::to_string(i));  // details of address i
         if (!p) throw n_u::InvalidParameterException(getName(),
               "dev", "not found");
-        // addrInfo string contains addr, numphases, procquery, port,
+        // addrInfo string contains addr, scaleflag, procquery, port,
         // eg 0,1,5,30101
         _addrInfo[i] = p->getStringValue(0);
         ILOG(("device info for addresses ") << i << (": ") << _addrInfo[i]);
